d7039ec2f07b24cc1f4a9b9ed354a5d3327111a2
Pramod Chunduri <pramodc@gatech.edu>
2018-09-25 09:49:34 -0400

    Minor code changes in filter.py
    Former-commit-id: c1a34ecaa01e7cbcc23376365a8f5dde218b0eb3 [formerly c1a34ecaa01e7cbcc23376365a8f5dde218b0eb3 [formerly afc17613b2e5b063238c8cf98c0b108fcc25d977]]
    Former-commit-id: 25fe375857877d93cb60e4757b25f42c66c7fe2e
    Former-commit-id: 2fdbccec3303f9ccfeaf3500ebd6c4867aebc339

--- filters/filter.py
+++ filters/filter.py
@@ -33,7 +33,7 @@ def load_dataset(input_path):
 		X[i] = img_to_array(img)
 		X[i] = (X[i] - 128)/128
 		i += 1
-		if (i%1000 == 0):
+		if (i%10000 == 0):
 			print ("Loaded %d images" %i)
 	return X, num_frames_list			
 def get_vehtype_labels(filter, label_path, num_frames_list):
@@ -66,12 +66,23 @@ def get_vehtype_labels(filter, label_path, num_frames_list):
 			i+=1
 	return y
 
+def PP(clf,X_train, X_test, y_train, y_test):
+	train_start_time = time.time()
+	clf.fit(X_train,y_train)
+	print("train time: %.3f sec" % (time.time() - train_start_time))
+	test_start_time = time.time()
+	y_pred = clf.predict(X_test)
+	print("test time: %.3f sec" % (time.time() - test_start_time))
+	print ("Accuracy: %.3f"%clf.score(X_test,y_test))
+	unique,counts = np.unique(y_pred,return_counts=True)
+	print "Absolute reduction: ",counts[0],"/",len(y_test)
+	print ("reduction rate: %.3f"%(counts[0]*1.0/len(y_test)))
 
 def main():
-	# label_path = '../dataset/DETRAC-Train-Annotations-XML/'
-	label_path = '../dataset/small-annotation/'
-	# input_path = '../dataset/DETRAC-train-data/Insight-MVT_Annotation_Train/'
-	input_path = '../dataset/DETRAC-train-data/small-data/'
+	label_path = '../dataset/DETRAC-Train-Annotations-XML/'
+	# label_path = '../dataset/small-annotation/'
+	input_path = '../dataset/DETRAC-train-data/Insight-MVT_Annotation_Train/'
+	# input_path = '../dataset/DETRAC-train-data/small-data/'
 	vehtype_filters = ['car','van','bus']
 	X, num_frames_list = load_dataset(input_path)
 	nsamples, nx, ny, nc = X.shape
@@ -81,34 +92,19 @@ def main():
 		print ("--------------------Working on %s filter--------------------"%item)
 		y = get_vehtype_labels(item, label_path, num_frames_list)
 		y = np.array(y)
-		unique,counts = np.unique(y,return_counts=True)
 		# print "number of ",item, unique[1],counts[1]
 		# print ("Loaded %d labels"%len(y))
-		X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=33)
+		X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.33, random_state=42)
+		unique,counts = np.unique(y_test,return_counts=True)
+		print "No.of test frames without cars :",counts[0],"/",len(y_test)
+		########## Linear SVC ############
 		clf = LinearSVC(random_state=0)
 		print ("--- LinearSVC ---")
-		train_start_time = time.time()
-		clf.fit(X_train,y_train)
-		print("train time: %.3f sec" % (time.time() - train_start_time))
-		test_start_time = time.time()
-		y_pred = clf.predict(X_test)
-		print("test time: %.3f sec" % (time.time() - test_start_time))
-		print "Accuracy: ",clf.score(X_test,y_test)
-		unique,counts = np.unique(y_pred,return_counts=True)
-		print "Absolute reduction: ",counts[0],"/",len(y_test)
-		print "reduction rate: ", counts[0]*1.0/len(y_test)
+		PP(clf,X_train, X_test, y_train, y_test)
+		######### Random Forest ###########
 		clf = RandomForestClassifier(max_depth=2, random_state=0)
 		print ("--- RandomForest ---")
-		train_start_time = time.time()
-		clf.fit(X_train,y_train)
-		print("train time: %.3f sec" % (time.time() - train_start_time))
-		test_start_time = time.time()
-		y_pred = clf.predict(X_test)
-		print "Accuracy: ",clf.score(X_test,y_test)
-		unique,counts = np.unique(y_pred,return_counts=True)
-		print "Absolute reduction: ",counts[0],"/",len(y_test)
-		print "reduction rate: ", counts[0]*1.0/len(y_test)
-		print("test time: %.3f sec" % (time.time() - test_start_time))
+		PP(clf,X_train, X_test, y_train, y_test)
 	# X = 
 
 if __name__ == '__main__':
