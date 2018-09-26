5a9313e9a0a7540062eb2abfc451d60095a3a5bc
Jaeho Bang <jaehob@andrew.cmu.edu>
2018-09-26 15:47:31 -0400

    Adding bash scripts
    Former-commit-id: ca3d9f14627a7b42c98db4e27ef004375c60497c

---
+++ data/ua_detrac/unzip.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+sudo apt-get install unzip
+unzip DETRAC-train-data.zip
+unzip DETRAC-test-data.zip
+unzip DETRAC-Train-Annotations-MAT.zip
+unzip DETRAC-Train-Annotations-XML.zip
