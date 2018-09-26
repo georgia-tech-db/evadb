ca3d9f14627a7b42c98db4e27ef004375c60497c
Jaeho Bang <jaehob@andrew.cmu.edu>
2018-09-26 15:47:31 -0400

    Adding bash scripts

---
+++ data/ua_detrac/download.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+wget https://detrac-db.rit.albany.edu/Data/DETRAC-train-data.zip
+wget https://detrac-db.rit.albany.edu/Data/DETRAC-test-data.zip
+wget https://drive.google.com/drive/folders/1FJfBiR7SPbMrOoPXfGDJjmV5qepXFsJy?usp=sharing/DETRAC-Train-Annotations-XML.zip -o DETRAC-Train_Annotations-XML.zip
+wget https://drive.google.com/drive/folders/1FJfBiR7SPbMrOoPXfGDJjmV5qepXFsJy?usp=sharing/DETRAC-Train-Annotations-MAT.zip -o DETRAC-Train_Annotations-MAT.zip
