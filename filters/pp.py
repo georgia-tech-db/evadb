b8de841e229232bf256b6c69c1b4d719470b8335
Jaeho Bang <jaeho.bang@gmail.com>
2018-10-01 15:47:27 -0400

    Updated pp.py a little before heading to class
    Former-commit-id: 583da116a8a00bcef5ee577d6843feceb210fdff

--- filters/pp.py
+++ filters/pp.py
@@ -21,16 +21,60 @@ class PP_gen:
 
 
   def __init__(self):
-    self.model_library = {} #KDE, SVM, NN - this should be a mapping of model name to model CONSTRUCTOR
-    self.model_library_pre = {None} #feature hashing, PCA, None - Separated to do mix and match
+
+    """The way to use these libraries would be something like
+      def do_ping(self, arg):
+          return 'Pong, {0}!'.format(arg)
+
+      def do_ls(self, arg):
+          return '\n'.join(os.listdir(arg))
+
+      dispatch = {
+          'ping': do_ping,
+          'ls': do_ls,
+      }
+
+      def process_network_command(command, arg):
+          send(dispatch[command](arg))
+    """
+    self.model_library = {"kde": self.kde,
+                          "svm": self.svm,
+                          "dnn": self.dnn} #KDE, SVM, NN - this should be a mapping of model name to model CONSTRUCTOR
+
+    self.model_library_pre = {"fh": self.feature_hashing,
+                              "pca": self.pca,
+                              "sampling": self.sampling} #feature hashing, PCA, None - Separated to do mix and match
+
     self.category_libary = {} #save the trained model
     self.category_stats = {} #save the statistics related to the model, although most stats are embedded in the model,
                              #made this just in case there could be stats that are not saved
     #TODO: append the models to model_library (feature hashing, PCA, KDE, SVM, NN)
 
+  # Give category_name and its parsed X, Y, also provide arguments if you want to specify model arguments
+  def train(self, category_name, X, Y, args=None):
+    for model_name in self.model_libary:
+      model = self.model_library[model_name]
+      # TODO: try fitting and save statistics - we will have this information available for QO (Query Optimizer)
+      model.fit(X, Y)
+
+      self.category_stats[category_name] = {model_name: {"reduction_rate": model.score(),
+                                                         "false_negative_rate": model.......,
+                                                         "time_to_train":}
+
+                                            self.category_library[category_name] = {model_name: model}
+
+  def getCategoryInfo(self, category_name):
+    return self.category_stats[category_name]
+
+  def getCategoryModels(self, category_name):
+    return self.category_library[category_name]
+
+  # returns list of model names
+  def getModelLibrary(self):
+    return self.model_library.keys()
 
   #TODO: need to perform feature hashing after all the categories are inputted??
-  def feature_hashing(self):
+  def feature_hashing(self, args):
     category_count = len(self.category_libary.keys())
     if category_count < 2:
       return
@@ -43,14 +87,28 @@ class PP_gen:
     f = h.transform(D)
     f.toarray()
 
-  def pca(self):
+  def pca(self, args):
     X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
     pca = PCA(n_components=2)
     pca.fit(X)
     PCA(copy=True, iterated_power='auto', n_components=2, random_state=None,
         svd_solver='auto', tol=0.0, whiten=False)
-    print(pca.explained_variance_ratio_)
-    print(pca.singular_values_)
+
+    return pca #we will return the models for the caller to save them
+
+
+
+  def sampling(self, args):
+    pass
+
+  def svm(self, args):
+    pass
+
+  def dnn(self, args):
+    pass
+
+  def kde(self, args):
+    pass
 
   def PP(clf, X_train, X_test, y_train, y_test):
     train_start_time = time.time()
@@ -66,30 +124,6 @@ class PP_gen:
 
 
 
-  # Give category_name and its parsed X, Y, also provide arguments if you want to specify model arguments
-  def train(self, category_name, X, Y, args = None):
-    for model_name in self.model_libary:
-      model = self.model_library[model_name]
-      #TODO: try fitting and save statistics - we will have this information available for QO (Query Optimizer)
-      model.fit(X,Y)
-
-      self.category_stats[category_name] = {model_name: {"reduction_rate": model.score(),
-                                                           "false_negative_rate":model.......,
-                                                         "time_to_train": }
-
-      self.category_library[category_name] = {model_name: model}
-
-
-  def getCategoryInfo(self, category_name):
-    return self.category_stats[category_name]
-
-  def getCategoryModels(self, category_name):
-    return self.category_library[category_name]
-
-
-  # returns list of model names
-  def getModelLibrary(self):
-    return self.model_library.keys()
 
 
 
