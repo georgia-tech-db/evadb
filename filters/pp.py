828cacce357f2ddc122faa7300165576489f586b
Jaeho Bang <jaehob@andrew.cmu.edu>
2018-10-02 01:59:51 -0400

    Almost finished writing the filters; TODOs are listed; Need running; Need testing

--- filters/pp.py
+++ filters/pp.py
@@ -1,18 +1,9 @@
-import time
-import os, sys
-import numpy as np
-#import matplotlib.pyplot as plt
-
-
+from kdewrapper import KernelDensityWrapper
 from sklearn.svm import LinearSVC
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-import xml.etree.ElementTree as ET
-from scipy import ndimage
-from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 from sklearn.feature_extraction import FeatureHasher
 from sklearn.decomposition import PCA
-
+from sklearn.neural_network import MLPClassifier
 
 # Meant to be a black box for trying all models available and returning statistics and model for
 # the query optimizer to choose for a given query
@@ -22,46 +13,136 @@ class PP_gen:
 
   def __init__(self):
 
-    """The way to use these libraries would be something like
-      def do_ping(self, arg):
-          return 'Pong, {0}!'.format(arg)
-
-      def do_ls(self, arg):
-          return '\n'.join(os.listdir(arg))
-
-      dispatch = {
-          'ping': do_ping,
-          'ls': do_ls,
-      }
-
-      def process_network_command(command, arg):
-          send(dispatch[command](arg))
-    """
     self.model_library = {"kde": self.kde,
                           "svm": self.svm,
-                          "dnn": self.dnn} #KDE, SVM, NN - this should be a mapping of model name to model CONSTRUCTOR
+                          "dnn": self.dnn,
+                          "rf": self.rf} #KDE, SVM, NN - this should be a mapping of model name to model CONSTRUCTOR
 
-    self.model_library_pre = {"fh": self.feature_hashing,
-                              "pca": self.pca,
-                              "sampling": self.sampling} #feature hashing, PCA, None - Separated to do mix and match
+    self.pre_model_library = {"pca": self.pca,
+                              "none": self.none
+                              #"fh": self.feature_hashing,
+                              # "sampling": self.sampling,
+                              } #feature hashing, PCA, None - Separated to do mix and match
+
+    self.pre_results = {} #save the preprocessed results {"pre_model_name": reformed_data
+
 
     self.category_libary = {} #save the trained model
     self.category_stats = {} #save the statistics related to the model, although most stats are embedded in the model,
                              #made this just in case there could be stats that are not saved
-    #TODO: append the models to model_library (feature hashing, PCA, KDE, SVM, NN)
 
-  # Give category_name and its parsed X, Y, also provide arguments if you want to specify model arguments
-  def train(self, category_name, X, Y, args=None):
-    for model_name in self.model_libary:
-      model = self.model_library[model_name]
-      # TODO: try fitting and save statistics - we will have this information available for QO (Query Optimizer)
-      model.fit(X, Y)
+  def train_all(self, X, label_dict):
+    # label_dict = {"category_name": y}
+    self.preprocess(X, label_dict)
+    self.process(X, label_dict)
 
-      self.category_stats[category_name] = {model_name: {"reduction_rate": model.score(),
-                                                         "false_negative_rate": model.......,
-                                                         "time_to_train":}
+  #TODO: Need to find a way to allow processing models to incorporate preprocessing
+  def process(self, X, label_dict):
+    for model in self.model_library:
+      self.model_library[model]([X, label_dict])
 
-                                            self.category_library[category_name] = {model_name: model}
+
+  def preprocess(self, X, label_dict):
+    for model in self.pre_model_library:
+      self.pre_model_library[model]([X,label_dict])
+
+
+  def evaluate(self, X_test, label_dict):
+    """
+    self.category_stats[category_name] = {model_name: {"reduction_rate": model.score(),
+                                                       "false_negative_rate": model.......,
+                                                       "time_to_train":}
+    """
+    #TODO: need to include various categories in self.category_stats, but will only include the accuracy for now
+    #TODO: need an evaluation method that allows mix and match of processing and preprocessing
+    for category_name in self.category_library:
+      for model_name in self.category_library[category_name]:
+        model = self.category_library[category_name][model_name]
+        score = model.score()
+        if category_name not in self.category_stats:
+          self.category_stats[category_name] = {}
+        self.category_stats[category_name][model_name] = {"score": score}
+
+    return
+
+  #random forest
+  def rf(self, args):
+    X, label_dict = args
+    for label in label_dict:
+      rf = RandomForestClassifier(max_depth=2, random_state=0)
+      rf.fit(X, label_dict[label])
+      if label not in self.category_library:
+        self.category_library[label] = {}
+      self.category_library[label]['rf'] = rf
+
+
+  def dnn(self, args):
+    X, label_dict = args
+    for label in label_dict:
+      dnn = MLPClassifier(solver='lbfgs', alpha=1e-5,
+                          hidden_layer_sizes = (5, 2), random_state = 1)
+      dnn.fit(X, label_dict[label])
+      if label not in self.category_library:
+        self.category_library[label] = {}
+      self.category_library[label]['dnn'] = dnn
+    return
+
+  def svm(self, args):
+    X, label_dict = args
+    for label in label_dict:
+      svm = LinearSVC(random_state=0)
+      svm.fit(X, label_dict[label])
+      if label not in self.category_library:
+        self.category_library[label] = {}
+      self.category_library[label]['svm'] = svm
+    return
+
+  def kde(self, args):
+    X, label_dict = args
+    for label in label_dict:
+      kde = KernelDensityWrapper(kernel='gaussian', bandwidth=0.2)
+      # We will assume each label is one-shot encoding
+      kde.fit(X, label_dict[label])
+      if label not in self.category_library:
+        self.category_library[label] = {}
+      self.category_library[label]['kde'] = kde
+
+    return
+
+  def pca(self, args):
+    X, label_dict = args
+    nsamples, nx, ny, nc = X.shape
+    X = X.reshape((nsamples, nx * ny * nc))
+    pca = PCA()
+    X_new = pca.fit_tranform(X)
+    self.pre_results["pca"] = pca
+
+    return [X_new, label_dict] #we will return the models for the caller to save them
+
+
+  # According to paper, each pixel or blob of 8x8 can be mapped to a dimension
+  # We will first try to make each pixel a dimension
+  # It is told that if feature vector is dense, accuracy becomes worse
+  # Will not do for now...
+  def feature_hashing(self, args):
+    """
+    category_count = len(self.category_libary.keys())
+    if category_count < 2:
+      return
+
+    h = FeatureHasher(n_features=category_count)
+    D = [{'dog': 1, 'cat': 2, 'elephant': 4}, {'dog': 2, 'run': 5}]
+    f = h.transform(D)
+    f.toarray()
+    """
+    return args
+
+  def sampling(self, args):
+    pass
+
+  def none(self, args):
+    return args
+
 
   def getCategoryInfo(self, category_name):
     return self.category_stats[category_name]
@@ -73,54 +154,8 @@ class PP_gen:
   def getModelLibrary(self):
     return self.model_library.keys()
 
-  #TODO: need to perform feature hashing after all the categories are inputted??
-  def feature_hashing(self, args):
-    category_count = len(self.category_libary.keys())
-    if category_count < 2:
-      return
-
-    h = FeatureHasher(n_features=category_count)
-    D = []
-    for i in range(category_count):
-      D.append()
-    D = [{'dog': 1, 'cat': 2, 'elephant': 4}, {'dog': 2, 'run': 5}]
-    f = h.transform(D)
-    f.toarray()
-
-  def pca(self, args):
-    X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
-    pca = PCA(n_components=2)
-    pca.fit(X)
-    PCA(copy=True, iterated_power='auto', n_components=2, random_state=None,
-        svd_solver='auto', tol=0.0, whiten=False)
-
-    return pca #we will return the models for the caller to save them
 
 
-
-  def sampling(self, args):
-    pass
-
-  def svm(self, args):
-    pass
-
-  def dnn(self, args):
-    pass
-
-  def kde(self, args):
-    pass
-
-  def PP(clf, X_train, X_test, y_train, y_test):
-    train_start_time = time.time()
-    clf.fit(X_train, y_train)
-    print("train time: %.3f sec" % (time.time() - train_start_time))
-    test_start_time = time.time()
-    y_pred = clf.predict(X_test)
-    print("test time: %.3f sec" % (time.time() - test_start_time))
-    print ("Accuracy: %.3f" % clf.score(X_test, y_test))
-    unique, counts = np.unique(y_pred, return_counts=True)
-    print "Absolute reduction: ", counts[0], "/", len(y_test)
-    print ("reduction rate: %.3f" % (counts[0] * 1.0 / len(y_test)))
 
 
 
--- pipeline.py
+++ pipeline.py
@@ -1,4 +1,9 @@
 
+import time
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+
 
 import filters.load.Load as Load
 import filters.pp.PP as PP
@@ -7,31 +12,70 @@ import filters.pp.PP as PP
 
 
 
+
 #TODO: Fill this file in with the components loaded from other files
 class Pipeline:
-    """1. Load the dataset
-       2. Load the QO
-       3. Load the Filters
-       4. Load the Central Network (RFCNN, SVM for other labels etc)
-       5. Listen to Queries
-       6. Give back result"""
+  """1. Load the dataset
+     2. Load the QO
+     3. Load the Filters
+     4. Load the Central Network (RFCNN, SVM for other labels etc)
+     5. Listen to Queries
+     6. Give back result"""
 
 
 
-    def __init__(self):
-        load = Load()
-        pp = PP()
+  def __init__(self):
+    self.load = Load()
+    self.pp = PP()
 
 
+  # We have access to train and test dataset -> Used for finding the score and evaluation
+  def test(self):
+    start_time = time.time()
 
-    def run(self):
-        pass
+    data, label_dict = self.load.load_dataset()
+    #TODO: Split the dataset into train, val, test (val should be used for evaluating the pps
+    #TODO: (continued) -> Need to look at paper to make sure it is the correct way
+    X = pd.DataFrame(data)
+    for label in label_dict:
+      y = pd.Series(label_dict[label])
+      X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=42)
+      break
+
+    train_index = X_train.index.values
+    test_index = X_test.index.values
+    label_dict_train = {}
+    label_dict_test = {}
+    for label in label_dict:
+      label_dict_train[label] = label_dict[label][train_index]
+      label_dict_test[label] = label_dict[label][test_index]
 
 
+    print("--- Total Execution Time for loading the dataset: %.3f seconds ---" % (time.time() - start_time))
+
+    start_time = time.time()
+    self.pp.train_all(X_train, label_dict_train)
+    print("--- Total Execution Time for training the dataset : %.3f seconds ---" % (time.time() - start_time))
+
+    self.pp.evaluate(X_test, label_dict_test)
 
 
+    return
+
+  # Actual run of the pipeline
+  def run(self):
+    start_time = time.time()
+
+    data, label_dict = self.load.load_dataset()
+    print("--- Total Execution Time for loading the dataset: %.3f seconds ---" % (time.time() - start_time))
+
+    start_time = time.time()
+    self.pp.train_all(data, label_dict)
+    print("--- Total Execution Time for training the dataset : %.3f seconds ---" % (time.time() - start_time))
+
+    return
 
 if __name__ == "__main__":
     pipeline = Pipeline()
-    pipeline.run()
+    pipeline.test()
 
