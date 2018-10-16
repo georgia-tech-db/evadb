5349b72f1717039962adf9e97e502a21ea2cba79
Jaeho Bang <jaehob@andrew.cmu.edu>
2018-10-16 10:50:59 -0400

    Pushing this week's update; query optimizer template
    Former-commit-id: 4fe4514fa6486e263a6680e3f983ad59a124b903 [formerly 0624dc712974a16b6690724f991b96b6054374b0] [formerly a34d95139cf3dc0f42f74723bf2ce13a58d90784 [formerly 7a8683ce012dfccf62daca474428ce971ae68ef4]]
    Former-commit-id: 0fc3c0d35077ffd3fda0f58ffa228cc42abc3f8a [formerly e1c2f0f556672b457bcf2daedb1d415432cf5862]
    Former-commit-id: 94d9e149a8a8446870b4e9dd5a1b21faae6dcc1d

--- pipeline.py
+++ pipeline.py
@@ -3,6 +3,11 @@ import time
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+import cv2
+import os
+from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing.image import img_to_array, load_img, array_to_img, save_img
+
 
 import filters.load as load
 import filters.pp as pp
@@ -21,7 +26,7 @@ class Pipeline:
   def __init__(self):
     self.load = load.Load()
     self.pp = pp.PP()
-    self.qo = qo.QueryOptimizer()
+    #self.qo = qo.QueryOptimizer()
 
   # We have access to train and test dataset -> Used for finding the score and evaluation
   def filter_performance_test(self):
@@ -59,6 +64,24 @@ class Pipeline:
 
     category_stats = self.pp.evaluate(X_test, label_dict_test)
     print category_stats
+
+    xs = self.pp.predict(X_test[0:100], "car", "none/dnn", bool=False)
+    xs = xs.values
+    nsamples = xs.shape[0]
+    xs_reshaped = xs.reshape((nsamples, nx, ny, nc))
+    description_str = "label: " + "car\n" + "model: none/dnn\n"
+    self.save_image(xs_reshaped, description_str)
+
+    print "finished saving images..."
+    return
+
+  def save_image(self, xs, description):
+    project_path = os.path.dirname(os.path.abspath(__file__))
+    count = 100
+    for x in xs:
+      img1 = array_to_img(x)
+      save_img(path=os.path.join(project_path, 'examples', str(count) + '.jpg'), x=img1, file_format='jpeg')
+      count += 1
     return
 
   # Actual run of the pipeline
@@ -74,7 +97,29 @@ class Pipeline:
 
     return
 
+  def filter_output_test(self):
+    prefix = os.path.dirname(os.path.abspath(__file__))
+    folder = "data"
+    name = "ua_detrac"
+    self.input_path = os.path.join(prefix,folder, name, 'small-data', 'MVI_20011', 'img00001.jpg')
+    image_width = 960
+    image_height = 540
+    ratio = 12
+    image_width = int(image_width / ratio)
+    image_height = int(image_height / ratio)
+    channels = 3
+    X = np.ndarray(shape=(1, image_height, image_width, channels), dtype=np.float32)
+
+
+    img = load_img(self.input_path, target_size=(image_height, image_width))
+    X[0] = img_to_array(img)
+
+    img1 = array_to_img(X[0])
+    save_img(path=os.path.join(prefix, 'examples' , str(9999) + '.jpg'), x=img1, file_format='jpeg')
+
+
+
 if __name__ == "__main__":
     pipeline = Pipeline()
-    pipeline.test()
-
+    pipeline.filter_performance_test()
+    #pipeline.filter_output_test()
