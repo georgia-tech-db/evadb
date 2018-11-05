e1431aef860dbccfb74aab2a1532a8f82a0fa18d
Pramodith Ballapuram <pballapuram3@ephemeral-1.cc.gatech.edu>
2018-11-05 11:44:33 -0500

    Adding code to connect pipelines
    Former-commit-id: 2f8d31ba235d7794958945065210d378d9ba94da [formerly ba75c34cd5cfb73fe141ab45eed8c0f8ff8c7a7a] [formerly 74eef9aaf91445efc125bd1b38be72d069ec9b93 [formerly 3fcb2a5a9250f6e2187fd0f89a148ddce1d17091]]
    Former-commit-id: 281cca904e77ce4d2be8c1c7e9cc867f81ea5922 [formerly 0d0847f2142dfd2491ee73abd7be7c174bca1f88]
    Former-commit-id: 2956fd554d773a036cbf2560222b34b88dd0b170

--- filters/filter.py
+++ filters/filter.py
@@ -3,11 +3,12 @@ import os, sys
 import numpy as np
 from sklearn.svm import LinearSVC
 from sklearn.ensemble import RandomForestClassifier
-#import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import xml.etree.ElementTree as ET
-from scipy import ndimage
-from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+import cv2
+import sys
+sys.path.append("/home/pballapuram3/Eva")
+from faster_rcnn_pytorch.demo import accept_input_from_pp
 
 def load_dataset(input_path):
 	list_of_files = []
@@ -29,13 +30,15 @@ def load_dataset(input_path):
 	X = np.ndarray(shape=(len(list_of_files), image_height, image_width, channels),dtype=np.float32)
 	i = 0
 	for file in list_of_files:
-		img = load_img(file, target_size=(image_height, image_width))
-		X[i] = img_to_array(img)
+		img = cv2.imread(file)
+		img=cv2.resize(img,(image_width,image_height))
+		X[i] = np.asarray(img)
 		X[i] = (X[i] - 128)/128
 		i += 1
-		if (i%1000 == 0):
+		if (i%10000 == 0):
 			print ("Loaded %d images" %i)
-	return X, num_frames_list			
+	return X, num_frames_list
+
 def get_vehtype_labels(filter, label_path, num_frames_list):
 	y = []
 	for root, subdirs, files in os.walk(label_path):
@@ -45,6 +48,9 @@ def get_vehtype_labels(filter, label_path, num_frames_list):
 			tree = ET.parse(file_path)
 			tree_root = tree.getroot()
 			j = 1
+			#Frames without any vehicle detected don't have an entry in Annotations.xml, the code below first checks if the frame is in the annotation if not it sets
+			#the label of the frame to 0, else checks against the filter to set it to 1 or 0. Finally we check if there are any frames not covered at the end of the
+			#annotation file.
 			for frame in tree_root.iter('frame'):
 				if (int(frame.attrib['num'])!=j):
 					for t in range(0,int(frame.attrib['num'])-j):
@@ -66,12 +72,32 @@ def get_vehtype_labels(filter, label_path, num_frames_list):
 			i+=1
 	return y
 
+def PP(clf,X_train, X_test, y_train, y_test):
+	train_start_time = time.time()
+	clf.fit(X_train,y_train)
+	print("train time: %.3f sec" % (time.time() - train_start_time))
+	test_start_time = time.time()
+	y_pred = clf.predict(X_test)
+	print("test time: %.3f sec" % (time.time() - test_start_time))
+	print ("Accuracy: %.3f"%clf.score(X_test,y_test))
+	unique,counts = np.unique(y_pred,return_counts=True)
+	print("Absolute reduction: ",counts[0],"/",len(y_test))
+	print ("reduction rate: %.3f"%(counts[0]*1.0/len(y_test)))
+
+
+#function to pass the frames that the trained classifer labels as 1 (in the test set) to the udf.
+def pass_to_udf(test_pred,test_X):
+	if len(test_X.shape)!=4:
+		test_X=test_X.reshape(1,test_X.shape[0],test_X.shape[1],test_X.shape[2])
+	pos_frames=np.where(test_pred==1)
+	accept_input_from_pp(test_X[pos_frames])
+
 
 def main():
-	# label_path = '../dataset/DETRAC-Train-Annotations-XML/'
-	label_path = '../dataset/small-annotation/'
-	# input_path = '../dataset/DETRAC-train-data/Insight-MVT_Annotation_Train/'
-	input_path = '../dataset/DETRAC-train-data/small-data/'
+	label_path = '../dataset/DETRAC-Train-Annotations-XML/'
+	# label_path = '../dataset/small-annotation/'
+	input_path = '../dataset/DETRAC-train-data/Insight-MVT_Annotation_Train/'
+	# input_path = '../dataset/DETRAC-train-data/small-data/'
 	vehtype_filters = ['car','van','bus']
 	X, num_frames_list = load_dataset(input_path)
 	nsamples, nx, ny, nc = X.shape
@@ -81,37 +107,25 @@ def main():
 		print ("--------------------Working on %s filter--------------------"%item)
 		y = get_vehtype_labels(item, label_path, num_frames_list)
 		y = np.array(y)
-		unique,counts = np.unique(y,return_counts=True)
 		# print "number of ",item, unique[1],counts[1]
 		# print ("Loaded %d labels"%len(y))
-		X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=33)
+		X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.33, random_state=42)
+		unique,counts = np.unique(y_test,return_counts=True)
+		print ("No.of test frames without cars :",counts[0],"/",len(y_test))
+		########## Linear SVC ############
 		clf = LinearSVC(random_state=0)
 		print ("--- LinearSVC ---")
-		train_start_time = time.time()
-		clf.fit(X_train,y_train)
-		print("train time: %.3f sec" % (time.time() - train_start_time))
-		test_start_time = time.time()
-		y_pred = clf.predict(X_test)
-		print("test time: %.3f sec" % (time.time() - test_start_time))
-		print "Accuracy: ",clf.score(X_test,y_test)
-		unique,counts = np.unique(y_pred,return_counts=True)
-		print "Absolute reduction: ",counts[0],"/",len(y_test)
-		print "reduction rate: ", counts[0]*1.0/len(y_test)
+		PP(clf,X_train, X_test, y_train, y_test)
+		######### Random Forest ###########
 		clf = RandomForestClassifier(max_depth=2, random_state=0)
 		print ("--- RandomForest ---")
-		train_start_time = time.time()
-		clf.fit(X_train,y_train)
-		print("train time: %.3f sec" % (time.time() - train_start_time))
-		test_start_time = time.time()
-		y_pred = clf.predict(X_test)
-		print "Accuracy: ",clf.score(X_test,y_test)
-		unique,counts = np.unique(y_pred,return_counts=True)
-		print "Absolute reduction: ",counts[0],"/",len(y_test)
-		print "reduction rate: ", counts[0]*1.0/len(y_test)
-		print("test time: %.3f sec" % (time.time() - test_start_time))
-	# X = 
+		PP(clf,X_train, X_test, y_train, y_test)
+	# X =
 
 if __name__ == '__main__':
 	start_time = time.time()
-	main()
+	#main()
+	img=np.asarray(cv2.imread("img00001.jpg"))
+	print(img.shape)
+	pass_to_udf(np.asarray([1]),img)
 	print("--- Total Execution Time : %.3f seconds ---" % (time.time() - start_time))
---
+++ filters/pp.py
@@ -0,0 +1,166 @@
+#from kdewrapper import KernelDensityWrapper
+from sklearn.svm import LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction import FeatureHasher
+from sklearn.decomposition import PCA
+from sklearn.neural_network import MLPClassifier
+
+# Meant to be a black box for trying all models available and returning statistics and model for
+# the query optimizer to choose for a given query
+
+class PP:
+  def __init__(self):
+
+    self.model_library = {"kde": self.kde,
+                          "svm": self.svm,
+                          "dnn": self.dnn,
+                          "rf": self.rf} #KDE, SVM, NN - this should be a mapping of model name to model CONSTRUCTOR
+
+    self.pre_model_library = {"none": self.none,
+                              "pca": self.pca,
+                              #"fh": self.feature_hashing,
+                              # "sampling": self.sampling,
+                              } #feature hashing, PCA, None - Separated to do mix and match
+
+    #self.pre_results = {} #save the preprocessed results {"pre_model_name": reformed_data
+    self.category_library = {} #save the trained model
+    self.category_stats = {} #save the statistics related to the model, although most stats are embedded in the model,
+                             #made this just in case there could be stats that are not saved
+    self.pre_category_library = {}
+
+  def train_all(self, X, label_dict):
+    X_preprocessed = self.preprocess(X, label_dict)
+    self.process(X_preprocessed, label_dict)
+
+  def process(self, X, label_dict):
+    for process_method in X:
+      for model in self.model_library:
+        self.model_library[model]([X[process_method], label_dict, process_method])
+
+
+  def preprocess(self, X, label_dict):
+    X_preprocessed = {}
+    for model in self.pre_model_library:
+      X_preprocessed[model], _ = self.pre_model_library[model]([X,label_dict])
+    return X_preprocessed
+
+  def evaluate(self, X_test, label_dict):
+    """
+    self.category_stats[category_name] = {model_name: {"reduction_rate": model.score(),
+                                                       "false_negative_rate": model.......,
+                                                       "time_to_train":}
+    """
+    #TODO: need to include various categories in self.category_stats, but will only include the accuracy for now
+    for category_name in self.category_library:
+      for model_name in self.category_library[category_name]:
+        #We need to parse by "/" token and apply the proper preprocessing method
+        pre, pro = model_name.split("/")
+        X_pre, _ = self.pre_model_library[pre]([X_test, label_dict])
+        model = self.category_library[category_name][model_name]
+        score = model.score(X_pre, label_dict[category_name])
+        if category_name not in self.category_stats:
+          self.category_stats[category_name] = {}
+        self.category_stats[category_name][model_name] = {"score": score}
+
+    return self.category_stats
+
+  #random forest
+  def rf(self, args):
+    X, label_dict, pre = args
+    for label in label_dict:
+      rf = RandomForestClassifier(max_depth=2, random_state=0)
+      rf.fit(X, label_dict[label])
+      if label not in self.category_library:
+        self.category_library[label] = {}
+      self.category_library[label][pre + '/rf'] = rf
+
+
+  def dnn(self, args):
+    X, label_dict, pre = args
+    for label in label_dict:
+      dnn = MLPClassifier(solver='lbfgs', alpha=1e-5,
+                          hidden_layer_sizes = (5, 2), random_state = 1)
+      dnn.fit(X, label_dict[label])
+      if label not in self.category_library:
+        self.category_library[label] = {}
+      self.category_library[label][pre +'/dnn'] = dnn
+    return
+
+  def svm(self, args):
+    X, label_dict, pre = args
+    for label in label_dict:
+      svm = LinearSVC(random_state=0)
+      svm.fit(X, label_dict[label])
+      if label not in self.category_library:
+        self.category_library[label] = {}
+      self.category_library[label][pre + '/svm'] = svm
+    return
+
+  def kde(self, args):
+    X, label_dict, pre = args
+    for label in label_dict:
+      kde = KernelDensityWrapper(kernel='gaussian', bandwidth=0.2)
+      # We will assume each label is one-shot encoding
+      kde.fit(X, label_dict[label])
+      if label not in self.category_library:
+        self.category_library[label] = {}
+      self.category_library[label][pre + '/kde'] = kde
+
+    return
+
+  def pca(self, args):
+    X, label_dict,n_components = args
+    if "pca" not in self.pre_category_library:
+      pca = PCA(n_components=n_components)
+      X_new = pca.fit_transform(X)
+      self.pre_category_library["pca"] = pca
+    else:
+      pca = self.pre_category_library["pca"]
+      X_new = pca.transform(X)
+
+    return [X_new, label_dict] #we will return the models for the caller to save them
+
+
+  # According to paper, each pixel or blob of 8x8 can be mapped to a dimension
+  # We will first try to make each pixel a dimension
+  # It is told that if feature vector is dense, accuracy becomes worse
+  # Will not do for now...
+  def feature_hashing(self, args):
+    """
+    category_count = len(self.category_libary.keys())
+    if category_count < 2:
+      return
+
+    h = FeatureHasher(n_features=category_count)
+    D = [{'dog': 1, 'cat': 2, 'elephant': 4}, {'dog': 2, 'run': 5}]
+    f = h.transform(D)
+    f.toarray()
+    """
+    return args
+
+  def sampling(self, args):
+    pass
+
+  def none(self, args):
+    return args
+
+
+  def getCategoryInfo(self, category_name):
+    return self.category_stats[category_name]
+
+  def getCategoryModels(self, category_name):
+    return self.category_library[category_name]
+
+  # returns list of model names
+  def getModelLibrary(self):
+    return self.model_library.keys()
+
+
+
+
+
+
+
+
+
+
