ff47ee0e73d258b86dd570fb1a07a17a191ce6f4
Pramodith Ballapuram <pballapuram3@ephemeral-1.cc.gatech.edu>
2018-11-13 02:36:52 -0500

    Adding all the necessary files after integration.
    Former-commit-id: 6a017ae63afa70334ff10c17fa11ac4ac5d32078 [formerly bb153913159ff869bc577e3be039ae7f45cc4735] [formerly ceaa18f39e033ed67cd2d90c123605f9aa9f6d06 [formerly 03148619d210ad5409c041f6b88b89f3b301fa90]] [formerly f68efa40ce824cc70e21907970303195040eb222 [formerly 453277d3d02bd5791ae5089e32dc7edfb3aa5dc9] [formerly 7643f39d4dc94ff818763f6ac55be4aa81d303e8 [formerly 3d4cee9ea66a75ec1995021beff5f607bd15f9d3]]]
    Former-commit-id: fca7a9c98421ff67c75d7565ef27a6bba28867bd [formerly f6fa039832d5f899f263954d82aba0548182b185] [formerly f1384a96a0d9d64d8625e36fd9324a2c9771718f [formerly d3a3b8150866a526dc54eb8aa4ff76b83ead9add]]
    Former-commit-id: be28860534db9605c8473d80833a3c5584591a2b [formerly 757852d920885f9dee3bddd524aefebe2a7b7f4c]
    Former-commit-id: ca296d4d2d4cdb0753f72b4e9c9d15bdc8c249d4

---
+++ pipeline.py
@@ -0,0 +1,83 @@
+
+import time
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+import filters.load as load
+import filters.pp as pp
+
+
+#TODO: Fill this file in with the components loaded from other files
+class Pipeline:
+  """1. Load the dataset
+     2. Load the QO
+     3. Load the Filters
+     4. Load the Central Network (RFCNN, SVM for other labels etc)
+     5. Listen to Queries
+     6. Give back result"""
+
+
+
+  def __init__(self):
+    self.load = load.Load()
+    self.pp = pp.PP()
+
+
+  # We have access to train and test dataset -> Used for finding the score and evaluation
+  def test(self):
+    start_time = time.time()
+
+    data, label_dict = self.load.load_dataset()
+    nsamples, nx, ny, nc = data.shape
+    data = data.reshape((nsamples, nx * ny * nc))
+
+    #TODO: Split the dataset into train, val, test (val should be used for evaluating the pps
+    #TODO: (continued) -> Need to look at paper to make sure it is the correct way
+    X = pd.DataFrame(data)
+    for label in label_dict:
+      y = pd.Series(label_dict[label])
+      X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=42)
+      break
+
+    train_index = X_train.index.values
+    test_index = X_test.index.values
+    label_dict_train = {}
+    label_dict_test = {}
+    for label in label_dict:
+      label_dict_train[label] = label_dict[label][train_index]
+      label_dict_test[label] = label_dict[label][test_index]
+
+
+    print("--- Total Execution Time for loading the dataset: %.3f seconds ---" % (time.time() - start_time))
+
+    start_time = time.time()
+    self.pp.train_all(X_train, label_dict_train)
+    print("--- Total Execution Time for training the dataset : %.3f seconds ---" % (time.time() - start_time))
+    if __debug__:
+      print X_train.shape
+      print X_test.shape
+
+    category_stats = self.pp.evaluate(X_test, label_dict_test)
+    print category_stats
+
+
+    return
+
+  # Actual run of the pipeline
+  def run(self):
+    start_time = time.time()
+
+    data, label_dict = self.load.load_dataset()
+    print("--- Total Execution Time for loading the dataset: %.3f seconds ---" % (time.time() - start_time))
+
+    start_time = time.time()
+    self.pp.train_all(data, label_dict)
+    print("--- Total Execution Time for training the dataset : %.3f seconds ---" % (time.time() - start_time))
+
+    return
+
+if __name__ == "__main__":
+    pipeline = Pipeline()
+    pipeline.test()
+
