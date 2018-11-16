961c22e0a4eb1aeacd802143c2cb5965a5096908
Pramodith Ballapuram <pballapuram3@ephemeral-1.cc.gatech.edu>
2018-11-16 03:51:38 -0500

    Changes to integrate intersection
    Former-commit-id: e74e0a83bb68f6f4d437e5eda1e88f6cd747f5d7

--- faster_rcnn_pytorch/lib/roi_data_layer/minibatch.py
+++ faster_rcnn_pytorch/lib/roi_data_layer/minibatch.py
@@ -44,6 +44,7 @@ def get_minibatch(roidb, num_classes):
   gt_boxes = np.empty((len(gt_inds), 5), dtype=np.float32)
   gt_boxes[:, 0:4] = roidb[0]['boxes'][gt_inds, :] * im_scales[0]
   gt_boxes[:, 4] = roidb[0]['gt_classes'][gt_inds]
+
   blobs['gt_boxes'] = gt_boxes
   blobs['im_info'] = np.array(
     [[im_blob.shape[1], im_blob.shape[2], im_scales[0]]],
