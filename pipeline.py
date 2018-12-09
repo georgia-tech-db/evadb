a128a3d8607f6147d349709f33cfdf1b78e0dc25
jaehobang <jaeho.bang@gmail.com>
2018-12-09 00:07:29 -0500

    Finished debugging loaders, now also has a save function which exports a panda dataframe to csv file; now need to move onto filters
    Former-commit-id: dfefb8a743818296ac7745d434ecae029619528a [formerly e97013e94d79f10bad13d80e880913c9eebe1e4b] [formerly e816622d9087ac1ff66e1da9d15444c58d8b8f89 [formerly 42ecbe444d4e23148ee51aeda27fd139e77acd4d]] [formerly bc1ffa0cb9ddd1a3b6d3771fb016fa974a32b5c7 [formerly 202efa6900aebcdc6a178f984618857825a389d6] [formerly f59b14cdc17224d0bf97cefe126a4c7f1e7d1caa [formerly ae079e1d23998813bea70bbdb915a3a9f78204eb]]]
    Former-commit-id: fe67e9d4f918abe56d4d5b25b00b76af7c01eeeb [formerly 33d1559d4e4c4246f89b832d012e7fe19b7cb6c0] [formerly 9f73d9b2a8070cce489b95358721baeaa21fda39 [formerly 02a5ccf98ae28a9a1b14d9b7d0cbff9957b3fa5d]]
    Former-commit-id: 210cf59faf3d08c75302bc39dac8ea0fadaea4f2 [formerly 21b8ad917c724ed1d1a495863318700c5d7c7d88]
    Former-commit-id: f0bfce9b8d74670a0930d8dc95845e77d154413a

--- pipeline.py
+++ pipeline.py
@@ -4,11 +4,21 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 import os
+import sys
 from keras.preprocessing.image import img_to_array, load_img, array_to_img, save_img
+from faster_rcnn_pytorch.demo import accept_input_from_pp
 
 
 import loaders.load as load
 import filters.pp as pp
+import query_optimizer.query_optimizer as qo
+
+try:
+  import constants
+except:
+  sys.path.append("/nethome/jbang36/eva")
+  sys.path.append("/home/jaeho-linux/fall2018/DDL/Eva")
+  import constants
 
 
 #TODO: Fill this file in with the components loaded from other files
@@ -23,6 +33,8 @@ class Pipeline:
   def __init__(self):
     self.LOAD = load.Load()
     self.PP = pp.PP()
+    self.QO = qo.QueryOptimizer()
+    self.data_table = None
     #self.qo = qo.QueryOptimizer()
 
   def load(self):
@@ -30,15 +42,119 @@ class Pipeline:
     train_image_dir = os.path.join(eva_dir, "data", "ua_detrac", "train_images")
     test_image_dir = os.path.join(eva_dir, "data", "ua_detrac", "test_images")
     train_anno_dir = os.path.join(eva_dir, "data", "ua_detrac", "train_annotations")
-    labels_of_interest = ["vehicle_type", "color", "speed", "intersection"]
+
+
     dir_dict = {"train_image": train_image_dir,
                 "test_image": test_image_dir,
                 "train_anno": train_anno_dir}
-    data_table = self.LOAD.load(dir_dict, labels_of_interest)
+    data_table = self.LOAD.load(dir_dict)
     return data_table
 
+  def run(self):
+    self.data_table = self.load()
+    self.train()
+
+  def pass_to_udf(self, test_pred, test_X):
+    if len(test_X.shape) != 4:
+      test_X = test_X.reshape(1, test_X.shape[0], test_X.shape[1], test_X.shape[2])
+    pos_frames = np.where(test_pred == 1)
+    accept_input_from_pp(test_X[pos_frames])
+
   def train(self):
-    pass
+    """
+    Need to train the PPs and UDF
+    :return: trained PPs and UDF
+    """
+    labels_list = ["image", "vehicle_type", "color", "speed", "intersection"]
+    label_of_interest = "vehicle_type"
+    data_series = self.data_table[label_of_interest]
+
+    self.pp.train_all(self.data_table) #TODO: Need to fix this function
+    #TODO: train UDF - but for now assume it is already trained
+
+
+
+  def execute(self):
+    TRAF_20 = ["t=suv", "s>60",
+                "c=white", "c!=white", "o=pt211", "c=white && t=suv",
+                "s>60 && s<65", "t=sedan || t=truck", "i=pt335 && o=pt211",
+                "t=suv && c!=white", "c=white && t!=suv && t!=van",
+                "t=van && s>60 && s<65", "t=sedan || t=truck && c!=white",
+                "i=pt335 && o!=pt211 && o!=pt208", "t=van && i=pt335 && o=pt211",
+                "t!=sedan && c!=black && c!=silver && t!=truck",
+                "t=van && s>60 && s<65 && o=pt211", "t!=suv && t!=van && c!=red && t!=white",
+                "i=pt335 || i=pt342 && o!=pt211 && o!=pt208",
+                "i=pt335 && o=pt211 && t=van && c=red"]
+
+    synthetic_pp_list = ["t=car", "t=bus", "t=van", "t=others",
+                         "c=red", "c=white", "c=black", "c=silver",
+                         "s>40", "s>50", "s>60", "s<65", "s<70",
+                         "i=pt335", "i=pt211", "i=pt342", "i=pt208",
+                         "o=pt335", "o=pt211", "o=pt342", "o=pt208"]
+
+    load_labels = ["car", "bus", "van", "others",
+                   "red", "white", "black","silver",
+                   ">40", ">50", ">60", "<65", "<70",
+                   "pt335", "pt211", "pt342", "pt208"]
+
+    synthetic_pp_stats = {"t=van": {"none/dnn": {"R": 0.1, "C": 0.1, "A": 0.9},
+                                    "pca/dnn": {"R": 0.2, "C": 0.15, "A": 0.92},
+                                    "none/kde": {"R": 0.15, "C": 0.05, "A": 0.95}},
+                          "t=suv": {"none/svm": {"R": 0.13, "C": 0.01, "A": 0.95}},
+                          "t=sedan": {"none/svm": {"R": 0.21, "C": 0.01, "A": 0.94}},
+                          "t=truck": {"none/svm": {"R": 0.05, "C": 0.01, "A": 0.99}},
+
+                          "c=red": {"none/svm": {"R": 0.131, "C": 0.011, "A": 0.951}},
+                          "c=white": {"none/svm": {"R": 0.212, "C": 0.012, "A": 0.942}},
+                          "c=black": {"none/svm": {"R": 0.133, "C": 0.013, "A": 0.953}},
+                          "c=silver": {"none/svm": {"R": 0.214, "C": 0.014, "A": 0.944}},
+
+                          "s>40": {"none/svm": {"R": 0.08, "C": 0.20, "A": 0.8}},
+                          "s>50": {"none/svm": {"R": 0.10, "C": 0.20, "A": 0.82}},
+
+                          "s>60": {"none/dnn": {"R": 0.12, "C": 0.21, "A": 0.87},
+                                   "none/kde": {"R": 0.15, "C": 0.06, "A": 0.96}},
+
+                          "s<65": {"none/svm": {"R": 0.05, "C": 0.20, "A": 0.8}},
+                          "s<70": {"none/svm": {"R": 0.02, "C": 0.20, "A": 0.9}},
+
+                          "o=pt211": {"none/dnn": {"R": 0.135, "C": 0.324, "A": 0.993},
+                                      "none/kde": {"R": 0.143, "C": 0.123, "A": 0.932}},
+
+                          "o=pt335": {"none/dnn": {"R": 0.134, "C": 0.324, "A": 0.994},
+                                      "none/kde": {"R": 0.144, "C": 0.124, "A": 0.934}},
+
+                          "o=pt342": {"none/dnn": {"R": 0.135, "C": 0.325, "A": 0.995},
+                                      "none/kde": {"R": 0.145, "C": 0.125, "A": 0.935}},
+
+                          "o=pt208": {"none/dnn": {"R": 0.136, "C": 0.326, "A": 0.996},
+                                      "none/kde": {"R": 0.146, "C": 0.126, "A": 0.936}},
+
+                          "i=pt211": {"none/dnn": {"R": 0.135, "C": 0.324, "A": 0.993},
+                                      "none/kde": {"R": 0.143, "C": 0.123, "A": 0.932}},
+
+                          "i=pt335": {"none/dnn": {"R": 0.134, "C": 0.324, "A": 0.994},
+                                      "none/kde": {"R": 0.144, "C": 0.124, "A": 0.934}},
+
+                          "i=pt342": {"none/dnn": {"R": 0.135, "C": 0.325, "A": 0.995},
+                                      "none/kde": {"R": 0.145, "C": 0.125, "A": 0.935}},
+
+                          "i=pt208": {"none/dnn": {"R": 0.136, "C": 0.326, "A": 0.996},
+                                      "none/kde": {"R": 0.146, "C": 0.126, "A": 0.936}}}
+
+    label_desc = {"t": [constants.DISCRETE, ["sedan", "suv", "truck", "van"]],
+                  "s": [constants.CONTINUOUS, [40, 50, 60, 65, 70]],
+                  "c": [constants.DISCRETE, ["white", "red", "black", "silver"]],
+                  "i": [constants.DISCRETE, ["pt335", "pt342", "pt211", "pt208"]],
+                  "o": [constants.DISCRETE, ["pt335", "pt342", "pt211", "pt208"]]}
+
+
+    for query in TRAF_20:
+      qo.run(query, synthetic_pp_list, synthetic_pp_stats, label_desc)
+
+    self.pass_to_udf(np.asarray([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]), img)
+
+
 
   """
   # We have access to train and test dataset -> Used for finding the score and evaluation
@@ -99,20 +215,8 @@ class Pipeline:
       count += 1
     return
 
-  """
-  # Actual run of the pipeline
-  def run(self):
-    start_time = time.time()
 
-    data, label_dict = self.LOAD.load_dataset()
-    print("--- Total Execution Time for loading the dataset: %.3f seconds ---" % (time.time() - start_time))
 
-    start_time = time.time()
-    self.pp.train_all(data, label_dict)
-    print("--- Total Execution Time for training the dataset : %.3f seconds ---" % (time.time() - start_time))
-
-    return
-  """
   def filter_output_test(self):
     prefix = os.path.dirname(os.path.abspath(__file__))
     folder = "data"
@@ -138,7 +242,7 @@ class Pipeline:
 
 if __name__ == "__main__":
     pipeline = Pipeline()
-    pipeline.test()
+    pipeline.run()
 
 
     # the actual pipeline will be train -> execute
