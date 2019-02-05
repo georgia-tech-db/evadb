de8d67bf3d192d4b0e00fcdf72242e230ca172fa
Jaeho Bang <jaehob@andrew.cmu.edu>
2019-02-05 00:17:39 -0500

    Uploading loaders and clusters

--- clusters/k_means.py
+++ clusters/k_means.py
@@ -9,15 +9,16 @@ by Jaeho Bang
 # Import necessary libraries
 
 import os
+import sys
 import cv2
 import numpy as np
-from copy import deepcopy
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-import matplotlib
-from loaders import load
-matplotlib.use('Agg')
-from matplotlib import pyplot as plt
+import random
+import time
 
+from scipy.cluster.hierarchy import fclusterdata
+import matplotlib
+sys.path.append("/nethome/jbang36/eva")
+from loaders.load import Load
 
 '''
 Distance function between any frame in the video is defined as follows
@@ -50,140 +51,104 @@ class DistanceCalculator:
 
 
   def __init__(self):
-    self.frames = []
+    self.frames = None
 
 
-  def feed_frames(self, frames):
+  def feed_frames(self, frames, is_grayscale):
     self.frames = frames
-    self._calc()
 
 
-  def _calc(self):
-    """
-    Need to calculate...
-    t
-    rho
-    color_histograms of all f_m where m is between 0 and len(frames)
-    chi square distances between all f_m,n where m,n is between 0 and len(frames)
-
-    :return:
-    """
-    t = len(self.frames)
-    bucket_x = 8
-    bucket_y = 8
-    bucket_z = 8
-    color_hist = np.ndarry(shape=(len(self.frames),bucket_x, bucket_y, bucket_z))
-    for i in xrange(len(self.frames)):
-      color_hist[i] = cv2.calcHist([self.frames[i]], [0, 1, 2],
-                        None, [8, 8, 8], [0, 256, 0, 256, 0, 256])
-
-    chi = np.ndarray(shape=(len(self.frames, self.frames)))
-    w = np.ndarray(shape=(len(self.frames, self.frames)))
-    self.dist = np.ndarray(shape=(len(self.frames, self.frames)))
-    for i in xrange(len(self.frames)):
-      for j in xrange(i, len(self.frames)):
-
-        chi[i][j] = self.chi_squared(color_hist[i], color_hist[j])
-        chi[j][i] = self.chi[i][j]
-        w[i][j] = 1 * max(0, t - abs(i - j)) / t
-        w[j][i] = self.w[i][j]
-
-    rho = np.mean(chi)
-
-    for i in xrange(len(self.frames)):
-      for j in xrange(i, len(self.frames)):
-        self.dist[i][j] = 1 - w[i][j] * np.exp(-1 / rho * chi[i][j])
-        self.dist[j][i] = self.dist[i][j]
-
-    return
 
   def chi_squared(self, p, q):
     return 0.5 * np.sum((p - q) ** 2 / (p + q + 1e-6))
 
+
+  def get_dist(self, frame1, frame2):
+    t = len(self.frames)
+    bucket_x = 256
+
+    color_hist1, _ = np.histogram(frame1.ravel(), bucket_x, [0, 255])
+    color_hist2, _ = np.histogram(frame2.ravel(), bucket_x, [0, 255])
+
+    color_hist1 = np.array(color_hist1)
+    color_hist2 = np.array(color_hist2)
+
+    #print("QWERQWERQWERWER")
+    #print(color_hist1)
+    #print(color_hist2)
+
+    #print("ASDFASDFASDFASDFSA")
+    chi = self.chi_squared(color_hist1, color_hist2)
+    w = 1
+    rho = np.mean(chi)
+
+    if rho == 0:
+      rho = 0.0001
+
+    return 1 - w * np.exp(-1 / rho * chi)
+
+
+  """  
   def get_dist(self, m, n):
     return self.dist[m][n]
-
+  """
 
 class Kmeans:
 
+  def feed_frames(self, frames, is_grayscale = False):
+    #self.frames = frames.astype(np.int8)
+    self.frames = frames
+
+    self.frames = self.frames.reshape(n_samples, height*width*channels)
+
+    self.dc.feed_frames(self.frames, is_grayscale)
+
+
+
   def __init__(self):
 
+    self.dc = DistanceCalculator()
+    self.frames = None
+
+
+  def run(self):
     # Set three centers, the model should predict similar results
-    center_1 = np.array([1,1])
-    center_2 = np.array([5,5])
-    center_3 = np.array([8,1])
+    if self.frames is None:
+      print ("Frames have not been fed ... returning")
+      return
 
-    # Generate random data and center it to the three centers
-    data_1 = np.random.randn(200, 2) + center_1
-    data_2 = np.random.randn(200,2) + center_2
-    data_3 = np.random.randn(200,2) + center_3
+    # calculate the number of centers
+    n_samples, arr_size = self.frames.shape
+    fps = 20
+    centers = np.ndarray(shape=(n_samples / fps, arr_size))
 
-    data = np.concatenate((data_1, data_2, data_3), axis = 0)
-
-    plt.scatter(data[:,0], data[:,1], s=7)
-
-    # Number of clusters
-    k = 3
-    # Number of training data
-    n = data.shape[0]
-    # Number of features in the data
-    c = data.shape[1]
-
-    # Generate random centers, here we use sigma and mean to ensure it represent the whole data
-    mean = np.mean(data, axis = 0)
-    std = np.std(data, axis = 0)
-    centers = np.random.randn(k,c)*std + mean
-
-    # Plot the data and the centers generated as random
-    plt.scatter(data[:,0], data[:,1], s=7)
-    plt.savefig('fig3.png')
-
-    plt.scatter(centers[:,0], centers[:,1], marker='*', c='g', s=150)
-    plt.savefig('fig4.png')
+    for i in xrange(n_samples / fps):
+      centers[i] = np.random.randn(1, arr_size) * random.randint(10, 100)
 
 
-    centers_old = np.zeros(centers.shape)  # to store old centers
-    centers_new = deepcopy(centers)  # Store new centers
-
-    data.shape
-    clusters = np.zeros(n)
-    distances = np.zeros((n, k))
-
-    error = np.linalg.norm(centers_new - centers_old)
-
-    # When, after an update, the estimate of that center stays the same, exit loop
-    while error != 0:
-      # Measure the distance to every center
-      for i in range(k):
-        distances[:, i] = np.linalg.norm(data - centers[i], axis=1)
-      # Assign all training data to closest center
-      clusters = np.argmin(distances, axis=1)
-
-      centers_old = deepcopy(centers_new)
-      # Calculate mean for every cluster and update the center
-      for i in range(k):
-        centers_new[i] = np.mean(data[clusters == i], axis=0)
-      error = np.linalg.norm(centers_new - centers_old)
-    centers_new
-
-    # Plot the data and the centers generated as random
-    fig1 = plt.scatter(data[:,0], data[:,1], s=7)
-    plt.savefig('fig1.png')
-
-    fig2 = plt.scatter(centers_new[:,0], centers_new[:,1], marker='*', c='g', s=150)
-    plt.savefig('fig2.png')
-
+    # For now, we will use both clustering algorithms to see how effective each one is
+    # we will include print statments to see how long it takes
+    tic = time.time()
+    fclust1 = fclusterdata(self.frames, t=1.0, metric=self.dc.get_dist)
+    toc = time.time()
+    print("Time elapsed for custom distance function is %d", toc - tic)
+    fclust2 = fclusterdata(self.frames, t=1.0, metric='euclidean')
+    toc2 = time.time()
+    print("Time elasped for euclidean distance is %d", toc2 - toc)
 
 
 if __name__ == "__main__":
-  load = load.Load()
+  load = Load()
   eva_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
   image_dir = os.path.join(eva_dir, "data", "ua_detrac", "small-data")
 
-  img_table = load._load_images(image_dir)
+  #TODO: Need to make sure the load comes in grayscale
+  img_table = load.load_images_nn(image_dir, downsize_rate = 12, grayscale=True)
+  n_samples, height, width, channels = img_table.shape
+
+  kmeans_obj = Kmeans()
+  kmeans_obj.feed_frames(img_table, True)
+  kmeans_obj.run()
 
 
-  """
-  1. load the images from data/ua_detrac/small_data
-  2. make instances of Kmeans and DistanceCalculator
-  3. print out the stats"""
+
