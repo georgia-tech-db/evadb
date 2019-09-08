9b5e7f9953a09992683c18fa8808aec822f0541b
Jaeho Bang <jaehob@andrew.cmu.edu>
2019-09-08 00:31:58 -0400

    Built interface and adapter for old qo to new qo; Still needs a lot of work for functionality though
    Former-commit-id: 1b244b1e0f13e24e1e20297bd94fd0247c84cbf3

---
+++ query_optimizer/qo_template.py
@@ -0,0 +1,34 @@
+"""
+This file gives interface to all query optimizer modules
+If any issues arise please contact jaeho.bang@gmail.com
+
+@Jaeho Bang
+"""
+
+"""
+Initial Design Thoughts:
+Query Optimizer by definition should perform two tasks:
+1. analyze Structered Query Language
+2. Determine efficient execution mechanisms (plans)
+
+"""
+
+
+from abc import ABCMeta, abstractmethod
+
+
+class QOTemplate(metaclass=ABCMeta):
+
+
+  @abstractmethod
+  def executeQueries(self, queries:list)->list:
+    """
+    Query Optimizer by definition should perform two tasks:
+    1. Analyze given Structured Query Language (SQL)
+    2. Determine efficient execution mechanisms/plans
+    :param queries: input queries / query
+    :return: output plans / plan that can be understood by the system
+    """
+    pass
+
+
