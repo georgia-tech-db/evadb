e511884f51a1cb1995e7d88b385cd06308742c8f
kedi zheng <zhengkedi27@gmail.com>
2019-09-09 03:16:26 -0400

    added grammar project from CSC4420
    Former-commit-id: 50e1ec221b14d722497f41d2d20b077a6fb9782d [formerly 28d2f9d78a51ec03c1ca69768b4b007b81e38673] [formerly a0ed66ba65a427f7fb1d69458f496ff5a61a5641 [formerly 9c452410bbfffeb19f35d8ddbd5d69e211c0bbbb]] [formerly b476a102e33a17f4fd2c433dd0e51a96e5f3734a [formerly 584b0974c7cb2985496869061bb76a5eb51517b9] [formerly 11119f66c79e2c0efaea22263d89fbb6075372e1 [formerly 76a717ad0da4e9d15573ae2561c2297b9fb77147]]]
    Former-commit-id: 0daa3f65f6ac877a1be05ca9e21c8e002e26cc3e [formerly a3b1663ccc4265184e1faf377dcf6f656ddc0e1e] [formerly aeb667f3892456711a6fa009ba5a9aaca006b2c0 [formerly 5e8680d2ac81e0886dbe4d9db580f205af1b33ad]]
    Former-commit-id: 68e4afe5e54b8499437859f9aee12d40f91272f1 [formerly 942d5c83dca2cf95473d5eee2d49cb0430827c19]
    Former-commit-id: ce8ec5fdcc7229c82a0877bff4fe5324eb7a15b2

---
+++ Grammar/playGround/hello/Hello.g4
@@ -0,0 +1,4 @@
+grammar Hello; // Define a grammar called Hello
+r : 'hello' ID ; // match keyword hello followed by an identifier
+ID : [a-z]+ ; // match lower-case identifiers
+WS : [ \t\r\n]+ -> skip ; // skip spaces, tabs, newlines, \r (Windows)	
---
+++ Grammar/playGround/hello/HelloListener.java
@@ -0,0 +1,19 @@
+// Generated from Hello.g4 by ANTLR 4.7.1
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link HelloParser}.
+ */
+public interface HelloListener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link HelloParser#r}.
+	 * @param ctx the parse tree
+	 */
+	void enterR(HelloParser.RContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HelloParser#r}.
+	 * @param ctx the parse tree
+	 */
+	void exitR(HelloParser.RContext ctx);
+}
