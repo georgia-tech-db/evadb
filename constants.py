1aa46ffe90a0345d0389582310e04f07f82b17c5
Sanjana Garg <sanjana@cse.iitk.ac.in>
2019-09-15 20:19:27 -0400

    Formatted code according to PEP8 guidelines
    Former-commit-id: c910ca4774d7df00adb90511bc9a9fa76717bf57

--- constants.py
+++ constants.py
@@ -1,3 +1,4 @@
-# Used for query optimizer wrangler step DISCRETE means whether it is a discrete label
+# Used for query optimizer wrangler step DISCRETE means whether it is a
+# discrete label
 DISCRETE = 0
 CONTINUOUS = 1
