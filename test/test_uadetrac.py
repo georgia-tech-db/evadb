330c138b652458b9b45ccc095b4e20315642552f
Sanjana Garg <garg.sanjana.95@gmail.com>
2019-10-13 03:04:56 -0400

    Fixed test
    Former-commit-id: 76967dc7498b4889ac5bcdc09e5c9031ebb72941

--- test/test_uadetrac.py
+++ test/test_uadetrac.py
@@ -1,13 +1,18 @@
+import os
 import sys
+
+root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 try:
     from src.loaders.uadetrac_loader import UADetracLoader
 except ImportError:
-    sys.path.append('../')
+    sys.path.append(root)
+    from src.loaders.uadetrac_loader import UADetracLoader
 
 
 def test():
-    image_dir = "./data/small-data"
+    image_dir = root + "/test/data/small-data"
+    print(image_dir)
     loader = UADetracLoader(None, 400, 400)
     loader.load_images(image_dir)
     assert loader.images.shape == (9, 400, 400, 3)
