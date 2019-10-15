a486de038b7cd9a8fd969f2a4789be896373d660
Prashanth Dintyala <saiprashanth173@gmail.com>
2019-10-15 17:41:56 -0400

    Add building blocks for query processor
    Former-commit-id: 0d319fdb1d8cd233b1106b6629dac88bc676cb90 [formerly d830f23dc381c3607ee04361b2dcca7afc8dfde2] [formerly ac12ea8e6fa347a3dfaa25b5865a12f9cdac2052 [formerly 539d1d08ef5028458e49119517f182b4e269d805]] [formerly 1c8877e9f22192024f9f4673f6a1b8a6732d2f78 [formerly 2849523e3fe63f6ebe4c8970f983dba2fd023da7] [formerly 5d9aac8797afef92f2d70087f91e416bf8048aa2 [formerly 5d9aac8797afef92f2d70087f91e416bf8048aa2 [formerly bf3eff2294f624066857d67ab4f77a956a69aa6a]]]]
    Former-commit-id: d48c0b1c6bb39eea72ce77a6a07cbfe15c079a2f [formerly 8e01bc0320a6eda51a816ef940a6958b6d55f53f] [formerly b0ad7f7ecfe39ed1af21df8f2e62309a2a384d00 [formerly 100c623a23b6dd755ee97386a6d2046a1edb80a2]]
    Former-commit-id: da141ac20e763da74f35211f6ad22a471a2461ce [formerly 4eab3f9be11c9de471dfb97923d687a54b14dc3c]
    Former-commit-id: 486b9b7c1815cf2181bb719ca731c12ddede296e
    Former-commit-id: cc38e630c7843c9cece3d4041befeeadb837f034

--- src/loaders/abstract_loader.py
+++ src/loaders/abstract_loader.py
@@ -6,6 +6,8 @@ If any issues arise, please email jaeho.bang@gmail.com
 """
 from abc import ABCMeta, abstractmethod
 
+from src.models import VideoMetaInfo
+
 
 class AbstractLoader(metaclass=ABCMeta):
 
@@ -23,4 +25,35 @@ class AbstractLoader(metaclass=ABCMeta):
 
     @abstractmethod
     def load_video(self, dir: str):
+        pass
+
+
+class AbstractVideoLoader(metaclass=ABCMeta):
+    """
+    Abstract class for defining video loader. All other video loaders use this abstract class.
+    Video loader are expected fetch the videos from storage and return the frames in an iterative manner.
+
+    Attributes:
+        video_metadata (eva.models.VideoMetaInfo): An object containing metadata of the video.
+        batch_size (int, optional): Number of frames to fetch in batch from video
+        skip_frames (int, optional): Number of frames to be skipped while fetching the video
+        offset (int, optional): Start frame location in video
+        limit (int, optional): Number of frames needed from the video
+    """
+
+    def __init__(self, video_metadata: VideoMetaInfo, batch_size=1, skip_frames=0, offset=None, limit=None):
+        self.video_metadata = video_metadata
+        self.batch_size = batch_size
+        self.skip_frames = skip_frames
+        self.offset = offset
+        self.limit = limit
+
+    @abstractmethod
+    def load(self):
+        """
+        This is a generator for loading the frames of a video. Uses the video metadata and other class arguments
+
+        Yields:
+        :obj: `eva.models.FrameBatch`: An object containing a batch of frames and frame specific metadata
+        """
         pass
