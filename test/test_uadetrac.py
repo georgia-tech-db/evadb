ead7b74325d1b2954c4ba90ba733f2b849fa2fb0
Sanjana Garg <garg.sanjana.95@gmail.com>
2019-10-17 00:07:00 -0400

    Added test for load annotations
    Former-commit-id: a68409ab38ccd44827e4652a30b172b9ead06b28

--- test/test_uadetrac.py
+++ test/test_uadetrac.py
@@ -2,6 +2,8 @@ import os
 import sys
 
 root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+image_dir = root + "/test/data/small-data"
+anno_dir = root + "/test/data/small-annotations"
 
 try:
     from src.loaders.uadetrac_loader import UADetracLoader
@@ -10,9 +12,29 @@ except ImportError:
     from src.loaders.uadetrac_loader import UADetracLoader
 
 
-def test():
-    image_dir = root + "/test/data/small-data"
-    print(image_dir)
+def test_load_images():
     loader = UADetracLoader(None, 400, 400)
     loader.load_images(image_dir)
     assert loader.images.shape == (9, 400, 400, 3)
+
+
+def test_load_annotations1():
+    loader = UADetracLoader(None, 400, 400)
+    labels = loader.load_labels(anno_dir)
+    assert labels is None
+
+
+def test_load_annotations2():
+    loader = UADetracLoader(None, 400, 400)
+    loader.load_images(image_dir)
+    loader.load_labels(anno_dir)
+    labels = {
+        'vehicle': [['car', 'car'], ['car', 'car', 'car']],
+        'speed': [[6.859 * 5, 1.5055 * 5],
+                  [6.859 * 5, 1.5055 * 5, 0.5206 * 5]],
+        'color': [None, None],
+        'intersection': [None, None]
+    }
+    print(loader.labels)
+    print(labels)
+    assert labels == loader.labels
