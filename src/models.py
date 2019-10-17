aa5f0ff2631864411508e75943d4aa95a3d4d0f1
Prashanth Dintyala <saiprashanth173@gmail.com>
2019-10-17 12:24:42 -0400

    Add factory method for Prediction class

--- src/models.py
+++ src/models.py
@@ -224,10 +224,11 @@ class Prediction:
 
     """
 
-    def __init__(self, frame: Frame, labels: List[str], boxes: List[BoundingBox] = None):
+    def __init__(self, frame: Frame, labels: List[str], scores: List[float], boxes: List[BoundingBox] = None):
         self._boxes = boxes
         self._labels = labels
         self._frame = frame
+        self._scores = scores
 
     @property
     def boxes(self):
@@ -240,3 +241,41 @@ class Prediction:
     @property
     def frame(self):
         return self._frame
+
+    @property
+    def scores(self):
+        return self._scores
+
+    @staticmethod
+    def predictions_from_batch_and_lists(batch: FrameBatch, predictions: List[List[str]],
+                                         scores: List[List[float]], boxes: List[List[BoundingBox]] = None):
+
+        """
+        Factory method for returning a list of Prediction objects from identified values
+
+        Arguments:
+            batch (FrameBatch): frame batch for which the predictions belong to
+            predictions (List[List[str]]): List of prediction labels per frame in batch
+            scores (List[List[float]]): List of prediction scores per frame in batch
+            boxes (List[List[BoundingBox]]): List of bounding boxes associated with predictions
+
+        Returns:
+            List[Prediction]
+        """
+        assert len(batch.frames) == len(predictions)
+        assert len(batch.frames) == len(scores)
+        if boxes is not None:
+            assert len(batch.frames) == len(boxes)
+
+        predictions_ = []
+        for i in range(len(batch.frames)):
+            prediction_boxes = boxes[i] if boxes is not None else None
+            predictions_.append(Prediction(batch.frames[i], predictions[i], scores[i], boxes=prediction_boxes))
+
+        return predictions_
+
+    def __eq__(self, other):
+        return self.boxes == other.boxes and \
+               self.frame == other.frame and \
+               self.scores == other.scores and \
+               self.labels == other.labels
