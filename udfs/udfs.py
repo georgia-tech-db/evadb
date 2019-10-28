8ec59e78289772055aa18301c15daef491e2d4b2
galipremsagar <sagarprem75@gmail.com>
2019-10-28 10:04:04 -0400

    adding C, A, R to be returned.

--- udfs/udfs.py
+++ udfs/udfs.py
@@ -1,8 +1,10 @@
 import pickle
+
+
 class CarType():
     def __init__(self, value):
         self.type = value
         self.model = pickle.load('car_types_model')
 
     def process(self, data):
-        return self.model.predict(data)
+        return (self.model.predict(data), self.model.C, self.model.A, self.model.R)
