4fd648d24a7502ee7d3e98b2bd210805bb8a36a0
Eric Gastineau <36159000+dearthas@users.noreply.github.com>
2019-10-28 12:30:43 -0400

    New version
    This version handles more queries.
    
    Former-commit-id: 009c7c1fa279a49526de5732aa4e01abed1e08cd

---
+++ evaQL/build/Nodes/Node.py
@@ -0,0 +1,14 @@
+from abc import ABCMeta, abstractmethod
+
+class Node(metaclass=ABCMeta):
+    
+    @abstractmethod
+    def __init__(self, children=[]):
+        self.children = children
+
+    @abstractmethod
+    def processing(self):
+        pass
+
+
+
