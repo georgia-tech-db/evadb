9d911c09413a452840dbe1d6f192ce4e35f73eb9
Rishabh Bhardwaj <rbnext29@gmail.com>
2019-10-28 13:47:38 -0400

    Adding custom udfs for classes

--- udfs/udfs.py
+++ udfs/udfs.py
@@ -1,10 +1,28 @@
 import pickle
 
 
-class CarType():
+class CarType:
     def __init__(self, value):
         self.type = value
-        self.model = pickle.load('car_types_model')
+        self.model = pickle.load('car_types_model.pkl')
 
     def process(self, data):
-        return (self.model.predict(data), self.model.C, self.model.A, self.model.R)
+        return self.model.predict(data), self.model.C, self.model.A, self.model.R
+
+
+class ColorType:
+    def __init__(self, value):
+        self.type = value
+        self.model = pickle.load('color_types_model.pkl')
+
+    def process(self, data):
+        return self.model.predict(data), self.model.C, self.model.A, self.model.R
+
+
+class SpeedLimit:
+    def __init__(self, value):
+        self.type = value
+        self.model = pickle.load('speed_limit_model.pkl')
+
+    def process(self, data):
+        return self.model.predict(data), self.model.C, self.model.A, self.model.R
