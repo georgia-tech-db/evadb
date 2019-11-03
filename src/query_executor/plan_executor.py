ea36ba12d544a8e8c2a41db55c72ccc01fadfb62
GTK <gaurav21776@gmail.com>
2019-11-03 17:50:04 -0500

    Imports are all valid now

--- src/query_executor/plan_executor.py
+++ src/query_executor/plan_executor.py
@@ -1,6 +1,8 @@
+from src.query_planner.abstract_plan import AbstractPlan
+from src.query_executor.abstract_executor import AbstractExecutor
 # This is an interface between plan tree and execution tree.
 # We traverse the plan tree and build execution tree from it
-
+#select * from video where label = car
 class PlanExecutor():
     __init__(self, AbstractPlan : plan):
         self.__plan = plan
@@ -19,9 +21,8 @@ class PlanExecutor():
             return root
         
         ## Get plan node type
-
-        plan_node_type = 
-
+        plan_node_type = plan.get_node_type()
+        #ToDo recursively build execution tree based on node type
 
 
     def __clean_execution_tree(tree_root : AbstractExecutor):
@@ -30,7 +31,8 @@ class PlanExecutor():
         Arguments:
             tree_root {AbstractExecutor} -- root of execution tree to delete
         """
-
+        #ToDo
+        #clear all the nodes from the execution tree
     
     def execute_plan():
         """execute the plan tree
@@ -38,7 +40,7 @@ class PlanExecutor():
         """
         execution_tree = __build_execution_tree(self.__plan)
 
-        ### do to
+        ### ToDo
 
         __clean_execution_tree(execution_tree)
 
