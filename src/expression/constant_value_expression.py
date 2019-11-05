f7fde58e83aac30f3e5baf9fd5220acd850ab8b2
Prashanth Dintyala <saiprashanth173@gmail.com>
2019-11-04 21:26:06 -0500

    Refactor | fix more formatting issues
    Former-commit-id: ce9b22cce2451b0ff339ce8a0912f110b673453d

--- src/expression/constant_value_expression.py
+++ src/expression/constant_value_expression.py
@@ -1,10 +1,11 @@
 from .abstract_expression import AbstractExpression, ExpressionType
 
+
 class ConstantValueExpression(AbstractExpression):
-    #ToDo Implement generic value class
-    #for now we don't assign any class to value
-    #it can have types like string, int etc
-    #return type not set, handle that based on value
+    # ToDo Implement generic value class
+    # for now we don't assign any class to value
+    # it can have types like string, int etc
+    # return type not set, handle that based on value
     def __init__(self, value):
         super().__init__(ExpressionType.CONSTANT_VALUE)
         self._value = value
@@ -12,7 +13,5 @@ class ConstantValueExpression(AbstractExpression):
     def evaluate(self, *args):
         return self._value
 
-    #ToDo implement other functinalities like maintaining hash
-    #comparing two objects of this class(==)
-
-    
+    # ToDo implement other functinalities like maintaining hash
+    # comparing two objects of this class(==)
