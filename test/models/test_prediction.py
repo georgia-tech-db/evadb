61fe12eac962e466ee02aa9e8844d5b81f2771a1
Prashanth Dintyala <saiprashanth173@gmail.com>
2019-11-04 20:46:20 -0500

    Refactor | follow PEP-8 style

--- test/models/test_prediction.py
+++ test/models/test_prediction.py
@@ -14,14 +14,16 @@ class PredictionTest(unittest.TestCase):
         predictions = []
         scores = []
         self.assertRaises(AssertionError,
-                          lambda x=None: Prediction.predictions_from_batch_and_lists(batch, predictions, scores))
+                          lambda x=None: Prediction.predictions_from_batch_and_lists(
+                              batch, predictions, scores))
 
     def test_should_check_if_batch_frames_equivalent_to_number_of_scores(self):
         batch = FrameBatch(frames=[Frame(1, np.ones((1, 1)), None)], info=None)
         predictions = [['A', 'B']]
         scores = []
         self.assertRaises(AssertionError,
-                          lambda x=None: Prediction.predictions_from_batch_and_lists(batch, predictions, scores))
+                          lambda x=None: Prediction.predictions_from_batch_and_lists(
+                              batch, predictions, scores))
 
     def test_should_check_if_batch_frames_equivalent_to_number_of_boxes_if_given(self):
         batch = FrameBatch(frames=[Frame(1, np.ones((1, 1)), None)], info=None)
@@ -29,8 +31,9 @@ class PredictionTest(unittest.TestCase):
         scores = [[1, 1]]
         boxes = []
         self.assertRaises(AssertionError,
-                          lambda x=None: Prediction.predictions_from_batch_and_lists(batch, predictions, scores,
-                                                                                     boxes=boxes))
+                          lambda x=None: Prediction.predictions_from_batch_and_lists(
+                              batch, predictions, scores,
+                              boxes=boxes))
 
     def test_should_return_list_of_predictions_for_each_frame_in_batch(self):
         batch = FrameBatch(frames=[Frame(1, np.ones((1, 1)), None)], info=None)
@@ -38,5 +41,6 @@ class PredictionTest(unittest.TestCase):
         scores = [[1, 1]]
         expected = [Prediction(batch.frames[0], predictions[0], scores[0])]
         self.assertEqual(expected,
-                         Prediction.predictions_from_batch_and_lists(batch, predictions, scores
+                         Prediction.predictions_from_batch_and_lists(batch, predictions,
+                                                                     scores
                                                                      ))
