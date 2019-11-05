5e6b3e74cecc934e54804579fe07473d2c8731b9
Prashanth Dintyala <saiprashanth173@gmail.com>
2019-11-04 21:26:06 -0500

    Refactor | fix more formatting issues

--- src/query_planner/seq_scan_plan.py
+++ src/query_planner/seq_scan_plan.py
@@ -1,15 +1,18 @@
+from typing import List
 
-from query_planner.abstract_scan_plan import AbstractScan
+from src.expression.abstract_expression import AbstractExpression
+from src.loaders.abstract_loader import AbstractVideoLoader
+from src.query_planner.abstract_scan_plan import AbstractScan
 from src.query_planner.abstract_plan import PlanNodeType
+
+
 class SeqScanPlan(AbstractScan):
-    def __init__(self, predicate : Expression, video : Storage, column_ids : List[int]):
-        super(SeqScanPlan, self, predicate, video, column_ids).__init__()
+    def __init__(self, predicate: AbstractExpression,
+                 video: AbstractVideoLoader,
+                 column_ids: List[int]):
+        super().__init__(predicate, video, column_ids)
 
     def get_node_type(self):
         return PlanNodeType.SEQSCAN
 
-    #ToDo Add other functionality based on optimiser
-
-    
-
-    
+    # ToDo Add other functionality based on optimiser
