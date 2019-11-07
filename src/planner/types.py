a4ed589752572587ee4eab2724346575840ec761
Prashanth Dintyala <saiprashanth173@gmail.com>
2019-11-07 12:48:58 -0500

    Refactor | restructure code and fix styling issues
    Former-commit-id: 72010ea6d633e59c5f574c721f18a718e08fe59a

--- src/planner/types.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-
-class NodeType(Enum):
-    SEQUENTIAL_SCAN_TYPE = 1
-    PP_FILTER_TYPE = 2
--- test/filters/test_abstract_pp.py
+++ test/filters/test_abstract_pp.py
@@ -1,26 +1,28 @@
 import unittest
-import numpy as np
-from src.filters.abstract_pp import abstract_PP_filter_template
-from src.query_executor.pp_executor import PpExecutor
-from src.models import FrameBatch, Frame, Prediction, Predicate
-from src.filters.minimum_filter import FilterMinimum
 from unittest import mock
-from src.filters.abstract_pp import abstract_PP_filter_template
-class abstract_ppTest(unittest.TestCase):
+
+import numpy as np
+
+from src.filters.abstract_pp import AbstractPPTemplate
+from src.models import FrameBatch, Frame
 
 
-    @mock.patch.object(abstract_PP_filter_template, '_init_model')
+class AbstractPPTest(unittest.TestCase):
+
+    @mock.patch.object(AbstractPPTemplate, '_init_model')
     def test_mocking_instance(self, mocked_instance):
-        dummy = type("dummymodel", (), {"predict": lambda x: [True, True, False]})
+        dummy = type("dummymodel", (),
+                     {"predict": lambda x: [True, True, False]})
         mocked_instance.return_value = dummy
-        a = abstract_PP_filter_template("blabla")
+        a = AbstractPPTemplate("blabla")
 
-        frame_1 = Frame(1, np.ones((1,1)), None)
-        frame_2 = Frame(1, 2 * np.ones((1,1)), None)
+        frame_1 = Frame(1, np.ones((1, 1)), None)
+        frame_2 = Frame(1, 2 * np.ones((1, 1)), None)
         frame_3 = Frame(1, 3 * np.ones(((1, 1))), None)
         batch = FrameBatch(frames=[
             frame_1,
             frame_2,
             frame_3,
         ], info=None)
-        self.assertEqual(a.predict(batch), FrameBatch(frames=[frame_1, frame_2], info=None))
+        self.assertEqual(a.predict(batch),
+                         FrameBatch(frames=[frame_1, frame_2], info=None))
