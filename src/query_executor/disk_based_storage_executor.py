d2040bb10452b9c97056bca1d9dc0ab53f51ada2
Prashanth Dintyala <saiprashanth173@gmail.com>
2019-11-07 02:26:37 -0500

    Add plan and executor for storage
    Former-commit-id: 490bd09c10559a23488949ccd156a420e5101215 [formerly 93c1a2beb0b650ae25a4f8aaa70f353ce24e3d85] [formerly 6c65cd3ed6cba6199c42a75698ccc9877ab1b3bf [formerly 32db4adaee5b46618b2a7bfd9f34606db101d653]] [formerly d9885ebffafbc8bf2dbbd2d9777d246c842967ed [formerly 89f5b7ab78b2cdc65938254988061204295fb7ae] [formerly b9c5a57f2013859d9addbfc0ac93c2501b7ef718 [formerly b9c5a57f2013859d9addbfc0ac93c2501b7ef718 [formerly 62c818287eca016fb077dfadebdf03fcb87dcfe7]]]]
    Former-commit-id: 0675a757669af63c38d1aeada8bfe5a59bcf9252 [formerly 64273d7b9a88a584c034d022b4c5543aa6a04327] [formerly 72339dc3f677c117b20731bfd8e524a889efcc87 [formerly e0f19bad3223cf021b7cb16079726299744b4a14]]
    Former-commit-id: 63fc4716638ff0dff23652fa10751a574288d6eb [formerly 91954cf4b275eb2bce44f16c54429dc499991397]
    Former-commit-id: 97fd4ff16780cbb546b3e58865dc65ec2401f1ad
    Former-commit-id: 350ef7a03410c3eb2e36ddec7865e9fe37180fdd

---
+++ src/query_executor/disk_based_storage_executor.py
@@ -0,0 +1,28 @@
+from typing import Iterator
+
+from src.loaders.video_loader import SimpleVideoLoader
+from src.models import FrameBatch
+from src.query_executor.abstract_storage_executor import \
+    AbstractStorageExecutor
+from src.query_planner.storage_plan import StoragePlan
+
+
+class DiskStorageExecutor(AbstractStorageExecutor):
+    """
+    This is a simple disk based executor. It assumes that frames are
+    directly being read from the disk(video file).
+
+    Note: For a full fledged deployment this might be replaced with a
+    Transaction manager which keeps track of the frames.
+    """
+
+    def __init__(self, node: StoragePlan):
+        super().__init__(node)
+        self.storage = SimpleVideoLoader(node.video)
+
+    def validate(self):
+        pass
+
+    def next(self) -> Iterator[FrameBatch]:
+        for batch in self.storage.load():
+            yield batch
---
+++ test/query_executor/utils.py
@@ -0,0 +1,12 @@
+from typing import List
+
+from src.models import FrameBatch
+
+
+class DummyExecutor:
+    def __init__(self, batch_list: List[FrameBatch]):
+        self.batch_list = batch_list
+
+    def next(self):
+        for batch in self.batch_list:
+            yield batch
