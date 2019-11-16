9cdf5d0dae89f74f0ff10ed38f50440d6e011789
Vaishnavi Kannan <vaishnavik.22@gmail.com>
2019-11-15 20:32:47 -0500

    Fixed styling error in video_loader. Removed class from framediff_utils

--- src/utils/framediff_utils.py
+++ src/utils/framediff_utils.py
@@ -1,20 +1,16 @@
 import cv2
 
-class FrameDiffUtils:
-	"""
-	
-	Class with utility functions to calculate inter-frame difference
+"""
 
-	"""
+Utility functions to calculate inter-frame difference
 
-	def __init__(self):
-		pass
+"""
 
-	def convert_to_grayscale(self, frame):
-		return cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+def convert_to_grayscale(self, frame):
+	return cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-	def absolute_difference(self, curr_frame, prev_frame):
-		curr_frame_grayscale = convert_to_grayscale(curr_frame)
-		prev_frame_grayscale = convert_to_grayscale(prev_frame)
-		frame_diff = cv2.absdiff(curr_frame_grayscale, prev_frame_grayscale)
-		return frame_diff
+def absolute_difference(self, curr_frame, prev_frame):
+	curr_frame_grayscale = convert_to_grayscale(curr_frame)
+	prev_frame_grayscale = convert_to_grayscale(prev_frame)
+	frame_diff = cv2.absdiff(curr_frame_grayscale, prev_frame_grayscale)
+	return frame_diff
