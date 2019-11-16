7d2e70f71f0b50a1f22c18208b4309acc8701bde
Vaishnavi Kannan <vaishnavik.22@gmail.com>
2019-11-15 20:54:44 -0500

    Fixed errors in framediff_utils

--- src/utils/framediff_utils.py
+++ src/utils/framediff_utils.py
@@ -6,10 +6,10 @@ Utility functions to calculate inter-frame difference
 
 """
 
-def convert_to_grayscale(self, frame):
+def convert_to_grayscale(frame):
 	return cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-def absolute_difference(self, curr_frame, prev_frame):
+def absolute_difference(curr_frame, prev_frame):
 	curr_frame_grayscale = convert_to_grayscale(curr_frame)
 	prev_frame_grayscale = convert_to_grayscale(prev_frame)
 	frame_diff = cv2.absdiff(curr_frame_grayscale, prev_frame_grayscale)
