008a85d0e0e1b55c26cf45409c66e5df6eddd72d
galipremsagar <sagarprem75@gmail.com>
2019-11-17 15:53:17 -0500

    Added tests for logical expression
    Co-Authored-By: Rishabh <rbnext29@gmail.com>

--- test/expression/test_expression.py
+++ test/expression/test_expression.py
@@ -5,6 +5,7 @@ from src.expression.abstract_expression import ExpressionType
 from src.expression.comparison_expression import ComparisonExpression
 from src.expression.constant_value_expression import ConstantValueExpression
 from src.expression.tuple_value_expression import TupleValueExpression
+from src.expression.logical_expression import ExpressionLogical
 
 
 class ExpressionsTest(unittest.TestCase):
@@ -24,6 +25,18 @@ class ExpressionsTest(unittest.TestCase):
         # to fetch the tuple from table
         tuple1 = [1, 2, 3]
         self.assertEqual(True, cmpr_exp.evaluate(tuple1, None))
+
+    def test_logical_expression(self):
+        tpl_exp = TupleValueExpression(0)
+        const_exp = ConstantValueExpression(1)
+
+        cmpr_exp = ComparisonExpression(
+            ExpressionType.COMPARE_EQUAL,
+            tpl_exp,
+            const_exp
+        )
+        self.assertEqual(True, ExpressionLogical(cmpr_exp, "AND", cmpr_exp).evaluate())
+
 
 
 if __name__ == '__main__':
