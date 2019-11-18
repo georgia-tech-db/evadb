8107346f5fbf79faab1d0b1a0e8bee5807870f62
Asra Yousuf <asrayousuf28@gmail.com>
2019-11-18 01:03:21 -0800

    Adding logical operations
    Former-commit-id: 890c1046c9d8676ed5ca370732b34dd210cea869 [formerly 5f5f2b566664eee7fba8283aa59fe275381a25a0] [formerly e07f271712f6e650b0716bb7bad7f4ed8cd90d7b [formerly 8f63d0640514b675a3301e46ae9cf6cfd0c4f48a]] [formerly 82ee84bff7d33452d1246357ffb50f04318c7930 [formerly ec8b322d243815803318dd31025c4d9266626b40] [formerly 2bff69d2fa90fff3c3d37a111e6950932a30499c [formerly 1ecc00627f6d69c31f317700385a9ce989f00a18]]]
    Former-commit-id: c3d46086f389fe0ee32b2fe65b043bf9c3c81fc9 [formerly 0d2b76bf7df88a8c422c5098649059df13c96487] [formerly 0f62ae17992acd991305bc3d58d301adb16d9813 [formerly 7bb937a9ba65ced09642e3c30ec14d8a4cce1323]]
    Former-commit-id: 689cafe2a313342861ec4f86e1552831c6d6921d [formerly da71561ce221746d953775cca415c3cb9714f0dc]
    Former-commit-id: 77019d5774f11054ad464d53410bf605677afce6

---
+++ src/expression/logical_expression.py
@@ -0,0 +1,31 @@
+from .abstract_expression import AbstractExpression, ExpressionType, \
+    ExpressionReturnType
+
+
+class LogicalExpression(AbstractExpression):
+    def __init__(self, exp_type: ExpressionType, left: AbstractExpression,
+                 right: AbstractExpression):
+        children = []
+        if left is not None:
+            children.append(left)
+        if right is not None:
+            children.append(right)
+        super().__init__(exp_type, rtype=ExpressionReturnType.BOOLEAN,
+                         children=children)
+
+    def evaluate(self, *args):
+        # print(*args)
+        if len(self._children) == 2 :
+            vl = self._children[0].evaluate(*args)
+            vr = self._children[1].evaluate(*args)
+        else:
+            vr = self._children[0].evaluate(*args)
+
+
+        if (self.etype == ExpressionType.LOGICAL_AND):
+            return vl and vr
+        elif(self.etype == ExpressionType.LOGICAL_OR):
+            return vl or vr
+        elif(self.etype == ExpressionType.LOGICAL_NOT):
+            return (not vr)
+             
