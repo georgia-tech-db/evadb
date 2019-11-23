99d65bcede6e7551afd90b845dcd66e13f8604a7
Prashanth Dintyala <saiprashanth173@gmail.com>
2019-11-22 22:23:26 -0500

    Update the AbstractScanPlan

--- src/query_planner/seq_scan_plan.py
+++ src/query_planner/seq_scan_plan.py
@@ -6,9 +6,25 @@ from src.query_planner.types import PlanNodeType
 
 
 class SeqScanPlan(AbstractScan):
+    """
+    This plan is used for storing information required for sequential scan
+    operations.
+
+    Arguments:
+        predicate (AbstractExpression): A predicate expression used for
+        filtering frames
+
+        column_ids List[int]: List of columns which need to be selected
+        (Note: This attribute might be removed in future)
+    """
+
     def __init__(self, predicate: AbstractExpression,
                  column_ids: List[int] = None):
         if column_ids is None:
             column_ids = []
-        super().__init__(PlanNodeType.SEQUENTIAL_SCAN_TYPE, predicate,
-                         column_ids)
+        super().__init__(PlanNodeType.SEQUENTIAL_SCAN_TYPE, predicate)
+        self._column_ids = column_ids
+
+    @property
+    def column_ids(self) -> List:
+        return self._column_ids
