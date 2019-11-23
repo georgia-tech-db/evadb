b6174543071a6af8d75156849c8b275d6389abc0
Prashanth Dintyala <saiprashanth173@gmail.com>
2019-11-22 22:23:26 -0500

    Update the AbstractScanPlan
    Former-commit-id: da8160882629e4785da6d8e463ca1f38ceec8dcb [formerly 869e0683d7c36b6a01c36664112d8b76acb2f112] [formerly 10e3c41fb5b41c18ca567809aa9d6f0beb732f20 [formerly 5c6f87daef65b2bc2b2cd5177725da924e2dafec]] [formerly bda473544640ded9a641d4107c8a5899159fe238 [formerly ce6e8a638bf28dc3de540b277c8baffad95809ea] [formerly 9b751b36036dcfee3b91017c41094da9baa1908d [formerly 9b751b36036dcfee3b91017c41094da9baa1908d [formerly 99d65bcede6e7551afd90b845dcd66e13f8604a7]]]]
    Former-commit-id: 65873ac0cc8493c1792baef3424f63e0a29c178e [formerly 3b7f71c302016e3a93348c66880dccf0c47c24d8] [formerly 7697afbc7c84161779d465f682f05253be561c10 [formerly 7e395dbf2f90f7f75ac092a45cbfd57a42f0484e]]
    Former-commit-id: be8054b99e8ff9a7c6c5954945e53aef1636b9ca [formerly b4e841bb3cf4599c853bdf09d74d125ab1c53a04]
    Former-commit-id: 0d5114b5d473bc70a4665ddf8df610ab2f1cecaf
    Former-commit-id: fd63621efc00f1ac9760c4c62c346bd8e8b1c13a

--- src/query_planner/abstract_scan_plan.py
+++ src/query_planner/abstract_scan_plan.py
@@ -2,8 +2,8 @@
 https://www.postgresql.org/docs/9.1/using-explain.html
 https://www.postgresql.org/docs/9.5/runtime-config-query.html
 """
+from src.expression.abstract_expression import AbstractExpression
 from src.query_planner.abstract_plan import AbstractPlan
-from typing import List
 
 from src.query_planner.types import PlanNodeType
 
@@ -12,20 +12,13 @@ class AbstractScan(AbstractPlan):
     """Abstract class for all the scan based planners
     
     Arguments:
-        predicate : Expression 
-        columns_id :
+        predicate (AbstractExpression): An expression used for filtering
     """
 
-    def __init__(self, node_type: PlanNodeType, predicate: 'Expression',
-                 column_ids: List[int]):
+    def __init__(self, node_type: PlanNodeType, predicate: AbstractExpression):
         super(AbstractScan, self).__init__(node_type)
         self._predicate = predicate
-        self._column_ids = column_ids
 
     @property
     def predicate(self) -> 'Expression':
         return self._predicate
-
-    @property
-    def column_ids(self) -> List:
-        return self._column_ids
