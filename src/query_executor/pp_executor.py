babbb8ccba2553c06f6ad42ab1a3378fca169064
Prashanth Dintyala <saiprashanth173@gmail.com>
2019-11-22 22:27:07 -0500

    Update PPExecutor to follow common executor template

--- src/query_executor/pp_executor.py
+++ src/query_executor/pp_executor.py
@@ -1,18 +1,36 @@
+from typing import Iterator
+
 from src.models.storage.batch import FrameBatch
+from src.query_executor.abstract_executor import AbstractExecutor
+from src.query_planner.abstract_plan import AbstractPlan
+from src.query_planner.pp_plan import PPScanPlan
 
 
-class PPExecutor:
+class PPExecutor(AbstractExecutor):
     """
       Applies PP to filter out the frames that doesn't satisfy the condition
       Arguments:
           node (AbstractPlan): ...
-      """
 
-    def __init__(self, node: 'AbstractPlan'):
+    Note: This look kind of redundant. This logic for now is similar to that
+    of sequential scan executor. Will decide to delete it depending on how
+    sequential scan evolves.
+    """
+
+    def __init__(self, node: PPScanPlan):
         super().__init__(node)
-        self.PP_plan = node.PP_plan
         self.predicate = node.predicate
 
-    def execute(self, batch: FrameBatch):
-        filtered_frame = self.PP_plan.predict(batch)
-        return filtered_frame
+    def validate(self):
+        pass
+
+    def next(self) -> Iterator[FrameBatch]:
+        child_executor = self.children[0]
+        for batch in child_executor.next():
+            outcomes = self.predicate.evaluate(batch)
+            required_frame_ids = []
+            for i, outcome in enumerate(outcomes):
+                if outcome:
+                    required_frame_ids.append(i)
+
+            yield batch[required_frame_ids]
