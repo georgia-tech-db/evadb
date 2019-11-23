705bbc910681bb85372cd1e5a624795d2b801bbe
Prashanth Dintyala <saiprashanth173@gmail.com>
2019-11-22 22:37:30 -0500

    Update the PP abstract class

--- src/filters/abstract_pp.py
+++ src/filters/abstract_pp.py
@@ -1,15 +1,12 @@
-class AbstractPPTemplate:
-    def __init__(self, model_path):
-        self.model_path = model_path
-        self.model = self._init_model()
+from abc import ABC, abstractmethod
+from typing import List
 
-    def _init_model(self):
-        return None
 
-    def predict(self, batch):
-        predictions = self.model.predict(batch)
-        required_frame_ids = []
-        for i, prediction in enumerate(predictions):
-            if prediction:
-                required_frame_ids.append(i)
-        return batch[required_frame_ids]
+class AbstractPP(ABC):
+    """
+    An abstract class for PPs
+    """
+
+    @abstractmethod
+    def predict(self, batch) -> List[bool]:
+        pass
