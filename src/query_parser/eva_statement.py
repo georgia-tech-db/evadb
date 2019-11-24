2f2b9069ac32f00746f729322b6f75b8f4c96ba7
GTK <gaurav21776@gmail.com>
2019-11-23 20:41:13 -0500

    Code Refactored: Removed EvaStatements class and parser functions renamed

--- src/query_parser/eva_statement.py
+++ src/query_parser/eva_statement.py
@@ -1,16 +1,9 @@
-from enum import IntEnum, unique
+from src.query_parser.types import StatementType
 
 
-@unique
-class StatementType(IntEnum):
-    SELECT = 1,
-    INSERT = 2,
-
-    # add other types
-
-
-class EvaStatement():
-    """Base class for all the EvaStatement
+class EvaStatement:
+    """
+    Base class for all the EvaStatement
 
     Attributes
     ----------
@@ -24,29 +17,3 @@ class EvaStatement():
     @property
     def stmt_type(self):
         return self._stmt_type
-
-
-class EvaStatementList():
-    """Holds the list of statements that we are processing
-
-    Attributes
-    ----------
-    _eva_statement_list: list
-        list of evaStatements
-
-    """
-
-    def __init__(self):
-        self._eva_statement_list = []
-
-    def add_statement(self, statement: EvaStatement):
-        self._eva_statement_list.append(statement)
-
-    def __len__(self) -> int:
-        return len(self._eva_statement_list)
-
-    def get_statement(self, idx: int) -> EvaStatement:
-        if idx < len(self._eva_statement_list):
-            return self._eva_statement_list[idx]
-        else:
-            return IndexError
