36efa2d1408df7e9af3d154d082d4d4d260b3b9e
GTK <gaurav21776@gmail.com>
2019-11-23 20:41:44 -0500

    Parser test cases updated with table ref

--- test/query_parser/test_parser.py
+++ test/query_parser/test_parser.py
@@ -1,9 +1,10 @@
 import unittest
 from src.query_parser.eva_parser import EvaFrameQLParser
-from src.query_parser.eva_statement import EvaStatementList, EvaStatement
+from src.query_parser.eva_statement import EvaStatement
 from src.query_parser.eva_statement import StatementType
 from src.query_parser.select_statement import SelectStatement
 from src.expression.abstract_expression import ExpressionType
+from src.query_parser.table_ref import TableRef
 
 
 class ParserTest(unittest.TestCase):
@@ -11,7 +12,7 @@ class ParserTest(unittest.TestCase):
         super().__init__(*args, **kwargs)
 
     def test_eva_parser(self):
-        eva = EvaFrameQLParser()
+        parser = EvaFrameQLParser()
         single_queries = []
         single_queries.append("SELECT CLASS FROM TAIPAI;")
         single_queries.append("SELECT CLASS FROM TAIPAI WHERE CLASS = 'VAN';")
@@ -22,11 +23,11 @@ class ParserTest(unittest.TestCase):
         single_queries.append("SELECT CLASS FROM TAIPAI \
             WHERE (CLASS = 'VAN' AND REDNESS < 300 ) OR REDNESS > 500;")
         for query in single_queries:
-            eva_statement_list = eva.build_eva_parse_tree(query)
-            self.assertIsInstance(eva_statement_list, EvaStatementList)
+            eva_statement_list = parser.parse(query)
+            self.assertIsInstance(eva_statement_list, list)
             self.assertEqual(len(eva_statement_list), 1)
             self.assertIsInstance(
-                eva_statement_list.get_statement(0), EvaStatement)
+                eva_statement_list[0], EvaStatement)
 
         multiple_queries = []
         multiple_queries.append("SELECT CLASS FROM TAIPAI \
@@ -35,25 +36,24 @@ class ParserTest(unittest.TestCase):
             WHERE (CLASS = 'VAN' AND REDNESS = 300)")
 
         for query in multiple_queries:
-            eva_statement_list = eva.build_eva_parse_tree(query)
-            self.assertIsInstance(eva_statement_list, EvaStatementList)
+            eva_statement_list = parser.parse(query)
+            self.assertIsInstance(eva_statement_list, list)
             self.assertEqual(len(eva_statement_list), 2)
             self.assertIsInstance(
-                eva_statement_list.get_statement(0), EvaStatement)
+                eva_statement_list[0], EvaStatement)
             self.assertIsInstance(
-                eva_statement_list.get_statement(1), EvaStatement)
+                eva_statement_list[1], EvaStatement)
 
     def test_select_parser(self):
-        eva = EvaFrameQLParser()
+        parser = EvaFrameQLParser()
         select_query = "SELECT CLASS, REDNESS FROM TAIPAI \
             WHERE (CLASS = 'VAN' AND REDNESS < 300 ) OR REDNESS > 500;"
-        eva_statement_list = eva.build_eva_parse_tree(select_query)
-        self.assertIsInstance(eva_statement_list, EvaStatementList)
+        eva_statement_list = parser.parse(select_query)
+        self.assertIsInstance(eva_statement_list, list)
         self.assertEqual(len(eva_statement_list), 1)
-        self.assertEqual(eva_statement_list.get_statement(
-            0).stmt_type, StatementType.SELECT)
+        self.assertEqual(eva_statement_list[0].stmt_type, StatementType.SELECT)
 
-        select_stmt = eva_statement_list.get_statement(0)
+        select_stmt = eva_statement_list[0]
 
         # target List
         self.assertIsNotNone(select_stmt.target_list)
@@ -64,10 +64,10 @@ class ParserTest(unittest.TestCase):
             select_stmt.target_list[1].etype, ExpressionType.TUPLE_VALUE)
 
         # from_table
-        # Todo change this to table ref
         self.assertIsNotNone(select_stmt.from_table)
-        # self.assertEqual(select_stmt.from_table.etype,
-        #   ExpressionType.TUPLE_VALUE)
+        self.assertIsInstance(select_stmt.from_table, TableRef)
+        self.assertEqual(
+            select_stmt.from_table.table_info.table_name, 'TAIPAI')
 
         # where_clause
         self.assertIsNotNone(select_stmt.where_clause)
