60596c50948571c83b3f0bf671d851a87fbd4252
GTK <gaurav21776@gmail.com>
2019-11-23 20:41:13 -0500

    Code Refactored: Removed EvaStatements class and parser functions renamed
    Former-commit-id: 129370d3283c83842b6afef88ee317c28fe728d2 [formerly 948ab2e7085d9c06e269b9e018cc2be8207a7bb6] [formerly 8c70dbca408835232629ff68e181673602e98322 [formerly fb5c1d503985c0905fc780f0e26880e20438b268]] [formerly 648680e3c93f194208721f671836e0d02e13aa72 [formerly 348e92ce9ae62cfd738e3b5ca083c84d2ee6f52d] [formerly 9e7561ee9809f320d34be85aebb2e65a192e2c65 [formerly 2f2b9069ac32f00746f729322b6f75b8f4c96ba7]]]
    Former-commit-id: 997b8eb95767b9235dcee5e4f84c6d1b8b94d1bb [formerly af3c8fe72d56c7122241eecc6857cb394fdf4936] [formerly 30f5e46a095d371679783f397accb0972897ca36 [formerly 0fd6ef241662407cbc51c6b1a49d9747f8d0ebef]]
    Former-commit-id: b397c9cd7588a03d077fed969bfefcb73a71c232 [formerly 9925578ab4746d1816a7b289f6725f0533613dbb]
    Former-commit-id: a751d88c3f72c649758860baf62098321d803b7b

--- src/query_parser/eva_parser.py
+++ src/query_parser/eva_parser.py
@@ -1,17 +1,21 @@
 import sys
 from antlr4 import InputStream, CommonTokenStream
-from third_party.evaQL.parser.frameQLParser import frameQLParser 
+from third_party.evaQL.parser.frameQLParser import frameQLParser
 from third_party.evaQL.parser.frameQLLexer import frameQLLexer
 from src.query_parser.eva_ql_parser_visitor import EvaParserVisitor
-from src.query_parser.eva_statement import EvaStatementList
 
 
 class EvaFrameQLParser():
-    def build_eva_parse_tree(self, query_string: str) -> EvaStatementList:    
+    """
+    Parser for eva; based on frameQL grammar
+    """
+    def __init__(self):
+        self._visitor = EvaParserVisitor()
+    
+    def parse(self, query_string: str) -> list:    
         lexer = frameQLLexer(InputStream(query_string))
         stream = CommonTokenStream(lexer)
         parser = frameQLParser(stream)
         tree = parser.root()
-        visitor = EvaParserVisitor()
-        return visitor.visit(tree)
-        # print(tree.toStringTree(recog=parser))
+        return self._visitor.visit(tree)
+        
