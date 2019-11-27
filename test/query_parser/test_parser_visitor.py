8d074d602a4f782ccdc0898963e9bba14f6aade9
GTK <gaurav21776@gmail.com>
2019-11-27 02:40:31 -0500

    parser unit test cases added
    Former-commit-id: 526847470f18ba01f18372b7cec1872bddb8ded8 [formerly 9fc48c8bd18aebbf84ec9794f1f53de8b108f1be] [formerly d2bf894ef83eb74a6c8bd91859374120188e353f [formerly 715418a3af867a95b42f7128bba27278010ced78]] [formerly a1b6952e358421a832ec71fdcdb997cd0b4356ba [formerly d4fa59629b9fd7fe8495b92293b77a2ac6dd1c41] [formerly 77f40baafbdb40d8e27427ddf7f168f6b3d2f540 [formerly 77f40baafbdb40d8e27427ddf7f168f6b3d2f540 [formerly aae1ab2db839a58d33082080a50d7dbecbcd5675]]]]
    Former-commit-id: 29b893e6bd14c2af1d63baef70c915fb57e7624b [formerly 201786c78a6232ac59eb47760f8be581d97d9852] [formerly 43f0cbbde760a99e72bceeda46e0a7b01c8853e4 [formerly cd58b19f5e645894c19fddcad21306b49d7a1534]]
    Former-commit-id: 1c0b62e7959bc469d5d2f9b4016962ee279e7fda [formerly f0274f200862c9ca0276319e76dd229b0fc7354a]
    Former-commit-id: d176e0b4db98f2445c687a799bd841f901959866
    Former-commit-id: d2878808f2ab40d1564636cce9f6546d06cd4431

--- test/query_parser/test_parser_visitor.py
+++ test/query_parser/test_parser_visitor.py
@@ -1,11 +1,16 @@
 import unittest
-from unittest.mock import MagicMock, call
+from unittest import mock
+from unittest.mock import MagicMock, call, Mock
 
 from src.query_parser.eva_ql_parser_visitor import EvaParserVisitor
 from third_party.evaQL.parser.frameQLParser import frameQLParser
+from src.expression.abstract_expression import ExpressionType
 
 
 class ParserVisitorTest(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
     def test_should_query_specification_visitor(self):
         EvaParserVisitor.visit = MagicMock()
         mock_visit = EvaParserVisitor.visit
@@ -28,3 +33,65 @@ class ParserVisitorTest(unittest.TestCase):
         self.assertEqual(expected.from_table, "from")
         self.assertEqual(expected.where_clause, "where")
         self.assertEqual(expected.target_list, "target")
+
+    @mock.patch.object(EvaParserVisitor, 'visit')
+    def test_from_clause_visitor(self, mock_visit):
+        mock_visit.side_effect = ["from", "where"]
+
+        ctx = MagicMock()
+        tableSources = MagicMock()
+        ctx.tableSources.return_value = tableSources
+        whereExpr = MagicMock()
+        ctx.whereExpr = whereExpr
+
+        visitor = EvaParserVisitor()
+        expected = visitor.visitFromClause(ctx)
+        mock_visit.assert_has_calls([call(tableSources), call(whereExpr)])
+
+        self.assertEqual(expected.get('where'), 'where')
+        self.assertEqual(expected.get('from'), 'from')
+
+    def test_logical_operator(self):
+        ctx = MagicMock()
+        visitor = EvaParserVisitor()
+
+        self.assertEqual(
+            visitor.visitLogicalOperator(ctx),
+            ExpressionType.INVALID)
+
+        ctx.getText.return_value = 'OR'
+        self.assertEqual(
+            visitor.visitLogicalOperator(ctx),
+            ExpressionType.LOGICAL_OR)
+
+        ctx.getText.return_value = 'AND'
+        self.assertEqual(
+            visitor.visitLogicalOperator(ctx),
+            ExpressionType.LOGICAL_AND)
+
+    def test_comparison_operator(self):
+        ctx = MagicMock()
+        visitor = EvaParserVisitor()
+
+        self.assertEqual(
+            visitor.visitComparisonOperator(ctx),
+            ExpressionType.INVALID)
+
+        ctx.getText.return_value = '='
+        self.assertEqual(
+            visitor.visitComparisonOperator(ctx),
+            ExpressionType.COMPARE_EQUAL)
+
+        ctx.getText.return_value = '<'
+        self.assertEqual(
+            visitor.visitComparisonOperator(ctx),
+            ExpressionType.COMPARE_LESSER)
+
+        ctx.getText.return_value = '>'
+        self.assertEqual(
+            visitor.visitComparisonOperator(ctx),
+            ExpressionType.COMPARE_GREATER)
+
+
+if __name__ == '__main__':
+    unittest.main()
