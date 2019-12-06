9977863a0d92068ef05427a5f6a37550fa3a7625
Ali Lakdawala <amlk@gatech.edu>
2019-12-05 20:49:12 -0500

    Updated frame_info to account for compressed indx

--- src/models/catalog/frame_info.py
+++ src/models/catalog/frame_info.py
@@ -17,14 +17,22 @@ class FrameInfo:
         color_space (ColorSpace)(default: ColorSpace.RGB): color space of
         the frame (RGB, HSV, BGR, GRAY)
 
+        compressed(bool)(default: False): Flag to indicate whether video is 
+        compressed
+
+        original_index(int)(default: None): integer describing frame of 
+        original index 
+
     """
 
     def __init__(self, height=-1, width=-1, channels=3,
-                 color_space=ColorSpace.RGB):
+                 color_space=ColorSpace.RGB, compressed = False, original_index = None):
         self._color_space = color_space
         self._width = width
         self._height = height
         self._channels = channels
+        self._compressed = compressed
+        self._original_index = original_index
 
     @property
     def width(self):
@@ -42,8 +50,18 @@ class FrameInfo:
     def channels(self):
         return self._channels
 
+    @property
+    def compressed(self):
+        return self._compressed
+
+    @property
+    def original_index(self):
+        return self._original_index
+
     def __eq__(self, other):
         return self.color_space == other.color_space and \
                self.width == other.width and \
                self.height == other.height and \
-               self.channels == other.channels
+               self.channels == other.channels and \
+               self.compressed == other.compressed and \
+               self.original_index == other.original_index 
