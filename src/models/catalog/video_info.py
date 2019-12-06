1ad2331e6b07ccf1dcc6c9d85d1472ba5d77f14c
Ali Lakdawala <amlk@gatech.edu>
2019-12-05 20:39:46 -0500

    Updates to video_info to include rep vids

--- src/models/catalog/video_info.py
+++ src/models/catalog/video_info.py
@@ -13,10 +13,13 @@ class VideoMetaInfo:
 
     """
 
-    def __init__(self, file: str, fps: int, c_format: VideoFormat):
+    def __init__(self, file: str, fps: int, c_format: VideoFormat, compressed=False, compressed_index = None, original_path = None):
         self._fps = fps
         self._file = file
         self._c_format = c_format
+        self._compressed = compressed
+        self._compressed_index = compressed_index
+        self._original_path = original_path 
 
     @property
     def file(self) -> str:
@@ -29,3 +32,16 @@ class VideoMetaInfo:
     @property
     def c_format(self) -> VideoFormat:
         return self._c_format
+
+    @property
+    def compressed(self) -> bool:
+        return self._compressed
+
+    
+    @property
+    def compressed_index(self) -> list:
+        return self._compressed_index
+
+    @property
+    def original_path(self) -> str:
+        return self._original_path
