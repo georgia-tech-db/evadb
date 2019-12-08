baaf98469a657814ff1312342518ba086c5293fd
Paula Gluss <glusspaula@gmail.com>
2019-12-08 10:45:47 -0500

    Fixed formatting to match new unit test requirements

--- test/filters/test_minimum_filter.py
+++ test/filters/test_minimum_filter.py
@@ -2,34 +2,36 @@ from src.filters.minimum_filter import FilterMinimum
 from src.filters.models.ml_pca import MLPCA
 from src.filters.models.ml_dnn import MLMLP
 import numpy as np
+import unittest
 
+class FilterMinimum_Test(unittest.TestCase):
 
-def test_FilterMinimum():
-    # Construct the filter minimum and test it with randomized values
-    # The idea is just to run it and make sure that things run to completion
-    # No actual output or known inputs are tested
-    filter = FilterMinimum()
+    def test_FilterMinimum(self):
+        # Construct the filter minimum and test it with randomized values
+        # The idea is just to run it and make sure that things run to completion
+        # No actual output or known inputs are tested
+        filter = FilterMinimum()
 
-    # Set up the randomized input for testing
-    X = np.random.random([100, 30])
-    y = np.random.random([100])
-    y *= 10
-    y = y.astype(np.int32)
+        # Set up the randomized input for testing
+        X = np.random.random([100, 30])
+        y = np.random.random([100])
+        y *= 10
+        y = y.astype(np.int32)
 
-    # Split into training and testing data
-    division = int(X.shape[0] * 0.8)
-    X_train = X[:division]
-    X_test = X[division:]
-    y_iscar_train = y[:division]
-    y_iscar_test = y[division:]
+        # Split into training and testing data
+        division = int(X.shape[0] * 0.8)
+        X_train = X[:division]
+        X_test = X[division:]
+        y_iscar_train = y[:division]
+        y_iscar_test = y[division:]
 
-    filter.addPostModel("dnn", MLMLP())
-    filter.addPreModel("pca", MLPCA())
+        filter.addPostModel("dnn", MLMLP())
+        filter.addPreModel("pca", MLPCA())
 
-    filter.train(X_train, y_iscar_train)
-    y_iscar_hat = filter.predict(X_test, pre_model_name='pca', 
-                                 post_model_name='dnn')
-    stats = filter.getAllStats()
+        filter.train(X_train, y_iscar_train)
+        y_iscar_hat = filter.predict(X_test, pre_model_name='pca', 
+                                     post_model_name='dnn')
+        stats = filter.getAllStats()
 
-    filter.deletePostModel("dnn")
-    filter.deletePreModel("pca")
+        filter.deletePostModel("dnn")
+        filter.deletePreModel("pca")
--- test/filters/test_pp.py
+++ test/filters/test_pp.py
@@ -1,19 +1,21 @@
 import numpy as np
 from src.filters.pp import PP
+import unittest
 
+class PP_Test(unittest.TestCase):
 
-def test_PP():
-    pp = PP()
+    def test_PP(self):
+        pp = PP()
 
-    labels = ""
-    x = np.random.random([2, 30, 30, 3])
+        labels = ""
+        x = np.random.random([2, 30, 30, 3])
 
-    y = {
-        'vehicle': [['car', 'car'], ['car', 'car', 'car']],
-        'speed': [[6.859 * 5, 1.5055 * 5],
-                  [6.859 * 5, 1.5055 * 5, 0.5206 * 5]],
-        'color': [None, None],
-        'intersection': [None, None]
-    }
+        y = {
+            'vehicle': [['car', 'car'], ['car', 'car', 'car']],
+            'speed': [[6.859 * 5, 1.5055 * 5],
+                      [6.859 * 5, 1.5055 * 5, 0.5206 * 5]],
+            'color': [None, None],
+            'intersection': [None, None]
+        }
 
-    stats = pp.train_all(x, y)
+        stats = pp.train_all(x, y)
