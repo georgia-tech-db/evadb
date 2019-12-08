67ee20cd3ebf80d182e3269250ae355282535fc6
Paula Gluss <glusspaula@gmail.com>
2019-12-08 11:51:47 -0500

    Fixed formatting to pass requirements

--- test/udfs/vid_to_frame_classifier_test.py
+++ test/udfs/vid_to_frame_classifier_test.py
@@ -9,9 +9,7 @@ class VidToFrameClassifier_Test(unittest.TestCase):
 
     def test_VidToFrameClassifier(self):
         model = video_action_classification.VideoToFrameClassifier()
-        assert model != None
+        assert model is not None
         
         X = np.random.random([240, 320, 3])
-        model.classify(FrameBatch([Frame(0,X,None)],None))
-
-
+        model.classify(FrameBatch([Frame(0, X, None)], None))
