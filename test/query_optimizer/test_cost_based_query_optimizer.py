4751de0f3dd600bea052739cccc244fd7a029166
galipremsagar <sagarprem75@gmail.com>
2019-12-09 00:56:20 -0500

    added tests for logical plan

--- test/query_optimizer/test_cost_based_query_optimizer.py
+++ test/query_optimizer/test_cost_based_query_optimizer.py
@@ -1,13 +1,19 @@
 import unittest
 
-from src.query_optimizer.pp_optimizer import PPOptimizer
-
-from src import constants
+import src.constants as constants
 from src.catalog.catalog import Catalog
 from src.expression.abstract_expression import ExpressionType
 from src.expression.comparison_expression import ComparisonExpression
 from src.expression.constant_value_expression import ConstantValueExpression
 from src.expression.logical_expression import LogicalExpression
+from src.expression.tuple_value_expression import TupleValueExpression
+from src.models.catalog.properties import VideoFormat
+from src.models.catalog.video_info import VideoMetaInfo
+from src.query_optimizer.cost_based_optimizer import CostBasedOptimizer
+from src.query_optimizer.pp_optimizer import PPOptimizer
+from src.query_planner.logical_projection_plan import LogicalProjectionPlan
+from src.query_planner.logical_select_plan import LogicalSelectPlan
+from src.storage.video_loader import SimpleVideoLoader
 
 
 class PredicateExecutorTest(unittest.TestCase):
@@ -83,6 +89,119 @@ class PredicateExecutorTest(unittest.TestCase):
 
         self.assertEqual(out[0], child)
 
+    def test_logical_plan(self):
+        meta1 = VideoMetaInfo(file='v1', c_format=VideoFormat.MOV, fps=30)
+        video1 = SimpleVideoLoader(video_metadata=meta1)
+        meta2 = VideoMetaInfo(file='v2', c_format=VideoFormat.MOV, fps=30)
+        video2 = SimpleVideoLoader(video_metadata=meta2)
+
+        projection_output = ['c', 't']
+        root = LogicalProjectionPlan(videos=[video1, video2], column_ids=projection_output, foreign_column_ids=[])
+
+        tup = TupleValueExpression(col_idx=int(1))
+        tup.set_col_name("c")
+        const_exp_01 = ConstantValueExpression("t")
+        const_exp_02 = ConstantValueExpression("suv")
+
+        # t=suv
+        cmpr_exp1 = ComparisonExpression(
+            ExpressionType.COMPARE_EQUAL, const_exp_01, const_exp_02)
+
+        # c=white
+        const_exp_21 = ConstantValueExpression("c")
+        const_exp_22 = ConstantValueExpression("white")
+        cmpr_exp3 = ComparisonExpression(
+            ExpressionType.COMPARE_NEQ, const_exp_21, const_exp_22)
+
+        logical_expr2 = LogicalExpression(
+            cmpr_exp1, ExpressionType.LOGICAL_AND, cmpr_exp3)
+        expression = logical_expr2
+        s1 = LogicalSelectPlan(predicate=expression, column_ids=['c'], videos=[video1, video2],
+                               foreign_column_ids=[])
+        s1.parent = root
+        root.append_child(s1)
+        print(root)
+        cb_optimzer = CostBasedOptimizer(constants.UADETRAC)
+        test22 = cb_optimzer.execute(root)
+        print(test22)
+
+        # expected
+        a = ComparisonExpression(
+            ExpressionType.COMPARE_EQUAL,
+            const_exp_01,
+            const_exp_02)
+
+        const_exp_111 = ConstantValueExpression("c")
+        const_exp_122 = ConstantValueExpression("red")
+        red = ComparisonExpression(
+            ExpressionType.COMPARE_EQUAL,
+            const_exp_111,
+            const_exp_122)
+
+        const_exp_1111 = ConstantValueExpression("c")
+        const_exp_1222 = ConstantValueExpression("black")
+        black = ComparisonExpression(
+            ExpressionType.COMPARE_EQUAL,
+            const_exp_1111,
+            const_exp_1222)
+
+        child = LogicalExpression(red, ExpressionType.LOGICAL_AND, black)
+        silver = ComparisonExpression(
+            ExpressionType.COMPARE_EQUAL,
+            ConstantValueExpression("c"),
+            ConstantValueExpression("silver"))
+        logical_expr3 = LogicalExpression(
+            child, ExpressionType.LOGICAL_AND, silver)
+        expected_result = LogicalExpression(
+            a, ExpressionType.LOGICAL_AND, logical_expr3)
+
+        self.assertEqual(expected_result, test22.children[0]._predicate)
+
+    def test_same_query_logical_plan(self):
+        meta1 = VideoMetaInfo(file='v1', c_format=VideoFormat.MOV, fps=30)
+        video1 = SimpleVideoLoader(video_metadata=meta1)
+        meta2 = VideoMetaInfo(file='v2', c_format=VideoFormat.MOV, fps=30)
+        video2 = SimpleVideoLoader(video_metadata=meta2)
+
+        projection_output = ['c', 't']
+        root = LogicalProjectionPlan(videos=[video1, video2], column_ids=projection_output, foreign_column_ids=[])
+
+        tup = TupleValueExpression(col_idx=int(1))
+        tup.set_col_name("c")
+        const_exp_01 = ConstantValueExpression("t")
+        const_exp_02 = ConstantValueExpression("suv")
+
+        # t=suv
+        cmpr_exp1 = ComparisonExpression(
+            ExpressionType.COMPARE_EQUAL, const_exp_01, const_exp_02)
+
+        # c=white
+        const_exp_21 = ConstantValueExpression("c")
+        const_exp_22 = ConstantValueExpression("white")
+        cmpr_exp3 = ComparisonExpression(
+            ExpressionType.COMPARE_NEQ, const_exp_21, const_exp_22)
+
+        logical_expr2 = LogicalExpression(
+            cmpr_exp1, ExpressionType.LOGICAL_AND, cmpr_exp3)
+        const_exp_01 = ConstantValueExpression("t")
+        const_exp_02 = ConstantValueExpression("suv")
+
+        # t=suv
+        cmpr_exp1 = ComparisonExpression(
+            ExpressionType.COMPARE_EQUAL, const_exp_01, const_exp_02)
+        child = LogicalExpression(cmpr_exp1, ExpressionType.LOGICAL_OR, None)
+
+        expression = child
+        s1 = LogicalSelectPlan(predicate=expression, column_ids=['c'], videos=[video1, video2],
+                               foreign_column_ids=[])
+        s1.parent = root
+        root.append_child(s1)
+        print(root)
+        cb_optimzer = CostBasedOptimizer(constants.UADETRAC)
+        test22 = cb_optimzer.execute(root)
+        print(test22)
+
+        self.assertEqual(child, test22.children[0]._predicate)
 #
 # TODO(galipremsagar): Add
 # the
