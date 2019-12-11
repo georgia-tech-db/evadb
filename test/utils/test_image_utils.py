7bd0044d6b4a42c0bba1293fc047da2547bc2925
Vaishnavi Kannan <vaishnavik.22@gmail.com>
2019-12-10 22:19:11 -0500

    Added test for absolute difference

--- test/utils/test_image_utils.py
+++ test/utils/test_image_utils.py
@@ -19,3 +19,18 @@ class ImageUtilsTest(unittest.TestCase):
 		grayscale = image_utils.convert_to_grayscale(frame)
 		self.assertEqual(3, len(frame.shape))
 		self.assertEqual(2, len(grayscale.shape))
+
+	def test_absolute_distance_metric(self):
+		"""
+        -> Test absolute difference between two frames.
+        Test should return 0 when the same frames are passed.
+
+        :param: None
+        :return: None
+        """
+
+		frame = np.array(np.ones((2, 2, 3)) * 0.1 * 255,
+                         dtype=np.uint8)
+		diff = image_utils.absolute_difference(frame, frame)
+		print(diff)
+		self.assertEqual(0, diff)
