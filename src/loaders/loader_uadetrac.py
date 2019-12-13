9d5ace6ba221c360a56c3766f0baf7e2490f75c8
Shubham Singhal <shubham07iiit@gmail.com>
2019-12-12 19:51:49 -0500

    Adding support for images dataset with annotated labels/ boxes in the loader

--- src/loaders/loader_uadetrac.py
+++ src/loaders/loader_uadetrac.py
@@ -1,20 +1,22 @@
 """
 This file implements the dataset loading methods for UA-detrac
 If any problem occurs, please email jaeho.bang@gmail.com
-
-
 @Jaeho Bang
-
 """
 
 import argparse
 import os
 import warnings
 import xml.etree.ElementTree as ET
-
+import random2 as random    # INSTALL RAMDOM2
 import cv2
 import numpy as np
+import sys
+import time
+import pandas as pd
+from PIL import Image
 
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath('../src'))))
 from src.loaders.TaskManager import TaskManager
 from src.loaders.abstract_loader import AbstractLoader
 
@@ -27,12 +29,13 @@ class UADetracLoader(AbstractLoader):
         self.data_dict = {}
         self.label_dict = {}
         self.vehicle_type_filters = ['car', 'van', 'bus', 'others']
+        self.cloud_type_filters = ['Fish','Flower','Gravel','Sugar']
         self.speed_filters = [40, 50, 60, 65, 70]
         self.intersection_filters = ["pt335", "pt342", "pt211", "pt208"]
         self.color_filters = ['white', 'black', 'silver', 'red']
+        original image height = 540
+        original image width = 960
 
-        # original image height = 540
-        # original image width = 960
         self.image_width = image_width
         self.image_height = image_height
         self.image_channels = 3
@@ -53,6 +56,48 @@ class UADetracLoader(AbstractLoader):
         return None
 
     def load_boxes(self, dir: str = None):
+        if args.data_type=='i':
+            boxes = loader.load_boxes_for_images()
+
+        elif args.data_type=='v':
+            boxes = loader.load_boxes_for_videos()
+
+    def load_labels(self, dir: str = None):
+        if args.data_type=='i':
+            labels = loader.load_labels_for_images()
+
+        elif args.data_type=='v':
+            labels = loader.load_labels_for_videos()
+
+    def load_boxes_for_images(self, dir: str = None):
+        """
+        Loads boxes from annotation
+        Should be same shape as self.labels
+        :return: boxes
+        """
+        if dir is None:
+            dir = os.path.join(self.eva_dir, 'data', 'cloud_dataset/train', self.args.anno_path)
+        info = self.image_info[image_id]
+                
+        mask = np.zeros((IMAGE_SIZE, IMAGE_SIZE, len(info['annotations'])), dtype=np.uint8)
+        labels = []
+        
+        for m, (annotation, label) in enumerate(zip(info['annotations'], info['labels'])):
+            sub_mask = np.full(info['height']*info['width'], 0, dtype=np.uint8)
+            annotation = [int(x) for x in annotation.split(' ')]
+            
+            for i, start_pixel in enumerate(annotation[::2]):
+                sub_mask[start_pixel: start_pixel+annotation[2*i+1]] = 1
+
+            sub_mask = sub_mask.reshape((info['height'], info['width']), order='F')
+            sub_mask = cv2.resize(sub_mask, (IMAGE_SIZE, IMAGE_SIZE), interpolation=cv2.INTER_NEAREST)
+            
+            mask[:, :, m] = sub_mask
+            labels.append(int(label)+1)
+        self.boxes = mask
+        return self.boxes
+
+    def load_boxes_for_videos(self, dir: str = None):
         """
         Loads boxes from annotation
         Should be same shape as self.labels
@@ -61,7 +106,7 @@ class UADetracLoader(AbstractLoader):
         if dir is None:
             dir = os.path.join(self.eva_dir, 'data', 'ua_detrac',
                                self.args.anno_path)
-        self.boxes = np.array(self.get_boxes(dir))
+        self.boxes = np.array(self.get_boxes_for_videos(dir))
         return self.boxes
 
     def load_images(self, image_dir: str = None, image_size=None):
@@ -72,40 +117,76 @@ class UADetracLoader(AbstractLoader):
         if image_size is not None:
             self.image_height = image_size
             self.image_width = image_size
-
+        
         if image_dir is None:
-            image_dir = os.path.join(self.eva_dir, 'data', 'ua_detrac',
-                                     self.args.image_path)
+            if self.args.data_type == 'i':
+                image_dir = os.path.join(self.eva_dir, 'data', 'cloud_dataset/data', self.args.image_path)
+            elif self.args.data_type == 'v':
+                image_dir = os.path.join(self.eva_dir, 'data', 'ua_detrac',
+-                                     self.args.image_path)
+        
         file_names = []
         for root, subdirs, files in os.walk(image_dir):
             files.sort()
-            self.video_start_indices.append(len(file_names))
+            if self.args.data_type == 'v':
+                self.video_start_indices.append(len(file_names))
             for file in files:
                 file_names.append(os.path.join(root, file))
 
         print("Number of files added: ", len(file_names))
-
         self.images = np.ndarray(shape=(
             len(file_names), self.image_height, self.image_width,
             self.image_channels),
             dtype=np.uint8)
-
         for i in range(len(file_names)):
             file_name = file_names[i]
             img = cv2.imread(file_name)
             img = cv2.resize(img, (self.image_width, self.image_height))
             self.images[i] = img
-
         return self.images
 
-    def load_labels(self, dir: str = None):
+    def load_labels_for_images(self, dir: str = None):
+        """
+        Loads cloud type, fish, flower, gravel and sugar of cloud dataset
+        :return: labels
+        """
+        if dir is None:
+            dir = os.path.join(self.eva_dir, 'data', 'cloud_dataset/train',
+                               self.args.anno_path)
+        category_list = self.filters
+
+        train_dict = {}
+        train_class_dict = {}
+        train_df = pd.read_csv(dir+"/train.csv")
+        train_df = train_df.dropna()
+        for idx, row in train_df.iterrows():
+            image_filename = row.Image_Label.split("_")[0]
+            class_name = row.Image_Label.split("_")[1]
+            class_id = category_list.index(class_name)
+            if train_dict.get(image_filename):
+                train_dict[image_filename].append(row.EncodedPixels)
+                train_class_dict[image_filename].append(class_id)
+            else:
+                train_dict[image_filename] = [row.EncodedPixels]
+                train_class_dict[image_filename] = [class_id]
+        
+        df = pd.DataFrame(columns=["image_id","EncodedPixels","CategoryId","Width","Height"])
+        for key, value in train_dict.items():
+            img = Image.open(dir+"/train_images/{}".format(key))
+            width, height = img.width, img.height
+            df = df.append({"image_id": key, "EncodedPixels": value, "CategoryId": train_class_dict[key], "Width": width, "Height": height},ignore_index=True)
+            self.labels = {'classes': df} #returning dataframes
+            return self.labels
+        else:
+            return None
+
+    def load_labels_for_videos(self, dir: str = None):
         """
         Loads vehicle type, speed, color, and intersection of ua-detrac
         vehicle type, speed is given by the dataset
         color, intersection is derived from functions built-in
         :return: labels
         """
-
         if dir is None:
             dir = os.path.join(self.eva_dir, 'data', 'ua_detrac',
                                self.args.anno_path)
@@ -194,7 +275,8 @@ class UADetracLoader(AbstractLoader):
         self.labels = labels_pickeled.item()
         return self.labels
 
-    def get_boxes(self, anno_dir):
+
+    def get_boxes_for_videos(self, anno_dir):
         width = self.image_width
         height = self.image_height
         import xml.etree.ElementTree as ET
@@ -231,6 +313,57 @@ class UADetracLoader(AbstractLoader):
 
                 boxes_dataset.append(boxes_frame)
 
+        return boxes_dataset
+
+    
+    def get_boxes_for_images(self, anno_dir):
+        width = self.image_width
+        height = self.image_height
+        category_list = self.filters
+        boxes_dataset = []
+        # for i, name in enumerate(category_list):
+        #   self.add_class("cloud", i+1, name)
+        df = pd.DataFrame(columns=["image_id","EncodedPixels","CategoryId","Width","Height"])
+        train_dataset = CloudDataset(df[:training_set_size])
+        for i, row in df.iterrows():
+            self.add_image("cloud", 
+                           image_id=row.name, 
+                           path='../understanding_cloud_organization/train_images/'+str(row.image_id), 
+                           labels=row['CategoryId'],
+                           annotations=row['EncodedPixels'], 
+                           height=row['Height'], width=row['Width'])
+        for i in range(50):
+            image_id = random.choice(train_dataset.image_ids)
+            # print(train_dataset.image_reference(image_id))
+            
+            # image = resize_image(self.image_info[image_id]['path'])
+            info = self.image_info[image_id]
+                
+            mask = np.zeros((IMAGE_SIZE, IMAGE_SIZE, len(info['annotations'])), dtype=np.uint8)
+            # labels = []
+            
+            for m, (annotation, label) in enumerate(zip(info['annotations'], info['labels'])):
+                sub_mask = np.full(info['height']*info['width'], 0, dtype=np.uint8)
+                annotation = [int(x) for x in annotation.split(' ')]
+                
+                for i, start_pixel in enumerate(annotation[::2]):
+                    sub_mask[start_pixel: start_pixel+annotation[2*i+1]] = 1
+
+                sub_mask = sub_mask.reshape((info['height'], info['width']), order='F')
+                sub_mask = cv2.resize(sub_mask, (IMAGE_SIZE, IMAGE_SIZE), interpolation=cv2.INTER_NEAREST)
+                
+                mask[:, :, m] = sub_mask
+                # labels.append(int(label)+1)
+                print(mask)
+                boxes_frame.append(mask)
+
+            boxes_dataset.append(boxes_frame)
+                
+            # return mask, np.array(labels)
+
+            # convert mask to bounding box
+
+        # return mask, np.array(labels)
         return boxes_dataset
 
     def get_corners(self, box, width_scale, height_scale):
@@ -334,8 +467,15 @@ class UADetracLoader(AbstractLoader):
 
 def get_parser():
     parser = argparse.ArgumentParser(description='Define arguments for loader')
+    
+    parser.add_argument('--data_type', default='i' ,
+                        help='i and v for image and video dataset respectively')
+    parser.add_argument('--path', default='small-data',
+                        help='Define data folder within eva/data/cloud_dataset')
+
     parser.add_argument('--image_path', default='small-data',
                         help='Define data folder within eva/data/uadetrac')
+
     parser.add_argument('--anno_path', default='small-annotations',
                         help='Define annotation folder within '
                              'eva/data/uadetrac')
@@ -357,21 +497,33 @@ def get_parser():
     return parser
 
 
-if __name__ == "__main__":
+def load_images():
+    if args.data_type=='i':
+        images = loader.load_images()
 
+    elif args.data_type=='v':
+        images = loader.load_video()
+
+
+if __name__ == "__main__":
     parser = get_parser()
     args = parser.parse_args()
 
     import time
-
+    
+    if args.data_type=='i':
+        args.image_path=args.path
+    elif args.data_type=='v':
+        args.video_path=args.path
     st = time.time()
     loader = UADetracLoader(args)
     images = loader.load_images()
     labels = loader.load_labels()
     boxes = loader.load_boxes()
+    
 
-    print("Time taken to load everything from disk", time.time() - st,
-          "seconds")
+    print("Time taken to load everything from disk ", time.time() - st,
+          " seconds")
     loader.save_boxes()
     loader.save_labels()
     loader.save_images()
