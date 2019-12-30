25df5b9ceb57219bffe4e2ce0970e98eacfddc30
jarulraj <jarulraj@cs.cmu.edu>
2019-12-29 19:40:05 -0500

    Added spark wrapper

---
+++ src/spark/session.py
@@ -0,0 +1,53 @@
+# coding=utf-8
+# Copyright 2019 EVA
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from pyspark.sql import SparkSession
+
+
+class Session:
+    """
+    Wrapper around Spark Session
+    """
+
+    def __init__(self, application_name, spark_master=None):
+        """Setup a spark session.
+    
+        :param spark_master: A master parameter used by spark session builder. Use default value (None) to use system
+          environment configured spark cluster. Use 'local[*]' to run on a local box.
+    
+        :return: spark_session: A spark session
+        """    
+        print("Starting session")
+
+        session_builder = SparkSession \
+            .builder \
+            .appName(application_name)
+        
+        if spark_master:
+            session_builder.master(spark_master)
+    
+        # Gets an existing SparkSession or, 
+        # if there is no existing one, creates a new one based on the options set in this builder.
+        self._session = session_builder.getOrCreate()
+        
+    def get_session(self):
+        print("Getting session")
+
+        return self._session
+    
+    def __del__(self):
+        # Stop spark session
+        print("Stopping session")
+        self._session.stop()
---
+++ test/spark/test_session.py
@@ -0,0 +1,21 @@
+import unittest
+
+from pyspark.sql import SparkSession
+
+from src.spark.session import Session
+
+class SparkSessionTest(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def test_session(self):
+
+        application_name = "default"        
+        session = Session(application_name)
+        
+        spark_session = session.get_session()
+        
+        self.assertIsInstance(spark_session, SparkSession)
+
+if __name__ == '__main__':
+    unittest.main()
