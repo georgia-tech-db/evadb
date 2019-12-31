5a16fbdb1b6474e32a3da2459851644aaff8473b
jarulraj <jarulraj@cs.cmu.edu>
2019-12-31 15:31:06 -0500

    Applied formatter

--- test/query_parser/test_parser_visitor.py
+++ test/query_parser/test_parser_visitor.py
@@ -1,6 +1,20 @@
+# coding=utf-8
+# Copyright 2018-2020 EVA
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import unittest
 from unittest import mock
-from unittest.mock import MagicMock, call, Mock
+from unittest.mock import MagicMock, call
 
 from src.query_parser.eva_ql_parser_visitor import EvaParserVisitor
 from third_party.evaQL.parser.frameQLParser import frameQLParser
