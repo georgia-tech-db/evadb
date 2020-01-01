751fbdf74bb6324aedc5502a41c52dc3ca92718d
jarulraj <jarulraj@cs.cmu.edu>
2020-01-01 01:48:33 -0500

    Added logger
    Former-commit-id: 05d316782cd97ac97b423067940b3239126e357e [formerly 62ae93f588fface858a3b1cb7810965173b255ae] [formerly d8a2c8594b75251c7154c1383e0d195ae82bb944 [formerly f5fe3a64080cfc09943a2ecd133f0e467567b77f]] [formerly 87ee6b6a6c032650cf3088cc8fd44e7a105a43c4 [formerly f309290d5ed7457dbc1570d7620475510e758b76] [formerly 3bc5d910dadac2143d7c21480749c27fb00d2bd9 [formerly 3bc5d910dadac2143d7c21480749c27fb00d2bd9 [formerly 29ac6ccb6a6e4bd59aa13b942b3e09a59ed46c53]]]]
    Former-commit-id: 6fa3121b4794fc2c7823b972955ff932313c925c [formerly 5effa4329f9c9b7dad1184851b4747b99c1419cc] [formerly 4d304864707eb034df6e26c8267999f260891c00 [formerly 8ec088410411c14455c55a12f88c99f11f6a6c22]]
    Former-commit-id: 2ef5cfbd6eebc47ee35415a2e6286acdee9b6ff3 [formerly 38f80e9797d4f7f7ad4ff5b651b726e21193cb0e]
    Former-commit-id: ccd0821c145a33fd47c665952ffec6c2201b9222
    Former-commit-id: 61647eccd7d1a4fe10b90ef877ad36a1bc3650f1

---
+++ src/utils/logging.py
@@ -0,0 +1,67 @@
+# coding=utf-8
+# Copyright 2018-2020 EVA
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+
+from enum import Enum
+
+
+class LoggingLevel(Enum):
+    DEBUG = 1
+    INFO = 2
+    WARNING = 3
+    ERROR = 4
+    CRITICAL = 5
+
+class Logger:
+    class __Logger:
+        def __init__(self):
+
+            # LOGGING CONFIGURATION            
+            self.LOG = logging.getLogger(__name__)
+            LOG_handler = logging.StreamHandler()
+            LOG_formatter = logging.Formatter(
+                fmt='%(asctime)s [%(funcName)s:%(lineno)03d] %(levelname)-5s: %(message)s',
+                datefmt='%m-%d-%Y %H:%M:%S'
+            )
+            LOG_handler.setFormatter(LOG_formatter)
+            self.LOG.addHandler(LOG_handler)
+            self.LOG.setLevel(logging.INFO)
+
+        def __str__(self):
+            return repr(self)
+
+    instance = None
+
+    def __init__(self):
+        if not Logger.instance:
+            Logger.instance = Logger.__Logger()
+    
+    def __getattr__(self, name):
+        return getattr(self.instance, name)
+
+
+    def log(self, string, level:LoggingLevel = LoggingLevel.DEBUG):
+        
+        if level == LoggingLevel.DEBUG:      
+            Logger.instance.LOG.debug(string)
+        elif level == LoggingLevel.INFO:
+            Logger.instance.LOG.info(string)
+        elif level == LoggingLevel.WARNING:
+            Logger.instance.LOG.warn(string)
+        elif level == LoggingLevel.ERROR:
+            Logger.instance.LOG.error(string)
+        elif level == LoggingLevel.CRITICAL:
+            Logger.instance.LOG.critical(string)
