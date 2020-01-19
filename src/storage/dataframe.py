cb41b6d026666ed4d67f12d989b7e28fd738c56e
jarulraj <jarulraj@cs.cmu.edu>
2020-01-19 14:04:47 -0500

    Checkpoint
    Former-commit-id: 7bc8ecd77f0dcdf95fceba15000736eca1500bd1 [formerly 575a1038a968d0ac6140db52da9d7f1580da246c] [formerly 2c30396b98a7125ddd734d520f471ca0834bf768 [formerly d874aab490c1857069d1ae5a94c9c376e6e27125]] [formerly fb371dbeddc0a63041df2a3c4dec881ae2325d5c [formerly 16f4093ee0d87255ff12429b17b2220d0e650c38] [formerly 1d95f9919bc54d646eada2e88803bf1cf77c1ed3 [formerly 9551ebdab1dc494c9e0426cf8f0a9530a19c2642]]]
    Former-commit-id: 7181d5d82c1af3496bbee1b3a8a94431410c3d13 [formerly 5118bf4a573a3d565c7a34d8f2858b1aee7350aa] [formerly 3524f6737aad31f968723b81ec893d138130ee93 [formerly 946b02101d0b5d8f50d15401757e0c2a831ab4c0]]
    Former-commit-id: c191a5cbdcd747e58ec140d2b7cd01228efbcd05 [formerly 4f7ab695d27aa0303ba6436973582c163b08ce84]
    Former-commit-id: 1537e8119364316ae1cdbb5af00ae81f894d911f

--- src/storage/dataframe.py
+++ src/storage/dataframe.py
@@ -19,6 +19,37 @@ from src.spark.session import Session
 from petastorm.etl.dataset_metadata import materialize_dataset
 
 
+class DataFrameMetadata(object):
+
+    _dataframe_file_url = None
+    _dataframe_schema = None
+    _dataframe_petastorm_schema = None
+    _dataframe_pyspark_schema = None
+
+    def __init__(self,
+                 dataframe_file_url,
+                 dataframe_schema
+                 ):
+        self._dataframe_file_url = dataframe_file_url
+        self._dataframe_schema = dataframe_schema
+        self._dataframe_petastorm_schema = \
+            dataframe_schema.get_petastorm_schema()
+        self._dataframe_pyspark_schema = \
+            self._dataframe_petastorm_schema.as_spark_schema()
+
+    def get_dataframe_file_url(self):
+        return self._dataframe_file_url
+
+    def get_dataframe_schema(self):
+        return self._dataframe_schema
+
+    def get_dataframe_petastorm_schema(self):
+        return self._dataframe_petastorm_schema
+
+    def get_dataframe_pyspark_schema(self):
+        return self._dataframe_pyspark_schema
+
+
 def load_dataframe(dataframe_url: str):
 
     spark = Session().get_session()
@@ -27,33 +58,30 @@ def load_dataframe(dataframe_url: str):
     return dataframe
 
 
-def append_rows(dataframe_url: str,
-                pyspark_schema,
-                petastorm_schema,
+def append_rows(data_frame_metadata: DataFrameMetadata,
                 rows):
 
     spark = Session().get_session()
 
     # Convert a list of rows to RDD
-    rows_df = spark.createDataFrame(rows, schema=pyspark_schema)
+    rows_df = spark.createDataFrame(rows,
+                                    schema=data_frame_metadata.get_dataframe_pyspark_schema())
     rows_rdd = rows_df.rdd
 
     # Use petastorm to appends rows
     with materialize_dataset(spark,
-                             dataframe_url,
-                             petastorm_schema):
+                             data_frame_metadata.get_dataframe_file_url(),
+                             data_frame_metadata.get_dataframe_petastorm_schema()):
 
         spark.createDataFrame(rows_rdd,
-                              pyspark_schema) \
+                              data_frame_metadata.get_dataframe_pyspark_schema()) \
             .coalesce(1) \
             .write \
             .mode('append') \
-            .parquet(dataframe_url)
+            .parquet(data_frame_metadata.get_dataframe_file_url())
 
 
-def create_dataframe(dataframe_url: str,
-                     pyspark_schema,
-                     petastorm_schema):
+def create_dataframe(data_frame_metadata: DataFrameMetadata):
 
     spark = Session().get_session()
     spark_context = Session().get_context()
@@ -63,12 +91,12 @@ def create_dataframe(dataframe_url: str,
 
     # Use petastorm to create dataframe
     with materialize_dataset(spark,
-                             dataframe_url,
-                             petastorm_schema):
+                             data_frame_metadata.get_dataframe_file_url(),
+                             data_frame_metadata.get_dataframe_petastorm_schema()):
 
         spark.createDataFrame(empty_rdd,
-                              pyspark_schema) \
+                              data_frame_metadata.get_dataframe_pyspark_schema()) \
             .coalesce(1) \
             .write \
             .mode('overwrite') \
-            .parquet(dataframe_url)
+            .parquet(data_frame_metadata.get_dataframe_file_url())
