58582d366cd2d44a5f6a5d785bdf1bb3c9eccd9a
GTK <gaurav21776@gmail.com>
2020-01-30 17:00:38 -0500

    1. Parsed statement converted to Logical Operator. 2. Logical plan tree built using operators. 3. Util functionality for binding tables, columns with catalog.

---
+++ src/optimizer/optimizer_utils.py
@@ -0,0 +1,56 @@
+from src.parser.table_ref import TableInfo
+from src.catalog.catalog_manager import CatalogManager
+from typing import List
+from src.expression.tuple_value_expression import ExpressionType
+
+
+def bind_table_ref(video_info: TableInfo) -> int:
+    """Grab the metadata id from the catalog for
+    input video
+
+    Arguments:
+        video_info {TableInfo} -- [input parsed video info]
+    Return:
+        catalog_entry for input table
+    """
+
+    catalog = CatalogManager()
+    catalog_entry_id, _ = catalog.get_table_bindings(video_info.database_name,
+                                                     video_info.table_name,
+                                                     None)
+    return catalog_entry_id
+
+
+def bind_columns_expr(target_columns: List['AbstractExpression']):
+    if target_columns is None:
+        return
+        
+    for column_exp in target_columns:
+        child_count = column_exp.get_children_count()
+        for i in range(child_count):
+            bind_columns_expr([column_exp.get_child(i)])
+
+        if column_exp.etype == ExpressionType.TUPLE_VALUE:
+            bind_tuple_value_expr(column_exp)
+
+
+def bind_tuple_value_expr(expr: 'AbstractExpression'):
+    catalog = CatalogManager()    
+    table_id, column_ids = catalog.get_table_bindings(None,
+                                                      expr.table_name,
+                                                      expr.col_name)
+    expr.table_metadata_id = table_id
+    expr.col_metadata_id = column_ids.pop()
+
+
+def bind_predicate_expr(predicate: 'AbstractExpression'):
+    # This function will be expanded as we add support for 
+    # complex predicate expressions and sub select predicates
+
+    child_count = predicate.get_children_count()
+    for i in range(child_count):
+        bind_predicate_expr(predicate.get_child(i))
+    
+    if predicate.etype == ExpressionType.TUPLE_VALE:
+        bind_tuple_value_expr(predicate)
+    
