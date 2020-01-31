de0171c8a34210192b4b2df1cfb6e01f2eeb168e
GTK <gaurav21776@gmail.com>
2020-01-30 20:07:18 -0500

    Ran Formatter

--- src/catalog/sql_config.py
+++ src/catalog/sql_config.py
@@ -1,3 +1,17 @@
+# coding=utf-8
+# Copyright 2018-2020 EVA
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -12,7 +26,8 @@ class SQLConfig(object):
         return cls._instance
 
     def __init__(self):
-        self.engine = create_engine('mysql+pymysql://root:root@localhost/eva_catalog')
+        self.engine = create_engine(
+            'mysql+pymysql://root:root@localhost/eva_catalog')
         self.session_factory = sessionmaker(bind=self.engine)
         self.session = self.session_factory()
         self.base.metadata.create_all(self.engine)
