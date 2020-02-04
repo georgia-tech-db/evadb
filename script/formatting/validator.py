1e2fe9eb45a8fdc8b9908cec7f701ca6367697f7
Akhilesh Siddhanti <akhileshsiddhanti23@gmail.com>
2020-02-04 03:29:47 -0500

    Checking githook run

--- script/formatting/validator.py
+++ script/formatting/validator.py
@@ -1,9 +1,33 @@
+# coding=utf-8
+# Copyright 2018-2020 EVA
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# ======================================================
+# GOAL : Warn about print statements and commented code
+# ======================================================
+
+
 import argparse
 import os
 import re
 import functools
 
 from formatter import LOG
+
+# ==============================================
+# LOGGING CONFIGURATION
+# ==============================================
 
 EXIT_SUCCESS = 0
 EXIT_FAILURE = -1
@@ -22,6 +46,10 @@ EVA_TEST_DIR = os.path.join(EVA_DIR, "test")
 EVA_SCRIPT_DIR = os.path.join(EVA_DIR, "script")
 
 DIRS = [EVA_SRC_DIR,EVA_TEST_DIR]
+
+# ==============================================
+# UTILITY FUNCTION DEFINITIONS
+# ==============================================
 
 def comment_contains_code(line):
     line = line.lstrip()
