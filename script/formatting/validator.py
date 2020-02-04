058f0e000bf1ec476ea78d07d7f96f9604175e98
Akhilesh Siddhanti <akhileshsiddhanti23@gmail.com>
2020-02-04 02:59:37 -0500

    Added comment checking code

--- script/formatting/validator.py
+++ script/formatting/validator.py
@@ -3,6 +3,8 @@ import os
 import re
 import functools
 
+from formatter import LOG
+
 EXIT_SUCCESS = 0
 EXIT_FAILURE = -1
 
@@ -21,12 +23,52 @@ EVA_SCRIPT_DIR = os.path.join(EVA_DIR, "script")
 
 DIRS = [EVA_SRC_DIR,EVA_TEST_DIR]
 
+def comment_contains_code(line):
+    line = line.lstrip()
+    
+    if 'utf-8' in line:
+        return False
+
+    if not line.startswith('#'):
+        return False
+
+    line = line.lstrip(' \t\v\n#').strip()
+
+    regex_list = [
+        r"def .+\)[\s]*[->]*[\s]*[a-zA-Z_]*[a-zA-Z0-9_]*:$",
+        r"with .+ as [a-zA-Z_][a-zA-Z0-9_]*:$",
+        r"for [a-zA-Z_][a-zA-Z0-9_]* in .+:$",
+        r'continue$', r'break$'
+    ]
+
+    for regex in regex_list:
+        if re.search(regex,line):
+            return True
+
+    symbol_list = list('[]{}=%') +\
+        ['print', 'break', 
+        'import ','elif ']
+
+
+    for symbol in symbol_list:
+        if symbol in line:
+            return True
+
+    #Handle return statements in a specific way
+    if 'return' in line:
+        if len(line.split(' ')) >= 2:
+            return False
+        else:
+            return True
+    return False
+
+
 def validate_file(file):
 
     file = os.path.abspath(file)
 
     if not os.path.isfile(file):
-        print ("ERROR: " + file + " isn't a file")
+        LOG.info ("ERROR: " + file + " isn't a file")
         sys.exit(EXIT_FAILURE)
 
     if not file.endswith('.py'):
@@ -34,16 +76,29 @@ def validate_file(file):
 
     code_validation = True
     line_number = 1
+    commented_code = False
 
     with open(file,'r') as opened_file:
         for line in opened_file:
+
+            #Check if the line has commented code
+            if line.lstrip().startswith('#'):
+                commented_code = comment_contains_code(line)
+
+                if commented_code:
+                    LOG.info ("Commented code " \
+                            +"in file "+file \
+                            +" Line {}: {}".format(line_number,line.strip()))
+
             
             #Search for a pattern, and report hits
             for validator_pattern in VALIDATOR_PATTERNS:
                 if validator_pattern.search(line):
                     code_validation = False
-                    print ("Unacceptable pattern:",validator_pattern.pattern.strip(),"in file",file,end=". ")
-                    print ("Line {}: {}".format(line_number,line.strip()))
+                    LOG.info ("Unacceptable pattern:"\
+                            +validator_pattern.pattern.strip()\
+                            +" in file "+file
+                            +" Line {}: {}".format(line_number,line.strip()))
 
             line_number += 1
 
@@ -77,12 +132,11 @@ if __name__ == '__main__':
     if ARGS.files:
         for each_file in ARGS.files:
             each_file = os.path.abspath(each_file.lower())
-            print (each_file)
             
             status = validate_file(each_file)
 
             if not status:
-                print ("WARNING: Code Validation fails for file:",each_file)
+                LOG.info ("WARNING: Code Validation fails for file:"+each_file)
 
     else:
         # Scanning entire source directory
@@ -90,7 +144,7 @@ if __name__ == '__main__':
         status = validate_directory(DIRS)
 
         if not status:
-            print ("WARNING: Code Validation fails!")
+            LOG.info ("WARNING: Code Validation fails!")
 
 
         
