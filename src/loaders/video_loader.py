16953c996108bd68ffe9aa53ae2bbec1a1e71699
Prashanth Dintyala <saiprashanth173@gmail.com>
2020-02-22 14:15:40 -0500

    Update video loader to use abstract loader and remove frame skipping logic

--- src/loaders/video_loader.py
+++ src/loaders/video_loader.py
@@ -12,49 +12,32 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from typing import Iterator
+
 import cv2
 
+from src.loaders.abstract_loader import AbstractVideoLoader
 from src.models.catalog.frame_info import FrameInfo
 from src.models.catalog.properties import ColorSpace
-from src.models.catalog.video_info import VideoMetaInfo
-from src.models.storage.batch import FrameBatch
 from src.models.storage.frame import Frame
-
-from src.utils.logging_manager import LoggingManager
 from src.utils.logging_manager import LoggingLevel
+from src.utils.logging_manager import LoggingManager
 
 
-class VideoLoader():
-    def __init__(self, video_metadata: VideoMetaInfo, batch_size=1,
-                 skip_frames=0, offset=None, limit=None):
+class VideoLoader(AbstractVideoLoader):
+
+    def __init__(self, *args, **kwargs):
         """
-         Abstract class for defining video loader.
-         All other video loaders use this abstract class.
-         Video loader are expected fetch the videos from storage
-         and return the frames in an iterative manner.
-
-         Attributes:
-             video_metadata (VideoMetaInfo):
-             Object containing metadata of the video
-             batch_size (int, optional):
-            No. of frames to fetch in batch from video
-             skip_frames (int, optional):
-             Number of frames to be skipped while fetching the video
-             offset (int, optional): Start frame location in video
-             limit (int, optional): Number of frames needed from the video
+            Loader which loads video frames using OpenCV.
          """
-        self.video_metadata = video_metadata
-        self.batch_size = batch_size
-        self.skip_frames = skip_frames
-        self.offset = offset
-        self.limit = limit
+        super().__init__(*args, **kwargs)
 
-    def load(self):
+    def _load_frames(self) -> Iterator[Frame]:
         video = cv2.VideoCapture(self.video_metadata.file)
         video_start = self.offset if self.offset else 0
         video.set(cv2.CAP_PROP_POS_FRAMES, video_start)
 
-        LoggingManager().log("Loading frames", LoggingLevel.CRITICAL)
+        LoggingManager().log("Loading frames", LoggingLevel.INFO)
 
         _, frame = video.read()
         frame_ind = video_start - 1
@@ -64,23 +47,7 @@ class VideoLoader():
             (height, width, num_channels) = frame.shape
             info = FrameInfo(height, width, num_channels, ColorSpace.BGR)
 
-        frames = []
         while frame is not None:
             frame_ind += 1
-            eva_frame = Frame(frame_ind, frame, info)
-            if self.skip_frames > 0 and frame_ind % self.skip_frames != 0:
-                _, frame = video.read()
-                continue
-
-            frames.append(eva_frame)
-            if self.limit and frame_ind >= self.limit:
-                return FrameBatch(frames, info)
-
-            if len(frames) % self.batch_size == 0:
-                yield FrameBatch(frames, info)
-                frames = []
-
+            yield Frame(frame_ind, frame, info)
             _, frame = video.read()
-
-        if frames:
-            return FrameBatch(frames, info)
