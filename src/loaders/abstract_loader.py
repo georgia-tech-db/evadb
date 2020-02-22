8f6b91468e96d0c5e28f2e9d972b1234d9b33bda
Prashanth Dintyala <saiprashanth173@gmail.com>
2020-02-22 14:13:39 -0500

    Add an abstract loader class

---
+++ src/loaders/abstract_loader.py
@@ -0,0 +1,65 @@
+from abc import ABCMeta, abstractmethod
+from typing import Iterator
+
+from src.models.catalog.video_info import VideoMetaInfo
+from src.models.storage.batch import FrameBatch
+from src.models.storage.frame import Frame
+
+
+class AbstractVideoLoader(metaclass=ABCMeta):
+    """
+    Abstract class for defining video loader. All other video loaders use this
+    abstract class. Video loader are expected fetch the videos from storage
+    and return the frames in an iterative manner.
+
+    Attributes:
+        video_metadata (VideoMetaInfo): Object containing metadata of the video
+        batch_size (int, optional): No. of frames to fetch in batch from video
+        skip_frames (int, optional): Number of frames to be skipped
+                                     while fetching the video
+        offset (int, optional): Start frame location in video
+        limit (int, optional): Number of frames needed from the video
+        shard (int, optional): Shard number to load from if sharded
+    """
+
+    def __init__(self, video_metadata: VideoMetaInfo, batch_size=1,
+                 skip_frames=0, offset=None, limit=None, shard=0):
+        self.video_metadata = video_metadata
+        self.batch_size = batch_size
+        self.skip_frames = skip_frames
+        self.offset = offset
+        self.limit = limit
+        self.shard = shard
+
+    def load(self) -> Iterator[FrameBatch]:
+        """
+        This is a generator for loading the frames of a video.
+         Uses the video metadata and other class arguments
+
+        Yields:
+        :obj: `eva.models.FrameBatch`: An object containing a batch of frames
+                                       and frame specific metadata
+        """
+
+        frames = []
+        for frame in self._load_frames():
+            if self.skip_frames > 0 and frame.index % self.skip_frames != 0:
+                continue
+            if self.limit and frame.index >= self.limit:
+                return FrameBatch(frames, frame.info)
+            frames.append(frame)
+            if len(frames) % self.batch_size == 0:
+                yield FrameBatch(frames, frame.info)
+                frames = []
+        if frames:
+            return FrameBatch(frames, frames[0].info)
+
+    @abstractmethod
+    def _load_frames(self) -> Iterator[Frame]:
+        """
+        Loads video frames from storage and returns the Frame type.
+
+        Yields:
+            Frame:  A frame object of the video, used for processing.
+        """
+        pass
