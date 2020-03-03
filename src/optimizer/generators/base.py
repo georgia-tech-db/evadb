6eab14620097b0200232aa0b2856109a0db51ce2
Prashanth Dintyala <saiprashanth173@gmail.com>
2020-03-02 20:11:25 -0500

    Add plan generator

---
+++ src/optimizer/generators/base.py
@@ -0,0 +1,37 @@
+# coding=utf-8
+# Copyright 2018-2020 EVA
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from abc import ABC, abstractmethod
+
+from src.optimizer.operators import Operator
+from src.planner.abstract_plan import AbstractPlan
+
+
+class Generator(ABC):
+
+    @abstractmethod
+    def build(self, operator: Operator) -> AbstractPlan:
+        """
+        Generates the physical plan from the Logical plan (Operator tree)
+
+        Arguments:
+            operator (Operator): the logical operator tree.
+
+        Returns:
+            `AbstractPlan`: the plan constructed by traversing the Operator
+            tree.
+        """
+        pass
+
+
---
+++ test/optimizer/test_plan_generator.py
@@ -0,0 +1,53 @@
+# coding=utf-8
+# Copyright 2018-2020 EVA
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import unittest
+
+from mock import patch
+
+from src.optimizer.operators import LogicalProject, LogicalGet, LogicalFilter, \
+    Operator
+from src.optimizer.plan_generator import PlanGenerator
+
+
+class PlanGeneratorTest(unittest.TestCase):
+    @patch("src.optimizer.plan_generator.ScanGenerator")
+    def test_should_return_use_scan_generator_for_logical_project(self,
+                                                                  mock_class):
+        mock_instance = mock_class.return_value
+        l_project = LogicalProject([])
+        PlanGenerator().build(l_project)
+        mock_instance.build.assert_called_with(l_project)
+
+    @patch("src.optimizer.plan_generator.ScanGenerator")
+    def test_should_return_use_scan_generator_for_logical_get(self,
+                                                              mock_class):
+        mock_instance = mock_class.return_value
+        l_get = LogicalGet(None, 1)
+        PlanGenerator().build(l_get)
+        mock_instance.build.assert_called_with(l_get)
+
+    @patch("src.optimizer.plan_generator.ScanGenerator")
+    def test_should_return_use_scan_generator_for_logical_filter(self,
+                                                                 mock_class):
+        mock_instance = mock_class.return_value
+        l_filter = LogicalFilter(None)
+        PlanGenerator().build(l_filter)
+        mock_instance.build.assert_called_with(l_filter)
+
+    @patch("src.optimizer.plan_generator.ScanGenerator")
+    def test_should_not_call_scan_generator_for_other_types(self,
+                                                            mock_class):
+        PlanGenerator().build(Operator(None))
+        mock_class.assert_not_called()
