f396715a24217b2231799aa10a700b3293fbaf28
GTK <gaurav21776@gmail.com>
2020-03-03 01:11:21 -0500

    Insert Generator added

--- test/optimizer/test_plan_generator.py
+++ test/optimizer/test_plan_generator.py
@@ -12,12 +12,26 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# coding=utf-8
+# Copyright 2018-2020 EVA
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import unittest
 
 from mock import patch
 
-from src.optimizer.operators import LogicalProject, LogicalGet, LogicalFilter, \
-    Operator
+from src.optimizer.operators import LogicalProject, LogicalGet, \
+    LogicalFilter, LogicalInsert, Operator
 from src.optimizer.plan_generator import PlanGenerator
 
 
@@ -50,4 +64,23 @@ class PlanGeneratorTest(unittest.TestCase):
     def test_should_not_call_scan_generator_for_other_types(self,
                                                             mock_class):
         PlanGenerator().build(Operator(None))
+        PlanGenerator().build(LogicalInsert(None, 1, [], []))
+        print(mock_class)
+        mock_class.assert_not_called()
+
+    @patch("src.optimizer.plan_generator.InsertGenerator")
+    def test_should_return_use_insert_generator_for_logical_insert(
+            self, mock_class):
+        mock_instance = mock_class.return_value
+        l_insert = LogicalInsert(None, 1, [], [])
+        PlanGenerator().build(l_insert)
+        mock_instance.build.assert_called_with(l_insert)
+
+    @patch("src.optimizer.plan_generator.InsertGenerator")
+    def test_should_not_call_insert_generator_for_other_types(
+            self, mock_class):
+        PlanGenerator().build(Operator(None))
+        PlanGenerator().build(Operator(LogicalFilter(None)))
+        PlanGenerator().build(Operator(LogicalGet(None, 1)))
+        PlanGenerator().build(Operator(LogicalProject([])))
         mock_class.assert_not_called()
