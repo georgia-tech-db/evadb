1db903bd685ac81cfdc50d8bc6656021aff09e4b
jarulraj <jarulraj@cs.cmu.edu>
2020-03-18 02:10:46 -0400

    Checkpoint

--- script/installation/install.sh
+++ script/installation/install.sh
@@ -1,25 +1,8 @@
 #!/bin/sh
 
-# Install mysql and start server
-sudo apt-get update
-sudo apt-get install mysql-server
-sudo systemctl start mysql
-
-# Install java8 (for running antlr4)
-sudo apt install openjdk-8-jdk openjdk-8-jre
-
-# Install conda
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
-bash miniconda.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
-hash -r
-conda config --set always_yes yes --set changeps1 no
-conda update -q conda
-conda info -a
-
-# Install conda packages
+# Install conda packages for eva
 conda env create -f script/installation/conda_eva_environment.yml
-source activate eva
+. activate eva
 conda list
 
 # Create eva-catalog database
