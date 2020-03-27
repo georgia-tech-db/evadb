0474d312786570fea5df1ffd01dc805636c294a3
Sanjana Garg <garg.sanjana.95@gmail.com>
2020-03-27 00:40:36 -0700

    Commented excluded patterns

--- docs/conf.py
+++ docs/conf.py
@@ -55,7 +55,7 @@ language = 'Python'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+# exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
