1ae9e5f0b2f36657fda85b0cac4525bbf43cc781
Sanjana Garg <garg.sanjana.95@gmail.com>
2020-04-02 21:08:07 -0700

    Adding master_doc

--- docs/conf.py
+++ docs/conf.py
@@ -30,6 +30,7 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+master_doc='index'
 extensions = [
 ]
 
