8fc6afee72b6d1d2c36709016f76b6fa10e83895
Sanjana Garg <garg.sanjana.95@gmail.com>
2020-04-16 22:45:19 -0700

    changed theme

--- api-docs/conf.py
+++ api-docs/conf.py
@@ -12,11 +12,11 @@
 #
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../src/'))
-print(sys.path)
+
 import sphinx_rtd_theme
-import mock
 
 # -- Project information -----------------------------------------------------
 
@@ -27,16 +27,22 @@ author = 'Database Lab'
 # The full version, including alpha/beta/rc tags
 release = '0.0.1'
 
+master_doc = 'index'
+
+html_theme = 'mps'
+html_sidebars = {
+    '**': ['localtoc.html', 'relations.html', 'links.html', 'contact.html'],
+}
+
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-master_doc = 'index'
 extensions = [
     'sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx',
     'sphinx.ext.todo', 'sphinx.ext.mathjax', 'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon', 'sphinx.ext.graphviz'
+    'sphinx.ext.napoleon', 'sphinx.ext.graphviz', 'extensions.mps'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -47,8 +53,13 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# The suffix of source filenames.
+source_suffix = '.rst'
+
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
+
+highlight_language = 'python'
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
@@ -64,10 +75,10 @@ autodoc_mock_imports = ["numpy", "sqlalchemy", "sqlalchemy_utils",
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+# html_theme = 'sphinx_rtd_theme'
 
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme_path = ['themes']
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
