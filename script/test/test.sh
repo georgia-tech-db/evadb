c5034ac880dfe1356a6cdf7ff8052e9e25c3833e
GTK <gaurav21776@gmail.com>
2020-07-11 17:03:39 -0400

    fixed travis | failed to capture exit code of pycodestyle

--- script/test/test.sh
+++ script/test/test.sh
@@ -2,7 +2,13 @@
 
 # Run linter (checks code style)
 pycodestyle  --select E src/ test/ --exclude src/filters,src/parser/evaql
-
+exit_code=$?
 # Run unit tests
-PYTHONPATH=./ pytest test/ --cov-report= --cov-config=.coveragerc --cov=src/ -s -v --log-level=10
+PYTHONPATH=./ pytest test/catalog/ --cov-report= --cov-config=.coveragerc --cov=src/ -s -v --log-level=10
 
+if [ $exit_code -ne 0 ]; 
+then 
+    exit $exit_code 
+else 
+    exit 0 
+fi
