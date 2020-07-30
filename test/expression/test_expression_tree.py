8782cd90e39be497a3095a1b23352c5fb16923ab
GTK <gaurav21776@gmail.com>
2020-07-29 20:39:24 -0400

    Prediction class removed

--- test/expression/test_expression_tree.py
+++ test/expression/test_expression_tree.py
@@ -15,12 +15,13 @@
 import unittest
 
 import numpy as np
+import pandas as pd
 
 from src.expression.abstract_expression import ExpressionType
 from src.expression.comparison_expression import ComparisonExpression
 from src.expression.constant_value_expression import ConstantValueExpression
 from src.expression.function_expression import FunctionExpression
-from src.models.inference.classifier_prediction import Prediction
+from src.models.inference.outcome import Outcome
 from src.models.storage.batch import Batch
 from src.models.storage.frame import Frame
 
@@ -29,8 +30,10 @@ class ExpressionEvaluationTest(unittest.TestCase):
     def test_func_expr_with_cmpr_and_const_expr_should_work(self):
         frame_1 = Frame(1, np.ones((1, 1)), None)
         frame_2 = Frame(1, 2 * np.ones((1, 1)), None)
-        outcome_1 = Prediction(frame_1, ["car", "bus"], [0.5, 0.6])
-        outcome_2 = Prediction(frame_1, ["bus"], [0.6])
+        outcome_1 = Outcome(pd.DataFrame(
+            {'labels': ["car", "bus"], 'scores': [0.5, 0.6]}), 'labels')
+        outcome_2 = Outcome(pd.DataFrame(
+            {'labels': ["bus"], 'scores': [0.6]}), 'labels')
 
         func = FunctionExpression(lambda x: [outcome_1, outcome_2])
         value_expr = ConstantValueExpression("car")
