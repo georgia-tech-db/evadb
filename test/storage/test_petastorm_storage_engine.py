d9a07f5b9327a052f32cdaf8019bf39c5a8a49dc
GTK <gaurav21776@gmail.com>
2020-07-31 22:22:35 -0400

    Migrated to new storage engine

--- test/storage/test_petastorm_storage_engine.py
+++ test/storage/test_petastorm_storage_engine.py
@@ -12,145 +12,89 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import os
+import shutil
 import unittest
 
-import cv2
 import numpy as np
+import pandas as pd
 
 from src.catalog.models.df_metadata import DataFrameMetadata
 from src.storage.petastorm_storage_engine import PetastormStorageEngine
-from src.readers.opencv_reader import OpenCVReader
-from test.util import custom_list_of_dicts_equal
 from src.catalog.models.df_column import DataFrameColumn
 from src.catalog.column_type import ColumnType
+from src.models.storage.batch import Batch
 
 NUM_FRAMES = 10
 
 
 class PetastormStorageEngineTest(unittest.TestCase):
 
-    def create_dummy_frames(self, num_frames=NUM_FRAMES, filters=[]):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.table = None
+
+    def create_dummy_batch(self, num_frames=NUM_FRAMES, filters=[]):
         if not filters:
             filters = range(num_frames)
+        data = []
         for i in filters:
-            yield {'id': i,
-                   'frame_data': np.array(np.ones((2, 2, 3))
+            data.append({'id': i,
+                         'data': np.array(np.ones((2, 2, 3))
                                           * 0.1 * float(i + 1) * 255,
-                                          dtype=np.uint8)}
+                                          dtype=np.uint8)})
+        return Batch(pd.DataFrame(data))
 
     def create_sample_table(self):
-        table_info = DataFrameMetadata("dataset_1", 'dummy.avi')
+        table_info = DataFrameMetadata("dataset", 'dataset')
         column_1 = DataFrameColumn("id", ColumnType.INTEGER, False)
         column_2 = DataFrameColumn(
-            "frame_data", ColumnType.NDARRAY, False, [
+            "data", ColumnType.NDARRAY, False, [
                 2, 2, 3])
         table_info.schema = [column_1, column_2]
         return table_info
 
-    def create_sample_video(self):
-        try:
-            os.remove('dummy.avi')
-        except FileNotFoundError:
-            pass
-
-        out = cv2.VideoWriter('dummy.avi',
-                              cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10,
-                              (2, 2))
-        for i in range(NUM_FRAMES):
-            frame = np.array(np.ones((2, 2, 3)) * 0.1 * float(i + 1) * 255,
-                             dtype=np.uint8)
-            out.write(frame)
-
     def setUp(self):
-        self.create_sample_video()
+        self.table = self.create_sample_table()
 
     def tearDown(self):
-        os.remove('dummy.avi')
+        try:
+            shutil.rmtree('dataset', ignore_errors=True)
+        except ValueError:
+            pass
 
     def test_should_create_empty_table(self):
-        table_info = self.create_sample_table()
         petastorm = PetastormStorageEngine()
-        petastorm.create(table_info)
-        row_iter = petastorm.read(table_info)
-        self.assertFalse(any(True for _ in row_iter))
+        petastorm.create(self.table)
+        records = list(petastorm.read(self.table))
+        self.assertEqual(records, [])
 
-    def test_should_return_equivalent_frames(self):
-        table_info = self.create_sample_table()
-        dummy_frames = list(self.create_dummy_frames())
+    def test_should_write_rows_to_table(self):
+        dummy_batch = self.create_dummy_batch()
 
         petastorm = PetastormStorageEngine()
-        petastorm.create(table_info)
-        petastorm.write_row(table_info, dummy_frames)
+        petastorm.create(self.table)
+        petastorm.write(self.table, dummy_batch)
 
-        expected_rows = list(petastorm.read(table_info))
-        self.assertEqual(len(expected_rows), NUM_FRAMES)
-        self.assertTrue(
-            custom_list_of_dicts_equal(
-                dummy_frames,
-                expected_rows))
-
-    def test_should_return_spefic_frame(self):
-        table_info = self.create_sample_table()
-        dummy_frames = list(self.create_dummy_frames())
-
-        petastorm = PetastormStorageEngine()
-        petastorm.create(table_info)
-        petastorm.write_row(table_info, dummy_frames)
-
-        return_rows = list(
-            petastorm.read_pos(
-                table_info, "id", [
-                    NUM_FRAMES / 2]))
-        expected_rows = list(
-            self.create_dummy_frames(
-                filters=[
-                    NUM_FRAMES / 2]))
-        self.assertEqual(len(return_rows), 1)
-        self.assertTrue(custom_list_of_dicts_equal(return_rows, expected_rows))
+        read_batch = list(petastorm.read(self.table))
+        self.assertEqual(len(read_batch), 1)
+        self.assertTrue(read_batch, dummy_batch)
 
     def test_should_return_even_frames(self):
-        table_info = self.create_sample_table()
-        dummy_frames = list(self.create_dummy_frames())
+        dummy_batch = self.create_dummy_batch()
 
         petastorm = PetastormStorageEngine()
-        petastorm.create(table_info)
-        petastorm.write_row(table_info, dummy_frames)
+        petastorm.create(self.table)
+        petastorm.write(self.table, dummy_batch)
 
-        return_rows = list(
-            petastorm.read_lambda(
-                table_info,
+        read_batch = list(
+            petastorm.read(
+                self.table,
                 ["id"],
                 lambda id: id %
                 2 == 0))
-        expected_rows = list(
-            self.create_dummy_frames(
-                filters=[
-                    i for i in range(NUM_FRAMES) if i %
-                    2 == 0]))
-        self.assertEqual(len(return_rows), len(expected_rows))
-        self.assertTrue(custom_list_of_dicts_equal(return_rows, expected_rows))
-
-    def test_should_return_equivalent_frames_by_videoloader(self):
-        """
-        This is an integration test with videoloader.
-        """
-        table_info = self.create_sample_table()
-
-        video_loader = OpenCVReader(table_info.file_url)
-        id = 0
-        rows = []
-        for batch in video_loader.read():
-            for frame in batch:
-                rows.append({"id": id, "frame_data": frame})
-                id += 1
-
-        petastorm = PetastormStorageEngine()
-        petastorm.create(table_info)
-        petastorm.write_row(table_info, rows)
-
-        dummy_frames = list(self.create_dummy_frames())
-        return_rows = list(petastorm.read(table_info))
-
-        self.assertEqual(len(return_rows), NUM_FRAMES)
-        self.assertTrue(custom_list_of_dicts_equal(dummy_frames, return_rows))
+        expected_batch = self.create_dummy_batch(
+            filters=[
+                i for i in range(NUM_FRAMES) if i %
+                2 == 0])
+        self.assertEqual(len(read_batch), 1)
+        self.assertTrue(read_batch, expected_batch)
