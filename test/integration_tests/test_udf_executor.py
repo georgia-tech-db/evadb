6157121db15647800e127d54e43d077eb8d2e146
xzdandy <xzdandy@gmail.com>
2020-08-10 14:54:58 -0400

    move example udfs and check duplicated column name when merging

--- test/integration_tests/test_udf_executor.py
+++ test/integration_tests/test_udf_executor.py
@@ -15,45 +15,10 @@
 import unittest
 import os
 import pandas as pd
-import numpy as np
 
 from src.catalog.catalog_manager import CatalogManager
 from src.models.storage.batch import Batch
-from src.models.catalog.frame_info import FrameInfo
-from src.models.catalog.properties import ColorSpace
-from src.models.inference.outcome import Outcome
-from src.udfs.abstract_udfs import AbstractClassifierUDF
 from test.util import create_sample_video, perform_query
-from typing import List
-
-
-class DummyObjectDetector(AbstractClassifierUDF):
-
-    @property
-    def name(self) -> str:
-        return "dummyObjectDetector"
-
-    def __init__(self):
-        super().__init__()
-
-    @property
-    def input_format(self) -> FrameInfo:
-        return FrameInfo(-1, -1, 3, ColorSpace.RGB)
-
-    @property
-    def labels(self) -> List[str]:
-        return ['__background__', 'person', 'bicycle']
-
-    def classify(self, frames: np.ndarray) -> List[Outcome]:
-        if (frames == np.array(np.ones((2, 2, 3)) * 0.1 * float(5 + 1) * 255,
-                               dtype=np.uint8)).all():
-            label = self.labels[1]
-        else:
-            label = self.labels[2]
-        prediction_df_list = [Outcome(
-            pd.DataFrame([{'label': [label, 'apple']}]), 'label')]
-        return prediction_df_list
-
 
 NUM_FRAMES = 10
 
@@ -77,7 +42,7 @@ class UDFExecutorTest(unittest.TestCase):
                   INPUT  (Frame_Array NDARRAY (3, 256, 256))
                   OUTPUT (Labels NDARRAY (10))
                   TYPE  Classification
-                  IMPL  'test/integration_tests/test_udf_executor.py';
+                  IMPL  'test/example_udfs.py';
         """
         perform_query(create_udf_query)
 
