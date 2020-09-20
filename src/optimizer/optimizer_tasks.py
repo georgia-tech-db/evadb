df2e945409f3a09d595e53ee0b6432412cf06676
GTK <gaurav21776@gmail.com>
2020-09-20 05:13:06 -0400

    checkpoint OExpr, OGroup work in progress

--- src/optimizer/optimizer_tasks.py
+++ src/optimizer/optimizer_tasks.py
@@ -24,6 +24,7 @@ class OptimizerTaskType(IntEnum):
     """Manages Enum for all the supported optimizer tasks
     """
     TOP_DOWN_REWRITE = auto()
+    OPTIMIZE_EXPRESSION = auto()
 
 
 class OptimizerTask:
@@ -44,17 +45,27 @@ class OptimizerTask:
     def optimizer_context(self):
         return self._optimizer_context
 
+    @root_expr.setter
+    def root_expr(self, expr):
+        self.root_expr = expr
+
     def execute(self):
         raise NotImplementedError
-
-    def push_task(self, task):
-        self.optimizer_context.task_stack.push_task(task)
 
 
 class TopDownRewrite(OptimizerTask):
     def __init__(self, root_expr: GroupExpression,
                  optimizer_context: OptimizerContext):
-        super().__init__(root_expr, optimizer_context, OptimizerTaskType.TOP_DOWN_REWRITE)
+        super().__init__(root_expr, root_expr.group_id,
+                         optimizer_context, OptimizerTaskType.TOP_DOWN_REWRITE)
+
+    @property
+    def root_expr(self):
+        return self._root_expr
+
+    @root_expr.setter
+    def root_expr(self, expr):
+        self._root_expr = expr
 
     def execute(self):
         """We apply rewrite rules in a top down fashion.
@@ -68,18 +79,52 @@ class TopDownRewrite(OptimizerTask):
         for rule in rewrite_rules:
             if rule.top_match(self.root_expr.opr):
                 valid_rules.append(rule)
-        
+
         # sort the rules by promise
         sorted(valid_rules, key=lambda x: x.promise(), reverse=True)
-        
         for rule in valid_rules:
-            binder = Binder(self.root_expr, rule.pattern, self.optimizer_context.memo)
+            binder = Binder(self.root_expr, rule.pattern,
+                            self.optimizer_context.memo)
             for match in iter(binder):
                 after = rule.apply(match, self.optimizer_context)
                 new_expr = GroupExpression(after, self.root_expr.group_id)
-                self.optimizer_context.memo.replace_group_expr(root_expr.group_id, new_expr)
-                # self.push_task(TopDownRewrite(new_expr, self.optimizer_context))
-                TopDownRewrite(new_expr, optimizer_context)
+                self.root_expr = new_expr
+                self.optimizer_context.memo.replace_group_expr(
+                    self.root_expr.group_id, new_expr)
+                self.optimizer_context.task_stack.push(TopDownRewrite(
+                    new_expr, new_expr.group_id, self.optimizer_context))
 
-        # for child in grp_expr.children:
-        #     # self.optimizer_context.task_stack.push_task(TopDownRewrite(child, self.optimizer_context))
+        for child in self.root_expr.children:
+            child_expr = self.optimizer_context.memo.get_group(
+                child).logical_exprs[0]
+            self.optimizer_context.task_stack.push(TopDownRewrite(
+                child_expr, child_expr.group_id, self.optimizer_context))
+
+
+class OptimizeExpression(OptimizerTask):
+    def __init__(self, root_expr, optimizer_context):
+        super().__init__(root_expr, root_expr.group_id,
+                         optimizer_context, OptimizerTaskType.OPTIMIZE_EXPRESSION)
+
+    def execute(self):
+        implementation_rules = RulesManager().implementation_rules
+        valid_rules = []
+        for rule in implementation_rules:
+            if rule.top_match(self.root_expr.opr):
+                valid_rules.append(rule)
+
+        sorted(valid_rules, key=lambda x: x.promise(), reverse=True)
+        for rule in valid_rules:
+            binder = Binder(self.root_expr, rule.pattern,
+                            self.optimizer_context.memo)
+            for match in iter(binder):
+                after = rule.apply()
+                new_expr = GroupExpression(
+                    after, self.root_expr.group_id, self.root_expr.children)
+                self.optimizer_context.memo.add_group_expr(new_expr)
+    
+            # Optimize the child groups
+    
+            # for child_id in self.root_expr.children:
+            #     child_expr = self.optimizer_context.task_stack.push(
+            #         OptimizeGroup(None, child_id, self.optimizer_context, ))
