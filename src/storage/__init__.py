a4a62657de28a556a52609efa8e2253e7ffba54d
xzdandy <xzdandy@gmail.com>
2020-10-23 13:17:47 -0400

    local changes

--- src/storage/__init__.py
+++ src/storage/__init__.py
@@ -12,6 +12,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""
+storage subdirectory
+"""
 from src.configuration.configuration_manager import ConfigurationManager
 from src.utils.generic_utils import str_to_class
 
