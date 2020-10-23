f914c47257eb85ac70be9a01443a49a7ad94d455
xzdandy <xzdandy@gmail.com>
2020-10-23 14:51:37 -0400

    1. move the StorageEngine outof __init__ file 2. Add dependency

--- api-docs/conf.py
+++ api-docs/conf.py
@@ -14,10 +14,11 @@ import warnings
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('..'))
-sys.path.insert(0, os.path.abspath('../src2/'))
-sys.path.insert(0, os.path.abspath('../src/'))
+sys.path.append(os.path.abspath('.'))
+sys.path.append(os.path.abspath('../'))
+
+#sys.path.insert(0, os.path.abspath('../src2/'))
+#sys.path.insert(0, os.path.abspath('../src/'))
 
 print('Sys paths:')
 for pathx in sys.path:
