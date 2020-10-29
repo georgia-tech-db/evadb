a29a8cc62cfa7aebd08090194d06b8dd528f5769
GTK <gaurav21776@gmail.com>
2020-10-29 16:32:04 -0400

    optimizer supports create materialized view

--- src/optimizer/operators.py
+++ src/optimizer/operators.py
@@ -17,6 +17,7 @@ from typing import List
 
 from src.catalog.models.df_metadata import DataFrameMetadata
 from src.parser.table_ref import TableRef
+from src.parser.create_statement import ColumnDefinition
 from src.expression.abstract_expression import AbstractExpression
 from src.catalog.models.df_column import DataFrameColumn
 from src.catalog.models.udf_io import UdfIO
@@ -81,7 +82,8 @@ class Operator:
 
     def is_logical(self):
         return self._opr_type < OperatorType.LOGICALDELIMITER
-    
+
+
 class Dummy(Operator):
     def __init__(self):
         super().__init__(OperatorType.DUMMY, None)
@@ -115,11 +117,11 @@ class LogicalGet(Operator):
     @property
     def select_list(self):
         self._select_list
-    
+
     @select_list.setter
     def select_list(self, select_list):
         self._select_list = select_list
-    
+
     def __eq__(self, other):
         is_subtree_equal = super().__eq__(other)
         if not isinstance(other, LogicalGet):
@@ -402,13 +404,16 @@ class LogicalCreateMaterializedView(Operator):
 
     Arguments:
         view {TableRef}: [view table that is to be created]
+        col_list{List[ColumnDefinition]} -- column names in the view
         if_not_exists {bool}: [whether to override if view exists]
     """
 
-    def __init__(self, view: TableRef,
+    def __init__(self, view: TableRef, col_list: List[ColumnDefinition],
                  if_not_exists: bool = False, children=None):
-        super().__init__(OperatorType.LOGICAL_CREATE_MATERIALIZED_VIEW, children)
+        super().__init__(OperatorType.LOGICAL_CREATE_MATERIALIZED_VIEW,
+                         children)
         self._view = view
+        self._col_list = col_list
         self._if_not_exists = if_not_exists
 
     @property
@@ -419,10 +424,15 @@ class LogicalCreateMaterializedView(Operator):
     def if_not_exists(self):
         return self._if_not_exists
 
+    @property
+    def col_list(self):
+        return self._col_list
+    
     def __eq__(self, other):
         is_subtree_equal = super().__eq__(other)
         if not isinstance(other, LogicalCreateMaterializedView):
             return False
         return (is_subtree_equal
                 and self.view == other.view
+                and self.col_list == other.col_list
                 and self.if_not_exists == other.if_not_exists)
