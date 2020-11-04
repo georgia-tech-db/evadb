9a48c2c037c8ff47a20d7f77fc9ca35592d5332b
xzdandy <xzdandy@gmail.com>
2020-11-04 15:48:16 -0500

    merge master and add an intergeration test for materlized view.

---
+++ test/integration_tests/test_mat_executor.py
@@ -0,0 +1,51 @@
+# coding=utf-8
+# Copyright 2018-2020 EVA
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import unittest
+import pandas as pd
+
+from src.catalog.catalog_manager import CatalogManager
+from test.util import perform_query
+from src.models.storage.batch import Batch
+
+
+class MaterializedViewTest(unittest.TestCase):
+
+    def setUp(self):
+        CatalogManager().reset()
+
+    def test_should_materlizied_view(self):
+        query = """LOAD DATA INFILE 'data/ua_detrac/ua_detrac.mp4'
+                   INTO MyVideo;"""
+        perform_query(query)
+
+        create_udf_query = """CREATE UDF FastRCNNObjectDetector
+                  INPUT  (Frame_Array NDARRAY (3, 256, 256))
+                  OUTPUT (label TEXT(10))
+                  TYPE  Classification
+                  IMPL  'src/udfs/fastrcnn_object_detector.py';
+        """
+        perform_query(create_udf_query)
+
+        select_query = """SELECT id FROM MyVideo WHERE id < 5;"""
+        query = 'CREATE MATERIALIZED VIEW uadtrac_fastRCNN (id) AS {}' \
+            .format(select_query)
+        perform_query(select_query)
+
+        query = 'SELECT id FROM uadtrac_fastRCNN;'
+        actual_batch = perform_query(select_query)
+        expected_batch = Batch(
+            frames=pd.DataFrame([{'id': i} for i in range(5)])
+        )
+        self.assertEqual(actual_batch, expected_batch)
