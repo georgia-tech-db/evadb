88cfde74e0443b4c9623ec316624b7aeda6f16af
xzdandy <xzdandy@gmail.com>
2020-11-16 18:37:53 -0500

    buff and protocol

--- eva_db_api.py
+++ eva_db_api.py
@@ -1,8 +1,10 @@
 import asyncio
 import random
 
+from typing import List
 from src.server.client import EvaClient
 from src.models.server.response import Response
+
 
 class EVAConnection:
     def __init__(self, transport, protocol):
@@ -12,41 +14,32 @@ class EVAConnection:
     def cursor(self):
         return EVACursor(self._protocol)
 
+
 class EVACursor:
 
     query_result = None
 
     def __init__(self, protocol):
-        self.valid = True
-        self.next_chunk = 0
         self._protocol = protocol
-        self.next_row = 0
 
-    async def execute (self, query):
-        self._protocol._response_chunks = []
-        self._protocol.send_message(query)
-        print("Message Sent")
-        while len(self._protocol._response_chunks) == 0:
-            await asyncio.sleep(2)
+    async def execute(self, query: str):
+        await self._protocol.send_message(query)
 
-        segs = self._protocol._response_chunks[0].split('|', 1)
-        result_length = int(segs[0])
-        self.query_result = segs[1]
-        next_chunk = 1
-        while len(self.query_result) < result_length:
-            while len(self._protocol._response_chunks) <= next_chunk:
-                await asyncio.sleep(2)
-            self.query_result += self._protocol._response_chunks[next_chunk]
-            next_chunk += 1
-
-    def fetch_one(self):
-        r = Response.from_json(self.query_result)
-        row_data = r._batch._frames.loc[self.next_row]
-        return row_data
+    async def fetch_one(self) -> Response:
+        """
+        fetch_one returns one batch instead of one row for now.
+        """
+        try:
+            message = await self._protocol.queue.get()
+            response = await asyncio.coroutine(Response.from_json)(message)
+        except Exception as e:
+            raise e
+        return response
 
 
-async def connect (host: str, port: int, max_retry_count: int = 3):
-    loop = asyncio.get_event_loop()
+async def connect(host: str, port: int, max_retry_count: int = 3, loop=None):
+    if loop is None:
+        loop = asyncio.get_event_loop()
 
     retries = max_retry_count * [1]
 
@@ -65,22 +58,20 @@ async def connect (host: str, port: int, max_retry_count: int = 3):
     return EVAConnection(transport, protocol)
 
 ###USAGE###
-'''
-async def run(query: str):
+
+
+async def run(query: List[str]):
     hostname = '0.0.0.0'
     port = 5432
 
-    loop = asyncio.get_event_loop()
     connection = await connect(hostname, port)
     cursor = connection.cursor()
-    await cursor.execute(query)
-    #cursor.fetch_one()
+    for onequery in query:
+        await cursor.execute(onequery)
+        response = await cursor.fetch_one()
+        print('Query: %s' % onequery)
+        print(response)
 
-asyncio.run(run('LOAD DATA INFILE "data/ua_detrac/ua_detrac.mp4" INTO MyVideo;'))
-asyncio.run(run('SELECT id,data FROM MyVideo WHERE id < 5;'))
-'''
-
-
-
-
-
+asyncio.run(run(['INVALID QUERY',
+                 'LOAD DATA INFILE "data/ua_detrac/ua_detrac.mp4" INTO MyVideo;',
+                 'SELECT id,data FROM MyVideo WHERE id < 5;']))
