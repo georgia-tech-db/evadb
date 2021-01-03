0671a4dbf0c56b66bc6fd0f8b2cb7a8e80e79a95
xzdandy <xzdandy@gmail.com>
2021-01-03 14:08:13 -0500

    Simplify the mat test

--- test/integration_tests/test_mat_executor.py
+++ test/integration_tests/test_mat_executor.py
@@ -12,20 +12,60 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import os
 import unittest
 import pandas as pd
 
 from src.catalog.catalog_manager import CatalogManager
-from test.util import perform_query
 from src.models.storage.batch import Batch
+from test.util import create_sample_video, create_dummy_batches, perform_query
+from test.util import DummyObjectDetector
+
+NUM_FRAMES = 10
 
 
 class MaterializedViewTest(unittest.TestCase):
 
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
+        super(MaterializedViewTest, cls).setUpClass()
         CatalogManager().reset()
+        create_sample_video(NUM_FRAMES)
 
-    def test_should_materlizied_view(self):
+    @classmethod
+    def tearDownClass(cls):
+        os.remove('dummy.avi')
+
+    def test_should_mat_view_with_dummy(self):
+        load_query = """LOAD DATA INFILE 'dummy.avi' INTO MyVideo;"""
+        perform_query(load_query)
+
+        create_udf_query = """CREATE UDF DummyObjectDetector
+                  INPUT  (Frame_Array NDARRAY (3, 256, 256))
+                  OUTPUT (label TEXT(10))
+                  TYPE  Classification
+                  IMPL  'test/util.py';
+        """
+        perform_query(create_udf_query)
+
+        materialized_query = """CREATE MATERIALIZED VIEW dummy_view (id, label)
+            AS SELECT id, DummyObjectDetector(data).label FROM MyVideo;
+        """
+        perform_query(materialized_query)
+
+        select_query = 'SELECT id, label FROM dummy_view;'
+        actual_batch = perform_query(select_query)
+        actual_batch.sort()
+
+        labels = DummyObjectDetector().labels
+        # BUG: materialized view turns the str to bstr.
+        expected = [{'id': i, 'label': labels[1 + i % 2].encode()}
+                    for i in range(NUM_FRAMES)]
+        expected_batch = Batch(frames=pd.DataFrame(expected))
+        self.assertEqual(actual_batch, expected_batch)
+
+    @unittest.skip('Too slow when no GPU')
+    def test_should_mat_view_with_fastrcnn(self):
         query = """LOAD DATA INFILE 'data/ua_detrac/ua_detrac.mp4'
                    INTO MyVideo;"""
         perform_query(query)
@@ -43,10 +83,9 @@ class MaterializedViewTest(unittest.TestCase):
             .format(select_query)
         perform_query(query)
 
-        query = 'SELECT id, label FROM uadtrac_fastRCNN;'
         actual_batch = perform_query(select_query)
         actual_batch.sort()
         expected_batch = Batch(
-                frames=pd.DataFrame([{'id': i, 'label': 'person'} for i in range(5)])
+            frames=pd.DataFrame([{'id': i, 'label': 'person'} for i in range(5)])
         )
         self.assertEqual(actual_batch, expected_batch)
