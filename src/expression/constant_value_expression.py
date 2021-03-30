34aea4246c2e7fa6a6727352606e56b52e6d3097
Gaurav Tarlok Kakkar <gaurav21776@gmail.com>
2021-03-30 01:36:05 -0400

    constant value expression needs the value type at initialization

--- src/expression/constant_value_expression.py
+++ src/expression/constant_value_expression.py
@@ -13,10 +13,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import pandas as pd
-
+from typing import Any
 from src.expression.abstract_expression import AbstractExpression, \
     ExpressionType
 from src.models.storage.batch import Batch
+from src.catalog.column_type import ColumnType
 
 
 class ConstantValueExpression(AbstractExpression):
@@ -24,9 +25,10 @@ class ConstantValueExpression(AbstractExpression):
     # for now we don't assign any class to value
     # it can have types like string, int etc
     # return type not set, handle that based on value
-    def __init__(self, value):
+    def __init__(self, value: Any, v_type: ColumnType = ColumnType.INTEGER):
         super().__init__(ExpressionType.CONSTANT_VALUE)
         self._value = value
+        self._v_type = v_type
 
     def evaluate(self, *args, **kwargs):
         return Batch(pd.DataFrame({0: [self._value]}))
@@ -34,6 +36,10 @@ class ConstantValueExpression(AbstractExpression):
     @property
     def value(self):
         return self._value
+
+    @property
+    def v_type(self):
+        return self._v_type
     # ToDo implement other functinalities like maintaining hash
     # comparing two objects of this class(==)
 
@@ -41,5 +47,11 @@ class ConstantValueExpression(AbstractExpression):
         is_subtree_equal = super().__eq__(other)
         if not isinstance(other, ConstantValueExpression):
             return False
-        return (is_subtree_equal
-                and self.value == other.value)
+        # if the value type is NDARRAY, we need to reduce the
+        # iterator to bool
+        is_equal = is_subtree_equal and self.v_type == other.v_type
+
+        if self.v_type == ColumnType.NDARRAY:
+            return is_equal and all(self.value == other.value)
+        else:
+            return is_equal and self.value == other.value
