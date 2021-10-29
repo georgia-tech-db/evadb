3a3b0fab99460de182a8730b5c4ba503f6ca8efb
Suryatej Reddy <suryatej16102@iiitd.ac.in>
2021-10-29 17:37:52 -0400

    imports getting installed

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+setuptools.setup(
+    install_requires=[
+        "numpy==1.20.1",
+        "opencv-python==4.5.1.48",
+        # "scikit-learn==0.23.2",
+        "pandas==1.2.3",
+        "torch",
+        "torchvision==0.8.2",
+        "pillow",
+        "sqlalchemy==1.3.20",
+        "pymysql==0.10.1",
+        "sqlalchemy-utils==0.36.6",
+        "pytest-asyncio==0.14.0",
+        "pyspark==3.0.2",
+        "petastorm==0.9.8"
+    ]
+)
