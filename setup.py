80db00f68ce2fde186f9f151819ba0f9f4bdfc1b
Suryatej Reddy <suryatej16102@iiitd.ac.in>
2021-10-29 19:00:32 -0400

    tested setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
         "opencv-python==4.5.1.48",
         # "scikit-learn==0.23.2",
         "pandas==1.2.3",
-        "torch",
+        "torch==1.7.1",
         "torchvision==0.8.2",
         "pillow",
         "sqlalchemy==1.3.20",
@@ -13,6 +13,13 @@ setuptools.setup(
         "sqlalchemy-utils==0.36.6",
         "pytest-asyncio==0.14.0",
         "pyspark==3.0.2",
-        "petastorm==0.9.8"
-    ]
+        "petastorm==0.9.8",
+        "antlr4-python3-runtime==4.8",
+        "pyyaml"
+    ],
+    entry_points={
+        'console_scripts': [
+            'eva=eva.eva_server:main',
+        ],
+    },
 )
