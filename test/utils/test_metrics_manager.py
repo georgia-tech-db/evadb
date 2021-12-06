7017b0df59e32ad5873ab63476a6566b7fe6b89f
Elijah Oyekunle <eloyekunle@gmail.com>
2021-12-05 21:30:04 -0500

    add unit test

--- test/utils/test_metrics_manager.py
+++ test/utils/test_metrics_manager.py
@@ -1,10 +1,30 @@
 import unittest
 
+from src.catalog.catalog_manager import CatalogManager
+from src.server.command_handler import execute_query_fetch_all
+from src.utils.metrics_manager import MetricsManager
+from test.util import create_sample_video, file_remove
+
+NUM_FRAMES = 10
+
 
 class MetricsManagerTests(unittest.TestCase):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+
+    @classmethod
+    def setUpClass(cls):
+        CatalogManager().reset()
+        create_sample_video(NUM_FRAMES)
+        load_query = """LOAD DATA INFILE 'dummy.avi' INTO MyVideo;"""
+        execute_query_fetch_all(load_query)
+
+    @classmethod
+    def tearDownClass(cls):
+        file_remove('dummy.avi')
 
     def test_latency(self):
-        # todo
-        return
+        mm = MetricsManager()
+
+        select_query = "SELECT id, data FROM MyVideo WHERE id < 5;"
+        execute_query_fetch_all(select_query, mm)
+
+        self.assertEqual(mm.print().keys(), ["latency"])
