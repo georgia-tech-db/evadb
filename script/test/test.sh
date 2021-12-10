cd33b5d394531e1ceb3c7c38f67bbc2ed12e15d4
Suryatej Reddy <suryatej16102@iiitd.ac.in>
2021-12-09 19:02:33 -0500

    updates test script

--- script/test/test.sh
+++ script/test/test.sh
@@ -4,7 +4,7 @@
 # linter fails or the pytest fails. This is important for the Travis.
 
 # Run linter (checks code style)
-flake8 --select E,F src/ test/ --exclude src/filters,src/parser/evaql
+flake8 --select E,F eva/ test/ --exclude src/filters,src/parser/evaql
 linter_code=$?
 # Run unit tests
 PYTHONPATH=./ pytest test/ --cov-report= --cov-config=.coveragerc --cov=src/ -s -v --log-level=WARNING
