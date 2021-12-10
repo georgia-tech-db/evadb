0950b10eca9ae23ac3cba763fefcc0e4820a02f3
Gaurav Tarlok Kakkar <gaurav21776@gmail.com>
2021-12-09 20:33:02 -0500

    Update test.sh

--- script/test/test.sh
+++ script/test/test.sh
@@ -4,10 +4,10 @@
 # linter fails or the pytest fails. This is important for the Travis.
 
 # Run linter (checks code style)
-flake8 --select E,F eva/ test/ --exclude src/filters,src/parser/evaql
+flake8 --select E,F eva/ test/ --exclude eva/filters,eva/parser/evaql
 linter_code=$?
 # Run unit tests
-PYTHONPATH=./ pytest test/ --cov-report= --cov-config=.coveragerc --cov=src/ -s -v --log-level=WARNING
+PYTHONPATH=./ pytest test/ --cov-report= --cov-config=.coveragerc --cov=eva/ -s -v --log-level=WARNING
 test_code=$?
 if [ $linter_code -ne 0 ];
 then
