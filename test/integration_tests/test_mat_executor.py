0e9e6091fa41f8be0b239310601b9bd4397dd67a
Gaurav Tarlok Kakkar <gaurav21776@gmail.com>
2022-01-17 10:35:10 -0500

    mat view integration test added

--- test/integration_tests/test_mat_executor.py
+++ test/integration_tests/test_mat_executor.py
@@ -18,7 +18,8 @@ import pandas as pd
 from src.catalog.catalog_manager import CatalogManager
 from src.models.storage.batch import Batch
 from src.server.command_handler import execute_query_fetch_all
-from test.util import create_sample_video, file_remove
+from test.util import create_sample_video, file_remove, \
+    copy_sample_video_to_prefix
 from test.util import DummyObjectDetector
 
 NUM_FRAMES = 10
@@ -29,6 +30,7 @@ class MaterializedViewTest(unittest.TestCase):
         # reset the catalog manager before running each test
         CatalogManager().reset()
         create_sample_video()
+        copy_sample_video_to_prefix()
         load_query = """LOAD DATA INFILE 'dummy.avi' INTO MyVideo;"""
         execute_query_fetch_all(load_query)
 
@@ -42,6 +44,7 @@ class MaterializedViewTest(unittest.TestCase):
 
     def tearDown(self):
         file_remove('dummy.avi')
+        file_remove('ua_detrac.mp4')
 
     def test_should_mat_view_with_dummy(self):
         materialized_query = """CREATE MATERIALIZED VIEW dummy_view (id, label)
@@ -84,32 +87,34 @@ class MaterializedViewTest(unittest.TestCase):
         expected_batch = Batch(frames=pd.DataFrame(expected))
         self.assertEqual(actual_batch, expected_batch)
 
-    @unittest.skip('Too slow when no GPU')
-    def test_should_mat_view_with_fastrcnn(self):
-        query = """LOAD DATA INFILE 'data/ua_detrac/ua_detrac.mp4'
+    # @unittest.skip('Too slow when no GPU')
+    def test_should_mat_view_with_aafastrcnn(self):
+        query = """LOAD DATA INFILE 'ua_detrac.mp4'
                    INTO MyVideo;"""
         execute_query_fetch_all(query)
 
         create_udf_query = """CREATE UDF FastRCNNObjectDetector
-                  INPUT  (Frame_Array NDARRAY (3, 256, 256))
-                  OUTPUT (label TEXT(10), pred_score INTEGER,
-                            pre_boxes NDARRAY(4))
+                  INPUT  (frame NDARRAY UINT8(3, ANYDIM, ANYDIM))
+                  OUTPUT (labels NDARRAY STR(ANYDIM),
+                          bboxes NDARRAY FLOAT32(ANYDIM, 4),
+                          scores NDARRAY FLOAT32(ANYDIM))
                   TYPE  Classification
                   IMPL  'src/udfs/fastrcnn_object_detector.py';
         """
         execute_query_fetch_all(create_udf_query)
 
-        select_query = """SELECT id, FastRCNNObjectDetector(data).label
+        select_query = """SELECT id, FastRCNNObjectDetector(data).labels
                             FROM MyVideo WHERE id < 5;"""
-        query = 'CREATE MATERIALIZED VIEW uadtrac_fastRCNN (id, label) AS {}' \
-            .format(select_query)
+        query = '''CREATE MATERIALIZED VIEW uadtrac_fastRCNN (id, labels) \
+        AS {}'''.format(select_query)
         execute_query_fetch_all(query)
 
-        select_view_query = 'SELECT id, label FROM uadtrac_fastRCNN'
+        select_view_query = 'SELECT id, labels FROM uadtrac_fastRCNN'
         actual_batch = execute_query_fetch_all(select_view_query)
         actual_batch.sort()
-        expected_batch = Batch(
-            frames=pd.DataFrame([{'id': i, 'label': 'person'}
-                                 for i in range(5)])
-        )
-        self.assertEqual(actual_batch, expected_batch)
+
+        self.assertEqual(actual_batch.batch_size, 5)
+        # non-trivial test case
+        res = actual_batch.frames
+        for idx in res.index:
+            self.assertTrue('car' in res['labels'][idx])
