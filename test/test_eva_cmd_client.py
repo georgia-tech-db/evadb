eddeeac49452e18c1d01f3198fcdbf9512b6c97e
xzdandy <xzdandy@gmail.com>
2022-01-26 00:09:15 -0500

    Change the import location

--- test/test_eva_cmd_client.py
+++ test/test_eva_cmd_client.py
@@ -16,8 +16,6 @@
 import unittest
 import mock
 import sys
-from eva.eva_cmd_client import eva_client, main
-from eva.eva_cmd_client import parse_args
 
 
 class CMDClientTest(unittest.TestCase):
@@ -26,16 +24,19 @@ class CMDClientTest(unittest.TestCase):
 
     @mock.patch('eva.eva_cmd_client.eva_client')
     def test_main(self, mock_client):
+        from eva.eva_cmd_client import main
         with mock.patch.object(sys, 'argv', ['test']):
             main()
         mock_client.called_once_with('0.0.0.0', 5432)
 
     def test_parse_args(self):
+        from eva.eva_cmd_client import parse_args
         args = parse_args(['-P', '2345', '-H', 'test'])
         self.assertEqual(args.host, 'test')
         self.assertEqual(args.port, 2345)
-    
+
     @mock.patch('eva.server.interpreter.start_cmd_client')
     def test_eva_client(self, mock_client):
+        from eva.eva_cmd_client import eva_client
         eva_client()
         mock_client.assert_called_once()
