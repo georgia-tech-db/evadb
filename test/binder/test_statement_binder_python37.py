f55c8f3992cf3d2ed5836e5f8774e116977cab41
Gaurav Tarlok Kakkar <gaurav21776@gmail.com>
2022-05-31 21:37:26 -0400

    python 3.7 patch (#252)
    * python 3.7 patch
    
    Co-authored-by: xzdandy <xzdandy@gmail.com>

---
+++ test/binder/test_statement_binder_python37.py
@@ -0,0 +1,43 @@
+# coding=utf-8
+# Copyright 2018-2020 EVA
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import unittest
+import sys
+from unittest.mock import MagicMock, patch
+from eva.binder.statement_binder_context import StatementBinderContext
+
+
+class StatementBinderPython37Tests(unittest.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @patch.object(sys, 'version_info', (3, 7))
+    def test_bind_with_python37(self):
+        from eva.binder.statement_binder import StatementBinder
+        with patch.object(StatementBinder, 'bind') as mock_binder:
+            binder = StatementBinder(StatementBinderContext())
+            select_statement = MagicMock()
+            mocks = [MagicMock(), MagicMock(), MagicMock(), MagicMock()]
+            select_statement.target_list = mocks[:2]
+            select_statement.orderby_list = [(mocks[2], 0), (mocks[3], 0)]
+            binder._bind_select_statement(select_statement)
+            mock_binder.assert_any_call(select_statement.from_table)
+            mock_binder.assert_any_call(select_statement.where_clause)
+            mock_binder.assert_any_call(select_statement.union_link)
+            for mock in mocks:
+                mock_binder.assert_any_call(mock)
+
+
+
