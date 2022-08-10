4ad743f8348afb0d2f6d7a8f95321e7cca244ae1
Aubhro Sengupta <hello@aubhro.com>
2022-08-10 19:59:35 -0400

    Add gaussian_blur udf

---
+++ eva/udfs/gaussian_blur.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+# Copyright 2018-2022 EVA
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from torchvision import transforms as tv_transforms
+
+from eva.udfs.pytorch_abstract_udf import PytorchAbstractTransformationUDF
+
+class GaussianBlur(PytorchAbstractTransformationUDF, tv_transforms.GaussianBlur):
+    def __init__(self, kernel_size: int, sigma: float):
+        blur = tv_transforms.GaussianBlur(kernel_size=kernel_size, sigma=sigma)
+        PytorchAbstractTransformationUDF.__init__(blur)
