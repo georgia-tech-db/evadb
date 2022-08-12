8ef6bb79a306c78fcd68c988b876dc54c4a1cec6
Gaurav Tarlok Kakkar <gaurav21776@gmail.com>
2022-08-12 00:29:42 -0400

    test: fix testcase

--- test/expression/test_function_expression.py
+++ test/expression/test_function_expression.py
@@ -20,6 +20,7 @@ from mock import MagicMock, Mock, patch
 from eva.constants import NO_GPU
 from eva.expression.function_expression import FunctionExpression
 from eva.models.storage.batch import Batch
+from eva.parser.alias import Alias
 from eva.udfs.gpu_compatible import GPUCompatible
 
 
@@ -34,7 +35,9 @@ class FunctionExpressionTest(unittest.TestCase):
         mock_function.to_device.return_value = gpu_mock_function
         context_instance.gpu_device.return_value = gpu_device_id
 
-        expression = FunctionExpression(mock_function, name="test", alias="func_expr")
+        expression = FunctionExpression(
+            mock_function, name="test", alias=Alias("func_expr")
+        )
 
         input_batch = Batch(frames=pd.DataFrame())
         expression.evaluate(input_batch)
@@ -44,7 +47,9 @@ class FunctionExpressionTest(unittest.TestCase):
     def test_should_use_the_same_function_if_not_gpu_compatible(self):
         mock_function = MagicMock(return_value=pd.DataFrame())
 
-        expression = FunctionExpression(mock_function, name="test", alias="func_expr")
+        expression = FunctionExpression(
+            mock_function, name="test", alias=Alias("func_expr")
+        )
 
         input_batch = Batch(frames=pd.DataFrame())
         expression.evaluate(input_batch)
@@ -53,11 +58,15 @@ class FunctionExpressionTest(unittest.TestCase):
     @patch("eva.expression.function_expression.Context")
     def test_should_execute_same_function_if_no_gpu(self, context):
         context_instance = context.return_value
-        mock_function = MagicMock(spec=GPUCompatible, return_value=pd.DataFrame())
+        mock_function = MagicMock(
+            spec=GPUCompatible, return_value=pd.DataFrame()
+        )
 
         context_instance.gpu_device.return_value = NO_GPU
 
-        expression = FunctionExpression(mock_function, name="test", alias="func_expr")
+        expression = FunctionExpression(
+            mock_function, name="test", alias=Alias("func_expr")
+        )
 
         input_batch = Batch(frames=pd.DataFrame())
         expression.evaluate(input_batch)
