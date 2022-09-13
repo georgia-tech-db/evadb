429a281658780ea1291c3bc9b2a51568265e2e90
Aubhro Sengupta <hello@aubhro.com>
2022-09-12 21:07:46 -0400

    Run reformat script

--- eva/configuration/configuration_manager.py
+++ eva/configuration/configuration_manager.py
@@ -12,9 +12,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import yaml
-
 from typing import Any
+
+import yaml
 
 from eva.configuration.bootstrap_environment import bootstrap_environment
 from eva.configuration.constants import (
