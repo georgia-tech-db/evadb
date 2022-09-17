4bfcb30dbf5731455a1f379075987d866f08dcda
Aubhro Sengupta <hello@aubhro.com>
2022-09-17 19:23:45 -0400

    Reformat with black

--- test/configuration/test_configuration_manager.py
+++ test/configuration/test_configuration_manager.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import unittest
+
 import pytest
 
 from eva.configuration.configuration_manager import ConfigurationManager
