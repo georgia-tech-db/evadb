1b62815231733f01fd2fd098297de4018ca0b39e
Aubhro Sengupta <hello@aubhro.com>
2022-09-24 23:13:41 -0400

    Convert FrameInfo to dataclass

--- test/models/catalog/test_frame_info.py
+++ test/models/catalog/test_frame_info.py
@@ -20,6 +20,6 @@ from eva.models.catalog.properties import ColorSpace
 
 class FrameInfoTest(unittest.TestCase):
     def test_frame_info_equality(self):
-        info1 = FrameInfo(250, 250, 3, ColorSpace.GRAY)
-        info2 = FrameInfo(250, 250, color_space=ColorSpace.GRAY)
+        info1 = FrameInfo(height=250, width=250, channels=3, color_space=ColorSpace.GRAY)
+        info2 = FrameInfo(250, 250, 3, color_space=ColorSpace.GRAY)
         self.assertEqual(info1, info2)
