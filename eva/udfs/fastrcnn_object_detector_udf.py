16a81d7f333926fd8798094b3bb74a1cf7a6eb91
Adarsh Honawad <adarsh2397@gmail.com>
2022-09-27 00:56:24 -0400

    (udfs): cleanup code and merge into fastrcnn detector

--- eva/udfs/fastrcnn_object_detector_udf.py
+++ eva/udfs/fastrcnn_object_detector_udf.py
@@ -16,7 +16,6 @@ from abc import ABCMeta
 
 import pandas as pd
 import numpy as np
-from numpy.typing import ArrayLike
 
 from eva.udfs.udf import UDF
 from abc import abstractmethod
@@ -28,10 +27,12 @@ import torch
 from numpy.typing import ArrayLike
 from PIL import Image
 from torch import Tensor, nn
+from torchvision.transforms import Compose, transforms
 
 from eva.models.catalog.frame_info import FrameInfo
 from eva.models.catalog.properties import ColorSpace
-
+from eva.udfs.gpu_compatible import GPUCompatible
+from eva.configuration.configuration_manager import ConfigurationManager
 
 try:
     from torch import Tensor
@@ -52,12 +53,11 @@ except ImportError as e:
 """
     `setup` is the name of the decorator.
 """
-def setup(
-    name=None,
-    cache=True
-    ):
+
+def setup(name=None,cache=True):
     def Inner(func):
         def wrapper(self):
+            # self.name = name
             return func(self)
         return wrapper
     return Inner
@@ -76,15 +76,19 @@ def forward():
         return wrapper
     return Inner
 
-
-class FastRCNNObjectDetectorUDF(UDF):
+class FastRCNNObjectDetectorUDF(nn.Module, GPUCompatible):
     def __init__(self):
-        UDF.__init__(self)
+        nn.Module.__init__(self)
+        self.transforms = [transforms.ToTensor()]
         self.setup()
 
     @property
     def input_format(self) -> FrameInfo:
         return FrameInfo(-1, -1, 3, ColorSpace.RGB)
+    
+    @property
+    def name(self) -> str:
+        return str(self)
 
     @setup(
         name="FastRCNNObjectDetectorUDF",
@@ -107,6 +111,72 @@ class FastRCNNObjectDetectorUDF(UDF):
     def forward(self, frames: Tensor):
         tens_batch = torch.cat([self.transform(x) for x in frames]).to(self.get_device())
         return self.classify(tens_batch)
+    
+    def classify(self, frames: Tensor) -> pd.DataFrame:
+        gpu_batch_size = ConfigurationManager().get_value("executor", "gpu_batch_size")
+        if gpu_batch_size:
+            chunks = torch.split(frames, gpu_batch_size)
+            outcome = pd.DataFrame()
+            for tensor in chunks:
+                outcome = outcome.append(
+                    self._get_predictions(tensor), ignore_index=True
+                )
+            return outcome
+        else:
+            return self._get_predictions(frames)
+
+    def _get_predictions(self, frames: Tensor) -> pd.DataFrame:
+        predictions = self.model(frames)
+        outcome = pd.DataFrame()
+        for prediction in predictions:
+            pred_class = [
+                str(self.labels[i]) for i in list(self.as_numpy(prediction["labels"]))
+            ]
+            pred_boxes = [
+                [i[0], i[1], i[2], i[3]]
+                for i in list(self.as_numpy(prediction["boxes"]))
+            ]
+            pred_score = list(self.as_numpy(prediction["scores"]))
+            valid_pred = [pred_score.index(x) for x in pred_score if x > self.threshold]
+
+            if valid_pred:
+                pred_t = valid_pred[-1]
+            else:
+                pred_t = -1
+
+            pred_boxes = np.array(pred_boxes[: pred_t + 1])
+            pred_class = np.array(pred_class[: pred_t + 1])
+            pred_score = np.array(pred_score[: pred_t + 1])
+            outcome = outcome.append(
+                {"labels": pred_class, "scores": pred_score, "bboxes": pred_boxes},
+                ignore_index=True,
+            )
+        return outcome
+
+    def as_numpy(self, val: Tensor) -> np.ndarray:
+        """
+        Given a tensor in GPU, detach and get the numpy output
+        Arguments:
+             val (Tensor): tensor to be converted
+        Returns:
+            np.ndarray: numpy array representation
+        """
+        return val.detach().cpu().numpy()
+
+    def to_device(self, device: str) -> GPUCompatible:
+        """
+        Required to make class a member of GPUCompatible Protocol.
+        """
+        return self.to(torch.device("cuda:{}".format(device)))
+
+    
+    def get_device(self):
+        return next(self.parameters()).device
+    
+    def transform(self, images: np.ndarray):
+        composed = Compose(self.transforms)
+        # reverse the channels from opencv
+        return composed(Image.fromarray(images[:, :, ::-1])).unsqueeze(0)
 
     @property
     def labels(self) -> List[str]:
@@ -206,7 +276,6 @@ class FastRCNNObjectDetectorUDF(UDF):
 
 
     def __call__(self, *args, **kwargs):
-        print("Entered the call method")
         if len(args) == 0:
             return nn.Module.__call__(self, *args, **kwargs)
 
@@ -214,5 +283,4 @@ class FastRCNNObjectDetectorUDF(UDF):
         if isinstance(frames, pd.DataFrame):
             frames = frames.transpose().values.tolist()[0]
 
-        print("Abbout to enter the UDF Call Method")
         return nn.Module.__call__(self, frames, **kwargs)
--- eva/udfs/udf.py
+++ eva/udfs/udf.py
@@ -22,7 +22,6 @@ from numpy.typing import ArrayLike
 from eva.models.catalog.frame_info import FrameInfo
 from eva.models.catalog.properties import ColorSpace
 
-from abc import abstractmethod
 from typing import List
 
 import numpy as np
@@ -33,108 +32,26 @@ from PIL import Image
 from torch import Tensor, nn
 from torchvision.transforms import Compose, transforms
 
-from eva.configuration.configuration_manager import ConfigurationManager
-from eva.udfs.abstract_udf import AbstractClassifierUDF
-from eva.udfs.gpu_compatible import GPUCompatible
+class UDF:
+    def setup_decorator(name=None,cache=True):
+        def Inner(func):
+            def wrapper(self):
+                self.name = name
+                self.transform = [transforms.ToTensor()]
+                return func(self)
+            return wrapper
+        return Inner
 
+    def preprocess_decorator():
+        def Inner(func):
+            def wrapper(self):
+                return func(self)
+            return wrapper
+        return Inner
 
-class UDF(AbstractClassifierUDF, nn.Module, GPUCompatible):
-    """
-    Class for all UDFs.
-
-    TODO: We might need to store the labels somewhere
-    """
-
-    def __init__(self):
-        AbstractClassifierUDF.__init__(self)
-        nn.Module.__init__(self)
-        self.transforms = [transforms.ToTensor()]
-    
-    def get_device(self):
-        return next(self.parameters()).device
-    
-    def transform(self, images: np.ndarray):
-        composed = Compose(self.transforms)
-        # reverse the channels from opencv
-        return composed(Image.fromarray(images[:, :, ::-1])).unsqueeze(0)
-
-
-    @property
-    def name(self) -> str:
-        return str(self)
-    
-    def _get_predictions(self, frames: Tensor) -> pd.DataFrame:
-        print("Inside Get Predictions Method")
-        predictions = self.model(frames)
-        print(predictions)
-        outcome = pd.DataFrame()
-        for prediction in predictions:
-            pred_class = [
-                str(self.labels[i]) for i in list(self.as_numpy(prediction["labels"]))
-            ]
-            pred_boxes = [
-                [i[0], i[1], i[2], i[3]]
-                for i in list(self.as_numpy(prediction["boxes"]))
-            ]
-            pred_score = list(self.as_numpy(prediction["scores"]))
-            valid_pred = [pred_score.index(x) for x in pred_score if x > self.threshold]
-
-            if valid_pred:
-                pred_t = valid_pred[-1]
-            else:
-                pred_t = -1
-
-            pred_boxes = np.array(pred_boxes[: pred_t + 1])
-            pred_class = np.array(pred_class[: pred_t + 1])
-            pred_score = np.array(pred_score[: pred_t + 1])
-            outcome = outcome.append(
-                {"labels": pred_class, "scores": pred_score, "bboxes": pred_boxes},
-                ignore_index=True,
-            )
-        return outcome
-
-    def classify(self, frames: Tensor) -> pd.DataFrame:
-        print("Starting the classify method")
-        gpu_batch_size = ConfigurationManager().get_value("executor", "gpu_batch_size")
-        print(gpu_batch_size)
-        if gpu_batch_size:
-            chunks = torch.split(frames, gpu_batch_size)
-            outcome = pd.DataFrame()
-            for tensor in chunks:
-                outcome = outcome.append(
-                    self._get_predictions(tensor), ignore_index=True
-                )
-            return outcome
-        else:
-            return self._get_predictions(frames)
-    
-    
-    def as_numpy(self, val: Tensor) -> np.ndarray:
-        """
-        Given a tensor in GPU, detach and get the numpy output
-        Arguments:
-             val (Tensor): tensor to be converted
-        Returns:
-            np.ndarray: numpy array representation
-        """
-        return val.detach().cpu().numpy()
-
-    def to_device(self, device: str) -> GPUCompatible:
-        """
-        Required to make class a member of GPUCompatible Protocol.
-        """
-        return self.to(torch.device("cuda:{}".format(device)))
-
-    def __call__(self, *args, **kwargs):
-        pass
-        # print("Entered the UDF call method")
-        # if len(args) == 0:
-        #     return nn.Module.__call__(self, *args, **kwargs)
-        # frames = args[0]
-        # if isinstance(frames, pd.DataFrame):
-        #     frames = frames.transpose().values.tolist()[0]
-
-        # return nn.Module.__call__(self, frames, **kwargs)
-
-    def get_device(self):
-        return next(self.parameters()).device
+    def forward_decorator():
+        def Inner(func):
+            def wrapper(self, frames):
+                return func(self, frames)
+            return wrapper
+        return Inner
