61003db55b0ab15cf93226d08187da4759a8e696
Saket Singh <sakettsingh@outlook.com>
2022-10-21 14:41:47 -0400

    Modify dockerfile

--- dev/commands.sh
+++ dev/commands.sh
@@ -1,11 +1,8 @@
-# Build
-docker build -f .\gpu-enabled.Dockerfile -t eva:gpu .
+# Build from the parent directory
+docker build -f .\dev\gpu-enabled.Dockerfile -t eva:gpu .
 
 # Run with GPUs exposed and volume mount
-docker run -it --gpus=all --rm -v ..:/eva eva:gpu tail -f /dev/null
-
-# sh script/antlr4/generate_parser.sh
-# pip install -e ".[dev]"
+docker run -it --gpus=all --rm -v  D:\Projects\eva:/eva eva:gpu tail -f /dev/null
 
 # python3 eva/eva_server.py
 # python3 eva/eva_cmd_client.py
--- dev/gpu-enabled.Dockerfile
+++ dev/gpu-enabled.Dockerfile
@@ -9,4 +9,12 @@ RUN apt-get -qq update \
     && python3 -m pip install --upgrade pip
 
 RUN apt install -y openjdk-11-jdk openjdk-11-jre wget
-RUN mkdir /eva && chmod -R 777 /eva
+RUN mkdir /eva && chmod -R 777 /eva && mkdir /src
+COPY ./ /src
+
+WORKDIR /src
+RUN chmod -R 777 /src \
+    && sh script/antlr4/generate_parser.sh \
+    && pip install -e ".[dev]"
+
+WORKDIR /eva
