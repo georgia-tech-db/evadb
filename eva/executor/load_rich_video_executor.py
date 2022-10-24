07481adb0bfa04302d88cc4c6c0460871ae082c1
tushar <tushar.aggarwal@gatech.edu>
2022-10-24 16:50:33 -0400

    Minor refactoring

--- eva/executor/load_rich_video_executor.py
+++ eva/executor/load_rich_video_executor.py
@@ -19,54 +19,24 @@ import pandas as pd
 from eva.configuration.configuration_manager import ConfigurationManager
 from eva.executor.abstract_executor import AbstractExecutor
 from eva.models.storage.batch import Batch
+from eva.executor.load_video_executor import LoadVideoExecutor
 from eva.planner.load_data_plan import LoadDataPlan
 from eva.storage.storage_engine import VideoStorageEngine
 from eva.utils.logging_manager import logger
 
 
-class LoadRichVideoExecutor(AbstractExecutor):
+class LoadRichVideoExecutor(LoadVideoExecutor):
     def __init__(self, node: LoadDataPlan):
         super().__init__(node)
-        config = ConfigurationManager()
-        self.upload_dir = Path(config.get_value("storage", "upload_dir"))
-
-    def validate(self):
-        pass
 
     def exec(self):
-        """
-        Read the input video using opencv and persist data
-        using storage engine
-        """
+        if super().exec() is not None:
 
-        video_file_path = None
-        # Validate file_path
-        if Path(self.node.file_path).exists():
-            video_file_path = self.node.file_path
-        # check in the upload directory
-        else:
-            video_path = Path(Path(self.upload_dir) / self.node.file_path)
-            if video_path.exists():
-                video_file_path = video_path
+            # TODO: Add transcript metadata here
 
-        if video_file_path is None:
-            error = "Failed to find a video file at location: {}".format(
-                self.node.file_path
-            )
-            logger.error(error)
-            raise RuntimeError(error)
-
-        VideoStorageEngine.create(self.node.table_metainfo, if_not_exists=True)
-        success = VideoStorageEngine.write(
-            self.node.table_metainfo,
-            Batch(pd.DataFrame([{"video_file_path": str(video_file_path)}])),
-        )
-
-        # ToDo: Add logic for indexing the video file
-        # Create an index of I frames to speed up random video seek
-        if success:
             yield Batch(
                 pd.DataFrame(
-                    [f"Video successfully added at location: {video_file_path}"]
+                    [f"Rich video successfully added at location: {self.node.file_path}"]
                 )
             )
+
