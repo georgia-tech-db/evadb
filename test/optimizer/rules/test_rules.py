f83f79bc054bf13283cdcaa1d65cf532234d6ec9
Kaushik Ravichandran <kaushikravichandran@Kaushiks-MacBook-Air.local>
2022-10-29 21:27:58 -0400

    fix old imports

--- test/optimizer/rules/test_rules.py
+++ test/optimizer/rules/test_rules.py
@@ -42,7 +42,6 @@ from eva.optimizer.rules.rules import (
     LogicalDerivedGetToPhysical,
     LogicalDropToPhysical,
     LogicalDropUDFToPhysical,
-    LogicalExchangeToPhysical,
     LogicalFilterToPhysical,
     LogicalFunctionScanToPhysical,
     LogicalGetToSeqScan,
@@ -61,8 +60,9 @@ from eva.optimizer.rules.rules import (
     LogicalUploadToPhysical,
     Promise,
     PushDownFilterThroughJoin,
-    RulesManager,
 )
+from eva.experimental.ray.optimizer.rules.rules import LogicalExchangeToPhysical
+from eva.optimizer.rules.rules_manager import RulesManager
 from eva.server.command_handler import execute_query_fetch_all
 
 
