9ef79dccbe1bf6513bc71c40ba5fddc5be8f4540
jarulraj <arulraj@gatech.edu>
2022-11-10 18:58:09 -0500

    checkpoint

--- eva/optimizer/group.py
+++ eva/optimizer/group.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from __future__ import annotations #noqa: F407
+from __future__ import annotations  # noqa: F407
 
 from typing import Dict, List
 
