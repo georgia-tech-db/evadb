4e312632ba082434dfb3cc8a197c10682cac50b9
jarulraj <arulraj@gatech.edu>
2022-11-10 18:55:01 -0500

    checkpoint

--- eva/optimizer/rules/rules.py
+++ eva/optimizer/rules/rules.py
@@ -12,6 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from __future__ import annotations  # noqa: F407
+
 from typing import TYPE_CHECKING
 
 from eva.expression.expression_utils import conjuction_list_to_expression_tree
