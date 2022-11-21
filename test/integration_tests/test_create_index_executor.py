ec3746dddffbd85a371dd60a3d50347279f18406
Jiashen Cao <caojiashen24@gmail.com>
2022-11-20 23:28:33 -0500

    add end-to-end index creation test

---
+++ test/integration_tests/test_create_index_executor.py
@@ -0,0 +1,112 @@
+# coding=utf-8
+# Copyright 2018-2022 EVA
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import faiss
+import unittest
+import numpy as np
+import pandas as pd
+
+from pathlib import Path
+
+from eva.catalog.catalog_manager import CatalogManager
+from eva.models.storage.batch import Batch
+from eva.catalog.column_type import ColumnType, NdArrayType
+from eva.server.command_handler import execute_query_fetch_all
+from eva.parser.create_statement import ColConstraintInfo, ColumnDefinition
+from eva.utils.generic_utils import generate_file_path
+from eva.storage.storage_engine import StorageEngine
+from eva.catalog.index_type import IndexType
+from eva.configuration.constants import EVA_DEFAULT_DIR, INDEX_DIR
+
+
+class CreateIndexTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # Reset catalog.
+        CatalogManager().reset()
+
+        # Create feature vector.
+        feat1 = np.array([[0, 0, 0]]).astype(np.float32)
+        feat2 = np.array([[100, 100, 100]]).astype(np.float32)
+        feat3 = np.array([[200, 200, 200]]).astype(np.float32)
+
+        # Create table.
+        col_list = [
+            ColumnDefinition("feat_id", ColumnType.INTEGER, None, [], ColConstraintInfo(unique=True)),
+            ColumnDefinition("feat", ColumnType.NDARRAY, NdArrayType.FLOAT32, [1, 3]) ,
+        ]
+        col_metadata = [
+            CatalogManager().create_column_metadata(
+                col.name,
+                col.type,
+                col.array_type,
+                col.dimension,
+                col.cci
+            ) for col in col_list
+        ]
+        tb_metadata = CatalogManager().create_metadata(
+            "testCreateIndexFeatTable",
+            str(generate_file_path("testCreateIndexFeatTable")),
+            col_metadata,
+            identifier_column="feat_id",
+            is_video=False,
+        )
+        StorageEngine.create(tb_metadata)
+
+        # Create pandas dataframe.
+        batch_data = Batch(
+            pd.DataFrame(data={
+                "feat_id": [0, 1, 2],
+                "feat": [feat1, feat2, feat3],
+            })
+        )
+        StorageEngine.write(tb_metadata, batch_data)
+
+    @classmethod
+    def tearDownClass(cls):
+        # TODO: Drop the created index after support drop command on index.
+        query = "DROP TABLE testCreateIndexFeatTable;"
+        execute_query_fetch_all(query)
+
+    def test_should_create_index(self):
+        query = (
+            "CREATE INDEX testCreateIndexName USING HNSW ON testCreateIndexFeatTable (feat);"
+        )
+        execute_query_fetch_all(query)
+
+        # Test index metadata.
+        index_metadata = CatalogManager().get_index_by_name("testCreateIndexName")
+        self.assertEqual(index_metadata.type, IndexType.HNSW)
+        self.assertEqual(index_metadata.save_file_path, str(EVA_DEFAULT_DIR / INDEX_DIR / Path("{}_{}.index".format(
+            index_metadata.type, index_metadata.name)))
+        )
+
+        # Test on disk index.
+        index = faiss.read_index(index_metadata.save_file_path)
+        distance, logical_id = index.search(np.array([[0, 0, 0]]).astype(np.float32), 1)
+        self.assertEqual(distance[0][0], 0)
+        self.assertEqual(logical_id[0][0], 0)
+
+        # Test secondary index.
+        secondary_index_metadata = CatalogManager().get_dataset_metadata(
+            None,
+            "secondary_index_{}_{}".format(index_metadata.type, index_metadata.name),
+        )
+        size = 0
+        for i, batch in enumerate(StorageEngine.read(secondary_index_metadata, 1)):
+            df_data = batch.frames
+            self.assertEqual(df_data["logical_id"][0], i)
+            self.assertEqual(df_data["row_id"][0], i)
+            size += 1
+        self.assertEqual(size, 3)
