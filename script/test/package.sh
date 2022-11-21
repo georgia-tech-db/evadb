9669220ce38f07ae1cf8ad8a54ed4bc606c77f8d
jarulraj <arulraj@gatech.edu>
2022-11-20 23:31:43 -0500

    checkpoint

--- script/test/package.sh
+++ script/test/package.sh
@@ -3,6 +3,20 @@
 eva_server &> eva.txt &
 sleep 10
 head -n20 eva.txt
-grep "serving" eva.txt || exit 255
+
+# check if server started
+grep "serving" eva.txt
+test_code=$?
+if [ $test_code -ne 0 ];
+then
+    exit $test_code
+fi
+
 eva_client &> client.txt &
+if [ $test_code -ne 0 ];
+then
+    exit $test_code
+fi
+
 head -n20 client.txt
+exit 0
