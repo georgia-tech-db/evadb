f9f3c8ac1836aa3344180eee3829cf52cb514d87
Aryan-Rajoria <57455619+Aryan-Rajoria@users.noreply.github.com>
2022-11-22 20:22:14 +0530

    adding license

--- test/test_eva_imports.py
+++ test/test_eva_imports.py
@@ -1,5 +1,19 @@
-import os
+# coding=utf-8
+# Copyright 2018-2022 EVA
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import importlib
+import os
 import shutil
 import unittest
 
@@ -14,7 +28,7 @@ class EVAImportTest(unittest.TestCase):
         when current working directory is changed.
         """
         cur_dir = os.getcwd()
-        new_dir = os.path.join('test_eva', 'test')
+        new_dir = os.path.join("test_eva", "test")
         if not os.path.exists(new_dir):
             os.makedirs(new_dir)
         os.chdir(new_dir)
