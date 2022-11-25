45f8d6495016b47926f147997ad9a64161ff3bab
Aryan-Rajoria <57455619+Aryan-Rajoria@users.noreply.github.com>
2022-11-25 23:43:55 +0530

    removing unnecessary imports

--- setup.py
+++ setup.py
@@ -98,10 +98,7 @@ udf_libs = [
     "facenet-pytorch>=2.5.2",
     "easyocr>=1.5.0",
     "ipython",
-    # "psutil",
-    # "thop>=0.1.1",
     "tqdm>=4.64.0",
-    # "tensorboard>=2.4.1",
     "seaborn>=0.11.0"
 ]
 
