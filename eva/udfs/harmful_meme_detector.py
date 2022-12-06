9d19ba69c455c446c6ee2db6693c1fea72e06a89
leungyukshing <jacky14.liang@gmail.com>
2022-12-05 23:41:25 -0500

    chore: format code

--- eva/udfs/harmful_meme_detector.py
+++ eva/udfs/harmful_meme_detector.py
@@ -1,13 +1,30 @@
+# coding=utf-8
+# Copyright 2018-2022 EVA
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 from typing import List
-import pytesseract
-import PIL
-from detoxify import Detoxify
-import pandas as pd
-from eva.udfs.abstract.abstract_udf import AbstractClassifierUDF
+
 import numpy as np
-from eva.utils.logging_manager import logger
+import pandas as pd
+import PIL
+import pytesseract
+from detoxify import Detoxify
+
 from eva.models.catalog.frame_info import FrameInfo
 from eva.models.catalog.properties import ColorSpace
+from eva.udfs.abstract.abstract_udf import AbstractClassifierUDF
+from eva.utils.logging_manager import logger
+
 
 class HarmfulMemeDetector(AbstractClassifierUDF):
     """
@@ -19,27 +36,32 @@ class HarmfulMemeDetector(AbstractClassifierUDF):
     @property
     def name(self) -> str:
         return "HarmfulMemeDetector"
-    
+
     def setup(self, threshold=0.2):
         logger.warn("setup start")
         self.threshold = threshold
-        self.model = Detoxify('original')
+        self.model = Detoxify("original")
         logger.warn("setup finish")
-    
+
     @property
     def input_format(self) -> FrameInfo:
         logger.warn("input_format start")
         return FrameInfo(-1, -1, 3, ColorSpace.RGB)
-    
+
     @property
     def labels(self) -> List[str]:
         logger.warn("labels start")
-        return [
-            "toxic",
-            "not toxic"
-        ]
+        return ["toxic", "not toxic"]
 
     def forward(self, frames: np.ndarray) -> pd.DataFrame:
+        """
+        Performs predictions on input frames
+        Arguments:
+            frames (np.ndarray): Frames on which predictions need
+            to be performed
+        Returns:
+            outcome (List[Str])
+        """
         # reconstruct dimension of the input
         frames_list = frames.values.tolist()
         frames = np.array(frames_list)
@@ -52,7 +74,7 @@ class HarmfulMemeDetector(AbstractClassifierUDF):
         for i in range(0, frames.shape[0]):
             frame = frames[i]
 
-            image = PIL.Image.fromarray(frame.astype('uint8'), 'RGB')
+            image = PIL.Image.fromarray(frame.astype("uint8"), "RGB")
             text = pytesseract.image_to_string(image)
             logger.warn(text)
             prediction_result = self.model.predict(text)
