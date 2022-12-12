f0317c8d02fa67267ee4be5dac00c9f0e45b9722
Jiashen Cao <caojiashen24@gmail.com>
2022-12-12 17:48:08 -0500

    reformat

--- test/udfs/ndarray/test_open.py
+++ test/udfs/ndarray/test_open.py
@@ -14,11 +14,12 @@
 # limitations under the License.
 import os
 import unittest
+from test.util import create_sample_image
+
 import numpy as np
 import pandas as pd
-
 from mock import patch
-from test.util import create_sample_image
+
 from eva.configuration.configuration_manager import ConfigurationManager
 from eva.executor.executor_utils import ExecutorError
 from eva.udfs.ndarray.open import Open
