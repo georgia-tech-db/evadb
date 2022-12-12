e9c8c6570e8486e12674256d7b263fb3b4655553
Jiashen Cao <caojiashen24@gmail.com>
2022-12-12 13:40:32 -0500

    fix lint error

--- test/integration_tests/test_open.py
+++ test/integration_tests/test_open.py
@@ -28,7 +28,7 @@ class OpenTests(unittest.TestCase):
     def setUp(self):
         CatalogManager().reset()
         ConfigurationManager()
-        
+
         # Load built-in UDFs.
         load_inbuilt_udfs()
 
