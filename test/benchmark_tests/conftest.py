f80c3d5817ef20388ffbcc5e177e323343902fe1
Gaurav Tarlok Kakkar <gaurav21776@gmail.com>
2022-12-14 17:15:56 -0500

    test: fix testcases

--- test/benchmark_tests/conftest.py
+++ test/benchmark_tests/conftest.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from test.util import copy_sample_videos_to_upload_dir, file_remove, load_inbuilt_udfs
+from test.util import load_inbuilt_udfs
 
 import pytest
 
@@ -22,15 +22,8 @@ from eva.server.command_handler import execute_query_fetch_all
 
 @pytest.fixture(autouse=False)
 def setup_pytorch_tests():
-    file_remove("ua_detrac.mp4")
     CatalogManager().reset()
-    copy_sample_videos_to_upload_dir()
-    query = """LOAD VIDEO 'ua_detrac.mp4'
-                INTO MyVideo;"""
-    execute_query_fetch_all(query)
-    query = """LOAD VIDEO 'mnist.mp4'
-                INTO MNIST;"""
-    execute_query_fetch_all(query)
+    execute_query_fetch_all("LOAD VIDEO 'data/ua_detrac/ua_detrac.mp4' INTO MyVideo;")
+    execute_query_fetch_all("LOAD VIDEO 'data/mnist/mnist.mp4' INTO MNIST;")
     load_inbuilt_udfs()
     yield None
-    file_remove("ua_detrac.mp4")
