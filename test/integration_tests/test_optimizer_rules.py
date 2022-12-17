327b3e197e442a8b12c1e2fe567e3a7b6d04f384
Gaurav Tarlok Kakkar <gaurav21776@gmail.com>
2022-12-16 19:31:35 -0500

    test: add new testcase

--- test/integration_tests/test_optimizer_rules.py
+++ test/integration_tests/test_optimizer_rules.py
@@ -1,16 +1,30 @@
+# coding=utf-8
+# Copyright 2018-2022 EVA
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import unittest
+from test.util import load_inbuilt_udfs
+
+from eva.catalog.catalog_manager import CatalogManager
+from eva.configuration.constants import EVA_ROOT_DIR
 from eva.optimizer.rules.rules import (
     PushDownFilterThroughApplyAndMerge,
     PushDownFilterThroughJoin,
     XformLateralJoinToLinearFlow,
 )
 from eva.optimizer.rules.rules_manager import disable_rules
-from eva.utils.timer import Timer
-from test.util import load_inbuilt_udfs
-
-from eva.catalog.catalog_manager import CatalogManager
-from eva.configuration.constants import EVA_ROOT_DIR
 from eva.server.command_handler import execute_query_fetch_all
+from eva.utils.timer import Timer
 
 
 class OptimizerRulesTest(unittest.TestCase):
