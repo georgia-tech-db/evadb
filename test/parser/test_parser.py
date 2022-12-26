65d00ff73cfbbe544762ebf1dddc8e56537e81f1
Joy Arulraj <arulraj@gatech.edu>
2022-12-26 02:12:30 -0500

    checkpoint

--- test/parser/test_parser.py
+++ test/parser/test_parser.py
@@ -844,12 +844,6 @@ class ParserTests(unittest.TestCase):
         self.assertEqual(select_stmt, expected_stmt)
 
     def test_lark(self):
-        #query = """LOAD VIDEO 'data/video.mp4' INTO MyVideo;"""
-        query = """CREATE TABLE IF NOT EXISTS Persons (
-                  Frame_ID INTEGER UNIQUE,
-                  Frame_Data TEXT(10),
-                  Frame_Value FLOAT(1000, 201),
-                  Frame_Array NDARRAY UINT8(5, 100, 2432, 4324, 100)
-            );"""
+        query = """CREATE INDEX testindex ON MyVideo (featCol) USING HNSW;"""
         parser = Parser()
         parser.parse(query)
