62cad7a5031625cd01d637ed04aac6225f442802
Jiashen Cao <caojiashen24@gmail.com>
2023-01-04 12:10:26 -0500

    remove unnecessary secondary index

--- eva/executor/create_index_executor.py
+++ eva/executor/create_index_executor.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import os
+import numpy as np
 from pathlib import Path
 
 import faiss
--- test/integration_tests/test_create_index_executor.py
+++ test/integration_tests/test_create_index_executor.py
@@ -41,7 +41,11 @@ class CreateIndexTest(unittest.TestCase):
         return str(
             EVA_DEFAULT_DIR
             / INDEX_DIR
-            / Path("{}_{}.index".format("HNSW", "testCreateIndexName"))
+            / Path(
+                "{}_{}.index".format(
+                    "HNSW", "testCreateIndexName"
+                )
+            )
         )
 
     @classmethod
