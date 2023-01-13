2ef6fce0c72d8f70742ffe0ff68eb7a55ac43ddb
jarulraj <arulraj@gatech.edu>
2023-01-12 22:41:55 -0500

    checkpoint

--- test/benchmark_tests/test_benchmark_pytorch.py
+++ test/benchmark_tests/test_benchmark_pytorch.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from os import sleep
+from time import sleep
 
 import pytest
 
