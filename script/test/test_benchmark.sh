de6183da1bb31868a988f4150bd9a5a9479b5462
jarulraj <arulraj@gatech.edu>
2023-01-12 21:50:09 -0500

    checkpoint

--- script/test/test_benchmark.sh
+++ script/test/test_benchmark.sh
@@ -16,7 +16,7 @@ then
 fi
 
 # Run only benchmark tests
-PYTHONPATH=./ pytest test/  --benchmark-autosave --benchmark-compare --benchmark-compare-fail=min:5%  -s -v --log-level=WARNING ${1:-} -m "benchmark"
+PYTHONPATH=./ pytest test/  --benchmark-autosave --benchmark-compare  -s -v --log-level=WARNING ${1:-} -m "benchmark"
 test_code=$?
 if [ $test_code -ne 0 ];
 then
