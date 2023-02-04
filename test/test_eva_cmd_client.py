f172c064d39c3b201dd068b92651a1013557330a
jarulraj <arulraj@gatech.edu>
2023-02-03 21:49:44 -0500

    checkpoint

--- test/test_eva_cmd_client.py
+++ test/test_eva_cmd_client.py
@@ -36,3 +36,10 @@ class CMDClientTest(unittest.TestCase):
         args = parse_args(["-P", "2345", "-H", "test"])
         self.assertEqual(args.host, "test")
         self.assertEqual(args.port, 2345)
+
+    @mock.patch("eva.server.interpreter.start_cmd_client")
+    def test_eva_client(self, mock_client):
+        from eva.eva_cmd_client import eva_client
+
+        eva_client()
+        mock_client.assert_called_once()
