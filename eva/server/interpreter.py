1ea91d57d1d929c150dd695a6ca73745f120c1bc
jarulraj <arulraj@gatech.edu>
2023-02-04 17:34:28 -0500

    major refactoring

--- eva/server/interpreter.py
+++ eva/server/interpreter.py
@@ -14,85 +14,12 @@
 # limitations under the License.
 import os
 import sys
-from cmd import Cmd
-from contextlib import ExitStack
 from typing import Dict
 import asyncio
 from asyncio import StreamReader, StreamWriter
 from eva.utils.logging_manager import logger
 from collections import deque
-from typing import Callable, Deque, Awaitable
 from eva.models.server.response import Response
-from pprint import pprint
-
-# Skip readline on Windows
-if os.name != "nt":
-    from readline import read_history_file, set_history_length, write_history_file
-
-from eva.server.db_api import EVAConnection
-
-# History file to persist EVA  command history across multiple client sessions
-histfile = "eva.history"
-histfile_size = 1000
-
-
-class EvaCommandInterpreter(Cmd):
-    def __init__(self):
-        super().__init__()
-
-    def cmdloop_with_keyboard_interrupt(self, intro=None):
-        quit_loop = False
-        while quit_loop is not True:
-            try:
-                self.cmdloop(intro)
-                quit_loop = True
-            except KeyboardInterrupt:
-                self.cursor.stop_query()
-                print("Interrupting query")
-
-    def preloop(self):
-        # To retain command history across multiple client sessions
-        if sys.platform != "msys":
-            if os.path.exists(histfile):
-                read_history_file(histfile)
-            else:
-                set_history_length(histfile_size)
-                write_history_file(histfile)
-
-    def postloop(self):
-        if sys.platform != "msys":
-            set_history_length(histfile_size)
-            write_history_file(histfile)
-
-    def set_connection(self, reader, writer):
-        self.connection = EVAConnection(reader, writer)
-        self.cursor = self.connection.cursor()
-
-    def emptyline(self):
-        print("Enter a valid query.")
-        return False
-
-    def do_quit(self, args):
-        """Quits the program."""
-        return SystemExit
-
-    def do_exit(self, args):
-        """Quits the program."""
-        return SystemExit
-
-    def default(self, line):
-        """Considers the input as a query"""
-        logger.info("default")
-        self.do_query(line)
-
-    def do_query(self, query):
-        """Takes in SQL query and generates the output"""
-        logger.info("do query")
-        self.cursor.execute(query)
-        message =  self.cursor.fetch_all()
-        print(message)
-        return False
-
 
 # version.py defines the VERSION and VERSION_SHORT variables
 VERSION_DICT: Dict[str, str] = {}
@@ -103,27 +30,6 @@ version_file_path = os.path.join(current_file_parent_dir, "version.py")
 
 with open(version_file_path, "r") as version_file:
     exec(version_file.read(), VERSION_DICT)
-
-
-def handle_user_input(reader, writer):
-    """
-    Reads from stdin in separate thread
-
-    If user inputs 'quit' stops the event loop
-    otherwise just echoes user input
-    """
-
-    # Start command interpreter
-    prompt = EvaCommandInterpreter()
-    prompt.prompt = "eva=#"
-
-    prompt.set_connection(reader, writer)
-
-    VERSION = VERSION_DICT["VERSION"]
-
-    prompt.cmdloop_with_keyboard_interrupt(
-        intro="eva (v " + VERSION + ')\nType "help" for help'
-    )
 
 async def read_line(stdin_reader: StreamReader) -> str:
     delete_char = b'\x7f'
@@ -185,9 +91,8 @@ async def start_cmd_client(host: str, port: int):
     """
 
     reader, writer = await asyncio.open_connection(host, port)
-
-    # read input from the client and send it to the server.
     stdin_reader = await create_stdin_reader()
+
     input_listener = asyncio.create_task(read_from_client_and_send_to_server(stdin_reader, writer, reader))
 
     try:
