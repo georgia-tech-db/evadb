8c75a5e41c93196de0d175f3f1f91e7f89a96e0e
Aryan-Rajoria <aryanrajoria1003@gmail.com>
2023-02-09 19:50:43 -0500

    remove order_by and limit

--- eva/parser/lark_visitor/_delete_statement.py
+++ eva/parser/lark_visitor/_delete_statement.py
@@ -34,7 +34,7 @@ class Delete:
                 elif child.data == "where_expr":
                     where_clause = self.visit(child)
                     
-        delete_stmt = DeleteTableStatement(table_ref, where_clause, order_clause, limit_count)
+        delete_stmt = DeleteTableStatement(table_ref, where_clause)
         return delete_stmt
 
     
