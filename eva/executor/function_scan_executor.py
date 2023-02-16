9138491b5560d18da14340d9244abf3acd4b0d98
Kaushik Ravichandran <kravicha3@ada-01.cc.gatech.edu>
2023-02-16 00:43:57 -0500

    linter changes

--- eva/executor/function_scan_executor.py
+++ eva/executor/function_scan_executor.py
@@ -12,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from time import time
 from typing import Iterator
 
 from eva.catalog.catalog_manager import CatalogManager
@@ -20,7 +21,7 @@ from eva.executor.executor_utils import ExecutorError
 from eva.models.storage.batch import Batch
 from eva.plan_nodes.function_scan_plan import FunctionScanPlan
 from eva.utils.logging_manager import logger
-from time import time
+
 
 class FunctionScanExecutor(AbstractExecutor):
     """
@@ -42,7 +43,9 @@ class FunctionScanExecutor(AbstractExecutor):
         # TODO: compute cost using time
         cost = time
         catalog_manager = CatalogManager()
-        catalog_manager.insert_udf_cost_catalog_entry(name, type, cost, frame_count, resolution)
+        catalog_manager.insert_udf_cost_catalog_entry(
+            name, type, cost, frame_count, resolution
+        )
 
     def exec(self, *args, **kwargs) -> Iterator[Batch]:
         assert (
@@ -55,16 +58,22 @@ class FunctionScanExecutor(AbstractExecutor):
                 t_start = time()
                 res = self.func_expr.evaluate(lateral_input)
                 t_end = time()
-                time_taken =  t_end - t_start
+                time_taken = t_end - t_start
                 resolution = None
                 udf_obj = catalog_manager.get_udf_catalog_entry_by_name(
                     self.func_expr.name
                 )
-                if(udf_obj):
+                if udf_obj:
                     type = udf_obj.type
                 else:
                     type = None
-                self.persistCost(self.func_expr.name, type, lateral_input.frames.shape[0], resolution, time_taken)
+                self.persistCost(
+                    self.func_expr.name,
+                    type,
+                    lateral_input.frames.shape[0],
+                    resolution,
+                    time_taken,
+                )
                 if not res.empty():
                     if self.do_unnest:
                         res.unnest()
