e3ee87329fb3d1c5851ff3386d1dcfa8453e47ab
Ishwarya Sivakumar <ish23.siva@gmail.com>
2023-02-21 12:18:48 -0500

    new exception type for input mismatches.

---
+++ eva/udfs/contrib/decorators/ish/io_descriptors/type_exception.py
@@ -0,0 +1,8 @@
+
+
+class TypeException(Exception):
+    def __init__(self, message):            
+        # Call the base class constructor with the parameters it needs
+        super().__init__(message)
+        
+            
