b144784ac108d5671a1fa44d635a29ba622ae2dd
Suryatej Reddy Vyalla <suryatej16102@iiitd.ac.in>
2023-02-20 19:49:05 -0500

    bug fix

--- test/integration_tests/test_s3_load_executor.py
+++ test/integration_tests/test_s3_load_executor.py
@@ -115,13 +115,15 @@ class S3LoadExecutorTest(unittest.TestCase):
 
         select_query = """SELECT * FROM MyVideos;"""
         result = execute_query_fetch_all(select_query)
-        result_videos = [Path(video).as_posix() for video in result.frames["myvideos.name"].unique()]
-        
+        result_videos = [
+            Path(video).as_posix() for video in result.frames["myvideos.name"].unique()
+        ]
+
         s3_dir_path = Path(self.s3_download_dir)
         expected_videos = [
             (s3_dir_path / "MyVideos/1.mp4").as_posix(),
             (s3_dir_path / "MyVideos/2.mp4").as_posix(),
-            self.video_file_path,
+            Path(self.video_file_path).as_posix(),
         ]
 
         self.assertEqual(result_videos, expected_videos)
