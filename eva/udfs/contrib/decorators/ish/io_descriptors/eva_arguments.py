67409fbca77137a09589e10291325ee8b9dc44a8
Ishwarya Sivakumar <ish23.siva@gmail.com>
2023-02-21 12:18:27 -0500

    new Eva Argumet type files.

---
+++ eva/udfs/contrib/decorators/ish/io_descriptors/eva_arguments.py
@@ -0,0 +1,19 @@
+
+class EvaArgument(object):
+    def __init__(self) -> None:
+        pass
+        
+    def check_type(self, input_object) -> bool:
+        pass
+    
+    def check_shape(self, input_object, required_shape) -> bool:
+        pass
+            
+    def name(self):
+        pass
+            
+        
+        
+    
+    
+    
