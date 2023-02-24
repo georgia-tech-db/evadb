fc7be06f71bf169f9d044f11e9d3bc46cf764876
Kaushik Ravichandran <kravicha3@ada-01.cc.gatech.edu>
2023-02-24 00:08:52 -0500

    changes

--- eva/catalog/models/udf_cost_catalog.py
+++ eva/catalog/models/udf_cost_catalog.py
@@ -38,7 +38,7 @@ class UdfCostCatalog(BaseModel):
     _resolution = Column("resolution", Integer())
     _udf_id = Column("udf_id", Integer(), ForeignKey("udf_catalog._row_id"))
 
-    # _name = Column("name", String(100), unique=True)
+    _udf_name = Column("name", String(100), unique=True)
     _udf_name = relationship("UdfCatalog", back_populates="udf_catalog._name")
 
     def __init__(
