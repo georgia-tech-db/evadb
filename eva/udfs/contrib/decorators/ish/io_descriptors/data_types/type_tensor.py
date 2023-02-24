317cf75ef611cd9bb89945b2a8d284f3f701174d
Ishwarya Sivakumar <ish23.siva@gmail.com>
2023-02-24 10:43:12 -0500

    fixed linting errors.

--- eva/udfs/contrib/decorators/ish/io_descriptors/data_types/type_tensor.py
+++ eva/udfs/contrib/decorators/ish/io_descriptors/data_types/type_tensor.py
@@ -1,38 +1,52 @@
-from eva.udfs.contrib.decorators.ish.io_descriptors.eva_arguments import EvaArgument
-import numpy as np
+# coding=utf-8
+# Copyright 2018-2022 EVA
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import torch
 
+from eva.udfs.contrib.decorators.ish.io_descriptors.eva_arguments import EvaArgument
+
+
 class PyTorchTensor(EvaArgument):
-    
-    def __init__(self, shape = None, dtype = None) -> None:
+    def __init__(self, shape=None, dtype=None) -> None:
         super().__init__()
         self.shape = shape
         self.dtype = dtype
-        
+
     def check_type(self, input_object) -> bool:
         if self.dtype:
             if self.dtype == "int32":
-                return (isinstance(input_object, torch.Tensor) and (input_object.dtype == torch.int32))
+                return isinstance(input_object, torch.Tensor) and (
+                    input_object.dtype == torch.int32
+                )
             elif self.dtype == "float16":
-                return (isinstance(input_object, torch.Tensor) and (input_object.dtype == torch.float16))
+                return isinstance(input_object, torch.Tensor) and (
+                    input_object.dtype == torch.float16
+                )
             elif self.dtype == "float32":
-                return (isinstance(input_object, torch.Tensor) and (input_object.dtype == torch.float32))
-            
+                return isinstance(input_object, torch.Tensor) and (
+                    input_object.dtype == torch.float32
+                )
+
         else:
             return isinstance(input_object, torch.Tensor)
 
-    
     def check_shape(self, input_object) -> bool:
         if self.shape:
             if input_object.shape != self.shape:
                 return False
-            
+
         return True
-            
+
     def name(self):
         return "PyTorch Tensor"
-        
-        
-
-
-    
