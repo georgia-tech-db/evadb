588003881b58a338d57310b3851d879d526a8480
Kaushik Ravichandran <kravicha3@ada-01.cc.gatech.edu>
2023-02-27 21:13:23 -0500

    formatter

--- eva/catalog/models/udf_cost_catalog.py
+++ eva/catalog/models/udf_cost_catalog.py
@@ -15,9 +15,10 @@
 from dataclasses import dataclass
 
 from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship
 
 from eva.catalog.models.base_model import BaseModel
+
+# from sqlalchemy.orm import relationship
 
 
 class UdfCostCatalog(BaseModel):
