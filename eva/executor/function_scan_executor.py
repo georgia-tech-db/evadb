8cd332fa283386516dc7ab9be8b3fd8105050bd7
Kaushik Ravichandran <kravicha3@ada-01.cc.gatech.edu>
2023-03-09 21:58:47 -0500

    lint fix

--- eva/executor/function_scan_executor.py
+++ eva/executor/function_scan_executor.py
@@ -12,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from time import time
 from typing import Iterator
 
 from eva.catalog.catalog_manager import CatalogManager
