55bfe38011a0c9c65dda3e64ed5df38fe8baa28e
Ishwarya Sivakumar <ish23.siva@gmail.com>
2023-03-09 21:36:02 -0500

    updated tests and data types classes.

--- eva/udfs/decorators/udf_decorators.py
+++ eva/udfs/decorators/udf_decorators.py
@@ -59,24 +59,16 @@ def forward(input_signature: EvaArgument, output_signature: EvaArgument):
             # check shape of input
             if input_signature:
                 if (input_signature.is_shape_defined) and (
-                    not (input_signature.check_shape(frames))
-                ):
-                    try:
-                        frames = input_signature.reshape(frames)
-                    except TypeException as e:
-                        msg = "Shape mismatch of Input parameter. " + str(e)
-                        raise TypeException(msg)
+                    not (input_signature.check_shape(frames))):
+                    msg = "Shape mismatch of input parameter. "
+                    raise TypeException(msg)
 
                 # check type of input
                 if (input_signature.is_dtype_defined) and (
                     not (input_signature.check_type(frames))
                 ):
-                    try:
-                        frames = input_signature.convert_data_type(frames)
-
-                    except TypeException as e:
-                        msg = "Datatype mismatch of Input parameter. " + str(e)
-                        raise TypeException(msg)
+                    msg = "Datatype mismatch of Input parameter. "
+                    raise TypeException(msg)
 
             # first argument is self and second is frames.
             output = arg_fn(args[0], frames)
@@ -87,22 +79,15 @@ def forward(input_signature: EvaArgument, output_signature: EvaArgument):
                 if (output_signature.is_shape_defined) and (
                     not (output_signature.check_shape(output))
                 ):
-                    try:
-                        frames = output_signature.reshape(output)
-                    except TypeException as e:
-                        msg = "Shape mismatch of Output parameter. " + str(e)
-                        raise TypeException(msg)
+                    msg = "Shape mismatch of Output parameter. "
+                    raise TypeException(msg)
 
                 # check type of output
                 if (output_signature.is_dtype_defined) and (
                     not (output_signature.check_type(output))
                 ):
-                    try:
-                        output = output_signature.convert_data_type(output)
-
-                    except TypeException as e:
-                        msg = "Datatype mismatch of Output parameter. " + str(e)
-                        raise TypeException(msg)
+                    msg = "Datatype mismatch of Output parameter. "
+                    raise TypeException(msg)
 
                 # check if the column headers are matching
                 if output_signature.is_output_columns_set():
