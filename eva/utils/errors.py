0b818db7728e5c8bab06597d0671eca0546adf00
Suryatej Reddy Vyalla <suryatej16102@iiitd.ac.in>
2023-03-13 01:09:31 -0400

    remove error

--- eva/utils/errors.py
+++ eva/utils/errors.py
@@ -16,12 +16,6 @@ class CatalogError(Exception):
     pass
 
 
-class UDFInputOutputTypeException(Exception):
-    def __init__(self, message):
-        # Call the base class constructor with the parameters it needs
-        super().__init__(message)
-
-
 class DatasetFileNotFoundError(Exception):
     def __init__(
         self,
