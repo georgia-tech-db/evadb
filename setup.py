f9952374b1d536fdb96b839e286668e852bec617
Suryatej Reddy Vyalla <suryatej16102@iiitd.ac.in>
2023-04-03 18:18:03 -0400

    install decord from fork

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ minimal_requirement = [
     "ray>=1.13.0",
     "aenum>=2.2.0",
     "diskcache>=5.4.0",
-    "decord>=0.6.0"
+    "eva-decord@git+https://github.com/georgia-tech-db/eva-decord.git@main#subdirectory=python"
 ]
 
 formatter_libs = [
