e1389dceca21d937e86f421c76644ad40af4a199
Ishwarya Sivakumar <ish23.siva@gmail.com>
2023-04-07 00:23:53 -0400

    added the check inputs flag

--- eva/udfs/decorators/decorators.py
+++ eva/udfs/decorators/decorators.py
@@ -45,7 +45,7 @@ def setup(cachable: bool = False, udf_type: str = "Abstract", batchable: bool = 
     return inner_fn
 
 
-def forward(input_signatures: List[IOArgument], output_signatures: List[IOArgument]):
+def forward(input_signatures: List[IOArgument], output_signatures: List[IOArgument], check_input: bool = True, check_output: bool = True):
     """decorator for the forward function. It will be used to set the input and output.
 
     Args:
@@ -57,43 +57,45 @@ def forward(input_signatures: List[IOArgument], output_signatures: List[IOArgume
         def wrapper(*args):
 
             # checking the user constraints
-            if len(input_signatures) > 0:
-                args_lst = []
-                if len(args) > 0:
-                    args_lst.append(args[0])
+            if check_input:
+                if len(input_signatures) > 0:
+                    args_lst = []
+                    if len(args) > 0:
+                        args_lst.append(args[0])
 
-                for i, input_signature in enumerate(input_signatures):
-                    try:
-                        args_lst.append(
-                            # the first object in the forward function is self so we start from the second object
-                            input_signature.validate_object(args[i + 1], True)
-                        )
-                    except UDFIODefinitionError as e:
-                        raise UDFIODefinitionError(str(e))
+                    for i, input_signature in enumerate(input_signatures):
+                        try:
+                            args_lst.append(
+                                # the first object in the forward function is self so we start from the second object
+                                input_signature.validate_object(args[i + 1], True)
+                            )
+                        except UDFIODefinitionError as e:
+                            raise UDFIODefinitionError(str(e))
 
-                if len(args_lst) > 0:
-                    args = tuple(args_lst)
+                    if len(args_lst) > 0:
+                        args = tuple(args_lst)
 
             # calling the forward function defined by the user inside the udf implementation
             output = arg_fn(*args)
 
-            if len(output_signatures) == 1:
-                try:
-                    return output_signatures[0].validate_object(output, False)
-                except UDFIODefinitionError as e:
-                    raise UDFIODefinitionError(str(e))
-            else:
-                output_lst = []
-                for i, output_signature in enumerate(output_signatures):
+            if check_output:
+                if len(output_signatures) == 1:
                     try:
-                        output_lst.append(output_signature.validate_object(output[i]))
-
+                        return output_signatures[0].validate_object(output, False)
                     except UDFIODefinitionError as e:
                         raise UDFIODefinitionError(str(e))
+                else:
+                    output_lst = []
+                    for i, output_signature in enumerate(output_signatures):
+                        try:
+                            output_lst.append(output_signature.validate_object(output[i]))
 
-                output = tuple(output)
+                        except UDFIODefinitionError as e:
+                            raise UDFIODefinitionError(str(e))
 
-                return output
+                    output = tuple(output)
+
+            return output
 
         tags = {}
         tags["input"] = input_signatures
