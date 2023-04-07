e51303dc9f7707431cae50779c922ccd5a0777d9
Kaushik Ravichandran <kravicha3@ada-01.cc.gatech.edu>
2023-04-07 17:34:34 -0400

    lint fix

--- test/models/catalog/test_frame_info.py
+++ test/models/catalog/test_frame_info.py
@@ -12,8 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import unittest
 import asyncio
+import unittest
 
 from eva.models.catalog.frame_info import FrameInfo
 from eva.models.catalog.properties import ColorSpace
