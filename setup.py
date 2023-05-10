884c78dd8757082d6b390ad9f4993aaee9e964d9
Gaurav Tarlok Kakkar <gaurav21776@gmail.com>
2023-05-10 00:38:59 -0400

    bug: move from yolo to HF for reuse testcases (#718)
    Co-authored-by: Ishwarya Sivakumar <ish23.siva@gmail.com>

--- setup.py
+++ setup.py
@@ -97,9 +97,10 @@ udf_libs = [
     "easyocr>=1.5.0",  # OCR EXTRACTION
     "ipython<8.13.0",  # NOTEBOOKS
     "thefuzz",  # FUZZY STRING MATCHING
-    "ultralytics",  # OBJECT DETECTION
+    "ultralytics",  # OBJECT DETECTION (opencv issue due to easy-ocr)
     "transformers>=4.27.4",  # HUGGINGFACE
     "openai>=0.27.4",  # CHATGPT
+    "timm>=0.6.13",  # HUGGINGFACE VISION TASKS
 ]
 
 ### NEEDED FOR EXPERIMENTAL FEATURES
