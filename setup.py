c1cd855ff24e70f020f7fb8339e5ba690082ee9f
Gaurav <gaurav21776@gmail.com>
2023-05-09 22:05:39 -0400

    fixed testcase

--- setup.py
+++ setup.py
@@ -97,7 +97,7 @@ udf_libs = [
     "easyocr>=1.5.0",  # OCR EXTRACTION
     "ipython<8.13.0",  # NOTEBOOKS
     "thefuzz",  # FUZZY STRING MATCHING
-    "ultralytics",  # OBJECT DETECTION
+    "ultralytics>=8.0.96",  # OBJECT DETECTION
     "transformers>=4.27.4",  # HUGGINGFACE
     "openai>=0.27.4",  # CHATGPT
     "timm>=0.6.13",  # HUGGINGFACE VISION TASKS
