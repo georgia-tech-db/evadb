488759847aa35bb0743fff06520c23756665cc64
Yulai Cui <alex@Yulais-MBP.fios-router.home>
2023-05-12 14:22:24 -0400

    video storage engine fix

--- eva/storage/video_storage_engine.py
+++ eva/storage/video_storage_engine.py
@@ -14,13 +14,14 @@
 # limitations under the License.
 import sys
 from pathlib import Path
-from typing import Iterator
+from typing import Iterator, List
 
 from eva.catalog.models.table_catalog import TableCatalogEntry
 from eva.expression.abstract_expression import AbstractExpression
 from eva.models.storage.batch import Batch
 from eva.readers.decord_reader import DecordReader
 from eva.storage.abstract_media_storage_engine import AbstractMediaStorageEngine
+from eva.utils.logging_manager import logger
 
 
 class DecordStorageEngine(AbstractMediaStorageEngine):
@@ -58,3 +59,16 @@ class DecordStorageEngine(AbstractMediaStorageEngine):
                     batch.frames[table.columns[0].name] = row_id
                     batch.frames[table.columns[1].name] = str(video_file_name)
                     yield batch
+
+    def clear(self, table: TableCatalogEntry, video_paths: List[Path]):
+        try:
+            media_metadata_table = self._get_metadata_table(table)
+            for video_path in video_paths:
+                dst_file_name = self._xform_file_url_to_file_name(Path(video_path))
+                image_file = Path(table.file_url) / dst_file_name
+                self._rdb_handler.clear(media_metadata_table)
+                image_file.unlink()
+        except Exception as e:
+            error = f"Deleting file path {video_paths} failed with exception {e}"
+            logger.exception(error)
+            raise RuntimeError(error)
