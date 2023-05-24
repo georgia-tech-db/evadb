e4f47ca85a7b743291d0589002621e4e93b58f6c
jarulraj <arulraj@gatech.edu>
2023-05-24 18:59:48 -0400

    updates

--- test/integration_tests/test_load_pdf_executor.py
+++ test/integration_tests/test_load_pdf_executor.py
@@ -13,9 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import unittest
-from test.util import (
-    shutdown_ray,
-)
 
 import pytest
 
