cdb97904e7302777338916b6f49dd5bc68a5afbc
vivek-mandal <vivek_mandal@outlook.com>
2023-05-26 02:23:19 +0530

    Updated UDF and corresponding notebook to test added

--- eva/udfs/saliency_feature_extractor.py
+++ eva/udfs/saliency_feature_extractor.py
@@ -13,40 +13,27 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import cv2
+import kornia
 import numpy as np
 import pandas as pd
-
-from typing import List
-
-##############
-import pandas as pd
-import numpy as np
-import os
-import torchvision
 import torch
+import torchvision
 import torch.nn as nn
 import torch.nn.functional as F
-
-
-from eva.models.catalog.frame_info import FrameInfo
-from eva.models.catalog.properties import ColorSpace
-from eva.udfs.abstract.pytorch_abstract_udf import PytorchAbstractClassifierUDF
-from torch import Tensor
-from torchvision import models
-from torchvision.transforms import Compose, ToTensor, Resize, ToPILImage
-from PIL import Image
-
-##################
 
 from eva.catalog.catalog_type import NdArrayType
 from eva.udfs.abstract.abstract_udf import AbstractUDF
 from eva.udfs.decorators.decorators import forward, setup
-from eva.udfs.decorators.io_descriptors.data_types import PandasDataframe,PyTorchTensor
+from eva.udfs.decorators.io_descriptors.data_types import PandasDataframe
+from eva.udfs.gpu_compatible import GPUCompatible
+from torchvision.transforms import Compose, ToTensor, Resize
+from PIL import Image
 
 
-class SaliencyMaps(AbstractUDF):
-    @setup(cacheable=False, udf_type="Classification", batchable=True)
+class SaliencyFeatureExtractor(AbstractUDF, GPUCompatible):
+    @setup(cacheable=False, udf_type="FeatureExtraction", batchable=False)
     def setup(self):
+        # self.model = kornia.feature.SIFTDescriptor(100)
         self.model = torchvision.models.resnet18(pretrained=True)
         num_features = self.model.fc.in_features
         self.model.fc = nn.Linear(num_features, 2) # binary classification (num_of_class == 2)
@@ -55,45 +42,48 @@ class SaliencyMaps(AbstractUDF):
         self.model.load_state_dict(model_state)
         self.model.eval()
 
+    def to_device(self, device: str) -> GPUCompatible:
+        self.model = self.model.to(device)
+        return self
+
     @property
-    def name(self):
-        return "SaliencyMaps"
+    def name(self) -> str:
+        return "SaliencyFeatureExtractor"
 
     @forward(
         input_signatures=[
-          PandasDataframe(
+            PandasDataframe(
                 columns=["data"],
-                column_types=[NdArrayType.FLOAT32],
+                column_types=[NdArrayType.UINT8],
                 column_shapes=[(None, None, 3)],
             )
-      ],
+        ],
         output_signatures=[
             PandasDataframe(
                 columns=["saliency"],
                 column_types=[NdArrayType.FLOAT32],
-                column_shapes=[(None, None, 3)],
+                column_shapes=[(1, 224,224)],
             )
         ],
     )
-    def forward(self, frames: pd.DataFrame) -> pd.DataFrame:
-        """
-        Apply Gaussian Blur to the frame
+    def forward(self, df: pd.DataFrame) -> pd.DataFrame:
+        def _forward(row: pd.Series) -> np.ndarray:
+            rgb_img = row[0]
 
-         Returns:
-             ret (pd.DataFrame): The modified frame.
-        """
-        
-        outcome = pd.DataFrame(columns=["saliency"])
-        frame_np = np.expand_dims(frames["saliency.data"][0],axis=0)
-        
-        frames_tensor = torch.tensor(frame_np).float()
-        frames_tensor.requires_grad_()
-        frames_shape = frames_tensor.size()
-        outputs = self.model(frames_tensor)
-        score_max_index = outputs.argmax()
-        score_max = outputs[0,score_max_index]
-        score_max.backward()
-        saliency, _ = torch.max(frames_tensor.grad.data.abs(),dim=1)
+            composed = Compose([
+            Resize((224, 224)),            
+            ToTensor()
+            ])
+            transfromed_img = composed(Image.fromarray(rgb_img[:, :, ::-1])).unsqueeze(0)
+            transfromed_img.requires_grad_()
+            outputs = self.model(transfromed_img)
+            score_max_index = outputs.argmax()
+            score_max = outputs[0,score_max_index]
+            score_max.backward()
+            saliency, _ = torch.max(transfromed_img.grad.data.abs(),dim=1)
 
-        outcome.loc[len(outcome.index)] = [saliency]
-        return outcome
+            return saliency
+
+        ret = pd.DataFrame()
+        ret["saliency"] = df.apply(_forward, axis=1)
+        return ret
