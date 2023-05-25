c5697ee2db7418cee9a5101fbd8e743f0f43971e
vivek-mandal <vivek_mandal@outlook.com>
2023-05-26 02:33:15 +0530

    Updated test cases for the Saliency Feature

--- test/integration_tests/test_saliency.py
+++ test/integration_tests/test_saliency.py
@@ -28,29 +28,13 @@ from eva.catalog.catalog_manager import CatalogManager
 from eva.models.storage.batch import Batch
 from eva.server.command_handler import execute_query_fetch_all
 
-# import matplotlib.pyplot as plt
-# import matplotlib as mpl
-# # mpl.use('TkAgg')
-# plt.ioff()
-
 
 @pytest.mark.notparallel
 class SaliencyTests(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         CatalogManager().reset()
-        # video_file_path = create_sample_video(NUM_FRAMES)
-
-        # Saliency1 = f"/Users/afaanansari/Desktop/gtech/eva_pdf_load/eva/test/integration_tests/test1.jpeg"
-        # Saliency2 = f"/Users/afaanansari/Desktop/gtech/eva_pdf_load/eva/test/integration_tests/test2.jpeg"
-
-
-        # load_query = f"LOAD VIDEO '{video_file_path}' INTO MyVideo;"
-        # execute_query_fetch_all(f"LOAD IMAGE '{Saliency1}' INTO MRI;")
-        # execute_query_fetch_all(f"LOAD IMAGE '{Saliency2}' INTO MRI;")
-        # execute_query_fetch_all(load_query)
-        # load_udfs_for_testing(mode="minimal")
-
+       
     @classmethod
     def tearDownClass(cls):
         shutdown_ray()
@@ -58,41 +42,18 @@ class SaliencyTests(unittest.TestCase):
         execute_query_fetch_all("DROP TABLE IF EXISTS SALIENCY;")
         # file_remove("dummy.avi")
 
-    # integration test
-
-    # def test_load_image(self):
-    #     Saliency1 = f"data/saliency/test1.jpeg"
-    #     create_udf_query = f"LOAD IMAGE '{Saliency1}' INTO SALIENCY;"
-       
-    #     execute_query_fetch_all(create_udf_query)
-
     def test_saliency(self):
         Saliency1 = f"data/saliency/test1.jpeg"
         create_udf_query = f"LOAD IMAGE '{Saliency1}' INTO SALIENCY;"
        
         execute_query_fetch_all(create_udf_query)
-        create_udf_query = """CREATE UDF IF NOT EXISTS SaliencyMaps
-                  INPUT (data NDARRAY UINT8(3, 224, 224)) 
-                  OUTPUT (saliency ANYTYPE) 
-                  TYPE  Classification 
-                  IMPL 'eva/udfs/saliency_map.py';
+        create_udf_query = """CREATE UDF IF NOT EXISTS SaliencyFeatureExtractor
+                    IMPL  'eva/udfs/saliency_feature_extractor.py';
         """
         execute_query_fetch_all(create_udf_query)
 
-        select_query_saliency = """SELECT data, SaliencyMaps(data)
-                        FROM SALIENCY;
-                       """
+        select_query_saliency = """SELECT data, SaliencyFeatureExtractor(data)
+                  FROM SALIENCY
+        """
         actual_batch_saliency = execute_query_fetch_all(select_query_saliency)
-        print(actual_batch_saliency.frames)
-        
-        # df = actual_batch_saliency.frames
-        # fig, ax = plt.subplots(nrows=2, ncols=len(df), figsize=[15,18])
-
-        # for i in range(len(df)):
-        #     img = df['mri.data'].iloc[i]
-        #     ax[0,i].imshow(img)
-        #     ax[1,i].imshow(df["mricnn.saliency"][i][0],cmap=plt.cm.hot)
-        #     ax[0,i].axis('off')
-        #     ax[1,i].axis('off')
-        # plt.gcf().set_size_inches(5, 5)
-        # plt.show()
+        self.assertEqual(len(actual_batch_saliency.columns), 2)
