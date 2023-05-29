9df801ea37f400dc5dd7d5f90d1837c04421d48c
Gaurav <gaurav21776@gmail.com>
2023-05-29 16:55:02 -0400

    temp server fix

--- test/interfaces/relational/test_relational_api.py
+++ test/interfaces/relational/test_relational_api.py
@@ -1,4 +1,5 @@
 import os
+import time
 import unittest
 from eva.catalog.catalog_manager import CatalogManager
 from eva.configuration.constants import EVA_ROOT_DIR
@@ -13,13 +14,22 @@ class RelationalAPI(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+    @classmethod
+    def setUpClass(cls):
+        os.system("nohup eva_server --stop")
+        os.system("nohup eva_server --port 8886 --start &")
+        time.sleep(5)
+
+    @classmethod
+    def tearDownClass(cls):
+        os.system("nohup eva_server --stop")
+
     def setUp(self):
         os.system("nohup eva_server --stop")
         os.system("nohup eva_server --port 8886 --start &")
         CatalogManager().reset()
         self.mnist_path = f"{EVA_ROOT_DIR}/data/mnist/mnist.mp4"
         load_udfs_for_testing()
-
         self.images = f"{EVA_ROOT_DIR}/data/detoxify/*.jpg"
 
     def tearDown(self):
@@ -27,7 +37,6 @@ class RelationalAPI(unittest.TestCase):
         # todo: move these to relational apis as well
         execute_query_fetch_all("""DROP TABLE IF EXISTS mnist_video;""")
         execute_query_fetch_all("""DROP TABLE IF EXISTS meme_images;""")
-        os.system("nohup eva_server --stop")
 
     def test_relation_apis(self):
         conn = connect(port=8886)
