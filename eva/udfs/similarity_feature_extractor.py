4aa0ce53be4f5e80bb3f7e6d3e0b64d2d59f3348
affan00733 <affanmohhd@gmail.com>
2023-06-01 14:05:04 +0530

    changes

--- eva/udfs/similarity_feature_extractor.py
+++ eva/udfs/similarity_feature_extractor.py
@@ -1,3 +1,17 @@
+# coding=utf-8
+# Copyright 2018-2023 EVA
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # coding=utf-8
 # Copyright 2018-2022 EVA
@@ -13,28 +27,21 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import cv2
-import kornia
 import numpy as np
 import pandas as pd
-import torch
-import torchvision
-import torch.nn as nn
-import torch.nn.functional as F
+import spacy
 
 from eva.catalog.catalog_type import NdArrayType
 from eva.udfs.abstract.abstract_udf import AbstractUDF
 from eva.udfs.decorators.decorators import forward, setup
 from eva.udfs.decorators.io_descriptors.data_types import PandasDataframe
 from eva.udfs.gpu_compatible import GPUCompatible
-from torchvision.transforms import Compose, ToTensor, Resize
-from PIL import Image
 
-import spacy
 try:
     nlp = spacy.load("en_core_web_lg")
-except:
+except Exception as e:
     import spacy.cli
+
     spacy.cli.download("en_core_web_lg")
     nlp = spacy.load("en_core_web_lg")
 
@@ -55,9 +62,9 @@ class SimilarityFeatureExtractor(AbstractUDF, GPUCompatible):
     @forward(
         input_signatures=[
             PandasDataframe(
-                columns=["data","filter"],
-                column_types=[NdArrayType.STR,NdArrayType.STR],
-                column_shapes=[(1),(1)],
+                columns=["data", "filter"],
+                column_types=[NdArrayType.STR, NdArrayType.STR],
+                column_shapes=[(1), (1)],
             )
         ],
         output_signatures=[
@@ -69,12 +76,11 @@ class SimilarityFeatureExtractor(AbstractUDF, GPUCompatible):
         ],
     )
     def forward(self, df: pd.DataFrame) -> pd.DataFrame:
-
         def _forward(row: pd.Series) -> np.ndarray:
-            columns=row.axes[0]
+            columns = row.axes[0]
             data = row.loc[columns[0]]
             filter_keyword = row.loc[columns[1]]
-            if data.strip()!="":
+            if data.strip() != "":
                 doc1 = nlp(data)
                 doc2 = nlp(filter_keyword)
                 return doc1.similarity(doc2)
--- test/integration_tests/test_eva_API.py
+++ test/integration_tests/test_eva_API.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-# Copyright 2018-2022 EVA
+# Copyright 2018-2023 EVA
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,20 +13,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import unittest
+from unittest.mock import AsyncMock
 
 import pytest
-import asyncio
 
 from eva.catalog.catalog_manager import CatalogManager
 from eva.configuration.constants import EVA_ROOT_DIR
-from eva.interfaces.relational.db import connect, EVACursor
-from unittest.mock import AsyncMock
+from eva.interfaces.relational.db import EVACursor
+
+
 @pytest.mark.asyncio
 class EVAAPITests(unittest.TestCase):
     def setUp(self):
         # reset the catalog manager before running each test
         CatalogManager().reset()
-    
+
     # async def get_connection_async(self):
     #     reader, writer = await asyncio.open_connection(host="127.0.0.1", port=8803)
     #     return reader, writer
@@ -40,28 +41,29 @@ class EVAAPITests(unittest.TestCase):
 
     def test_udf_eva_api(self):
         pdf_path = f"{EVA_ROOT_DIR}/data/documents/state_of_the_union.pdf"
-        
+
         connection = AsyncMock()
         cursor = EVACursor(connection)
 
-        load_pdf =cursor.load(file_regex=pdf_path,format="PDF",table_name="PDFss")
+        load_pdf = cursor.load(file_regex=pdf_path, format="PDF", table_name="PDFss")
         load_pdf.execute()
 
         udf_check = cursor.query("DROP UDF IF  EXISTS SimilarityFeatureExtractor")
         udf_check.execute()
-        udf = cursor.query(f"""CREATE UDF IF NOT EXISTS SimilarityFeatureExtractor
-                            IMPL  '{EVA_ROOT_DIR}/eva/udfs/similarity_feature_extractor.py'""")
+        udf = cursor.query(
+            f"""CREATE UDF IF NOT EXISTS SimilarityFeatureExtractor
+                            IMPL  '{EVA_ROOT_DIR}/eva/udfs/similarity_feature_extractor.py'"""
+        )
         udf.execute()
 
         table = cursor.table("PDFss")
-        table_udf = table.cross_apply("SimilarityFeatureExtractor(data,'BLOOD')","objs(simiarity)")
-        table_udf_data = table_udf.df()        
+        table_udf = table.cross_apply(
+            "SimilarityFeatureExtractor(data,'BLOOD')", "objs(simiarity)"
+        )
+        table_udf_data = table_udf.df()
         self.assertEqual(len(table_udf_data.columns), 6)
         self.assertTrue("objs.simiarity" in table_udf_data.columns)
         self.assertTrue("pdfss.data" in table_udf_data.columns)
         self.assertTrue(
-            all(
-                isinstance(x, float) for x in table_udf_data["objs.simiarity"]
-            )
+            all(isinstance(x, float) for x in table_udf_data["objs.simiarity"])
         )
-
