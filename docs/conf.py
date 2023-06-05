f513ca46fbaf3acca8a6a03822a98dc5545488c0
Gaurav <gaurav21776@gmail.com>
2023-06-05 03:14:41 -0400

    minor fixes

--- docs/conf.py
+++ docs/conf.py
@@ -87,7 +87,7 @@ master_doc = "index"
 # General information about the project.
 project = "EVA DB"
 copyright = str(date.today().year) + ", EVA DB."
-author = "EVA DB"
+author = u"EVA DB"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
