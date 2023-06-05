ba05955a4a5a347124b3e7479475bb4125e78714
affan00733 <affanmohhd@gmail.com>
2023-06-05 11:22:36 +0530

    fixes

---
+++ eva/udfs/spacy_feature_extractor.py
@@ -0,0 +1,90 @@
+# coding=utf-8
+# Copyright 2018-2023 EVA
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# coding=utf-8
+# Copyright 2018-2022 EVA
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import numpy as np
+import pandas as pd
+import spacy
+
+from eva.catalog.catalog_type import NdArrayType
+from eva.udfs.abstract.abstract_udf import AbstractUDF
+from eva.udfs.decorators.decorators import forward, setup
+from eva.udfs.decorators.io_descriptors.data_types import PandasDataframe
+from eva.udfs.gpu_compatible import GPUCompatible
+
+try:
+    nlp = spacy.load("en_core_web_lg")
+except Exception as e:
+    print(e)
+    import spacy.cli
+
+    spacy.cli.download("en_core_web_lg")
+    nlp = spacy.load("en_core_web_lg")
+
+
+class SpacyFeatureExtractor(AbstractUDF, GPUCompatible):
+    @setup(cacheable=False, udf_type="FeatureExtraction", batchable=False)
+    def setup(self):
+        pass
+
+    def to_device(self, device: str) -> GPUCompatible:
+        self.model = self.model.to(device)
+        return self
+
+    @property
+    def name(self) -> str:
+        return "SpacyFeatureExtractor"
+
+    @forward(
+        input_signatures=[
+            PandasDataframe(
+                columns=["data"],
+                column_types=[NdArrayType.STR],
+                column_shapes=[(1)],
+            )
+        ],
+        output_signatures=[
+            PandasDataframe(
+                columns=["features"],
+                column_types=[NdArrayType.FLOAT32],
+                column_shapes=[(1, 128)],
+            )
+        ],
+    )
+    def forward(self, df: pd.DataFrame) -> pd.DataFrame:
+        def _forward(row: pd.Series) -> np.ndarray:
+            columns = row.axes[0]
+            data = row.loc[columns[0]]
+            doc1 = nlp(data)
+            feat = doc1.vector
+            feat = feat.reshape(1, -1)
+            return feat
+
+        ret = pd.DataFrame()
+        ret["features"] = df.apply(_forward, axis=1)
+        return ret
