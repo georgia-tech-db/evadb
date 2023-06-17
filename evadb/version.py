5f2d1a4f3549c5663efa0749d77283a5770f2eff
jarulraj <arulraj@gatech.edu>
2023-06-17 00:30:41 -0400

    [BUMP]: v0.2.14+dev

--- evadb/version.py
+++ evadb/version.py
@@ -1,6 +1,6 @@
 _MAJOR = "0"
 _MINOR = "2"
-_REVISION = "13"
+_REVISION = "14+dev"
 
 VERSION_SHORT = f"{_MAJOR}.{_MINOR}"
-VERSION = f"{_MAJOR}.{_MINOR}.{_REVISION}"  # noqa: W292
+VERSION = f"{_MAJOR}.{_MINOR}.{_REVISION}"
