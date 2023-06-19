b76d15676b079cef1db5d5dbeaf5969f85c4d628
jarulraj <arulraj@gatech.edu>
2023-06-19 09:23:56 -0400

    updates

--- evadb/executor/ray_utils.py
+++ evadb/executor/ray_utils.py
@@ -12,3 +12,51 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import os
+from typing import Callable, Dict
+
+from evadb.executor.executor_utils import ExecutorError
+
+
+class StageCompleteSignal:
+    pass
+
+
+def ray_wait_and_alert(tasks, queue):
+    import ray
+    from ray.exceptions import RayTaskError
+
+    try:
+        ray.get(tasks)
+        queue.put(StageCompleteSignal)
+    except RayTaskError as e:
+        queue.put(ExecutorError(e.cause))
+
+
+# Max calls set to 1 to forcefully release GPU resource when the job is
+# complete. We choose to bypass resource management of Ray, but instead
+# control GPU resource ourselves by configuring the environmental variables
+# when the job enters the Ray process. Due to that, resource release is not
+# cleanly done on the Ray side, we need to set this to prevent memory leak.
+# More detailed explanation can be found in
+# https://github.com/georgia-tech-db/eva/pull/731
+def ray_parallel(
+    conf_dict: Dict[str, str],
+    executor: Callable,
+    input_queue,
+    output_queue,
+):
+    for k, v in conf_dict.items():
+        os.environ[k] = v
+
+    gen = executor(input_queue=input_queue)
+    for next_item in gen:
+        output_queue.put(next_item)
+
+
+def ray_pull(conf_dict: Dict[str, str], executor: Callable, input_queue):
+    for k, v in conf_dict.items():
+        os.environ[k] = v
+
+    for next_item in executor():
+        input_queue.put(next_item)
