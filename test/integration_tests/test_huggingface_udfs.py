3f12cbda2626286e6ff262afa0f59fb07bd92f38
Joy Arulraj <arulraj@gatech.edu>
2023-06-26 20:50:21 -0400

    feat: remove ocr (#905)
    👋 Thanks for submitting a Pull Request to EvaDB!
    
    🙌 We want to make contributing to EvaDB as easy and transparent as
    possible. Here are a few tips to get you started:
    
    - 🔍 Search existing EvaDB
    [PRs](https://github.com/georgia-tech-db/eva/pulls) to see if a similar
    PR already exists.
    - 🔗 Link this PR to a EvaDB
    [issue](https://github.com/georgia-tech-db/eva/issues) to help us
    understand what bug fix or feature is being implemented.
    - 📈 Provide before and after profiling results to help us quantify the
    improvement your PR provides (if applicable).
    
    👉 Please see our ✅ [Contributing
    Guide](https://evadb.readthedocs.io/en/stable/source/contribute/index.html)
    for more details.
    
    ---------
    
    Co-authored-by: Chitti Ankith <chittiankith@gmail.com>

--- test/integration_tests/test_huggingface_udfs.py
+++ test/integration_tests/test_huggingface_udfs.py
@@ -276,7 +276,6 @@ class HuggingFaceTests(unittest.TestCase):
         drop_udf_query = f"DROP UDF {summary_udf};"
         execute_query_fetch_all(self.evadb, drop_udf_query)
 
-    @pytest.mark.benchmark
     def test_toxicity_classification(self):
         udf_name = "HFToxicityClassifier"
         create_udf_query = f"""CREATE UDF {udf_name}
