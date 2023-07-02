3c3c002f2bd164313b10fd01f23c05c8fddf2735
Gaurav <gaurav21776@gmail.com>
2023-07-02 14:58:52 -0400

    revert changes

--- evadb/readers/pdf_reader.py
+++ evadb/readers/pdf_reader.py
@@ -35,12 +35,10 @@ class PDFReader(AbstractReader):
         doc = fitz.open(self.file_url)
 
         # PAGE ID, PARAGRAPH ID, STRING
-        # Maintain a global paragraph number per PDF
-        global_paragraph_no = 0
         for page_no, page in enumerate(doc):
             blocks = page.get_text("dict")["blocks"]
             # iterate through the text blocks
-            for _, b in enumerate(blocks):
+            for paragraph_no, b in enumerate(blocks):
                 # this block contains text
                 if b["type"] == 0:
                     # text found in block
@@ -53,8 +51,7 @@ class PDFReader(AbstractReader):
                             if span["text"].strip():
                                 block_string += span["text"]
                     yield {
-                        "paragraph": global_paragraph_no,
-                        "page": page_no,
+                        "page": page_no + 1,
+                        "paragraph": paragraph_no + 1,
                         "data": block_string,
                     }
-                    global_paragraph_no += 1
