9cbf9ad4ba4d28bdb8213f351c86b582b3cc3f17
yulaicui <ycui96@gatech.edu>
2023-07-17 01:10:51 -0400

    enable WAL

--- evadb/catalog/sql_config.py
+++ evadb/catalog/sql_config.py
@@ -75,7 +75,7 @@ class SQLConfig(metaclass=SingletonMeta):
                 # Currently, there are too many connections being made, which is not an
                 # optimal design. Ideally, we should implement a connection pool for
                 # better management.
-                # dbapi_con.execute("pragma journal_mode=WAL")
+                dbapi_con.execute("pragma journal_mode=WAL")
 
             event.listen(self.engine, "connect", _enable_sqlite_pragma)
         # statements
