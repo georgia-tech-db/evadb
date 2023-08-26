40993c4330a71729a266b2b44f8d1b3d7546d916
Gaurav Tarlok Kakkar <gaurav21776@gmail.com>
2023-08-25 22:00:33 -0400

    Bump v0.3.3+dev (#963)

--- script/releasing/releaser.py
+++ script/releasing/releaser.py
@@ -35,11 +35,13 @@ from github import Github
 
 background_loop = asyncio.new_event_loop()
 
+
 class SemanticVersionTypeEnum:
-    PRERELEASE = 'prerelease' # 0.2.4+dev -> 0.2.4.alpha.1
-    PATCH = 'patch' # 0.2.4+dev -> 0.2.5
-    MINOR = 'minor' # 0.2.4+dev -> 0.3.0
-    MAJOR = 'major' # 0.2.4+dev -> 1.0.0
+    PRERELEASE = "prerelease"  # 0.2.4+dev -> 0.2.4.alpha.1
+    PATCH = "patch"  # 0.2.4+dev -> 0.2.5
+    MINOR = "minor"  # 0.2.4+dev -> 0.3.0
+    MAJOR = "major"  # 0.2.4+dev -> 1.0.0
+
 
 def background(f):
     def wrapped(*args, **kwargs):
@@ -47,9 +49,11 @@ def background(f):
 
     return wrapped
 
+
 def get_string_in_line(file_path, line_number):
     line = linecache.getline(file_path, line_number)
     return line.strip()
+
 
 # ==============================================
 # CONFIGURATION
@@ -91,18 +95,16 @@ def run_command(command_str: str):
         command_str, shell=True, universal_newlines=True
     ).rstrip()
 
-
     pprint(command_str)
 
-    if 'version' in command_str:
+    if "version" in command_str:
         pprint(output)
     return output
 
 
 def get_changelog(github_timestamp):
-
     release_date = datetime.fromisoformat(github_timestamp[:-1])
-    utc_timezone = pytz.timezone('UTC')
+    utc_timezone = pytz.timezone("UTC")
     release_date = utc_timezone.localize(release_date)
 
     # GO TO ROOT DIR
@@ -161,31 +163,33 @@ def release_version(current_version):
     with open(version_path, "w") as version_file:
         version_file.write(output)
 
-    NEXT_RELEASE = current_version # without dev part
+    NEXT_RELEASE = current_version  # without dev part
 
     run_command("git checkout -b release-" + NEXT_RELEASE)
     run_command("git add . -u")
     run_command("git commit -m '[RELEASE]: " + NEXT_RELEASE + "'")
     run_command("git push --set-upstream origin release-" + NEXT_RELEASE)
 
-    #run_command(f"git tag -a {NEXT_RELEASE} -m '{NEXT_RELEASE} release'")
+    # run_command(f"git tag -a {NEXT_RELEASE} -m '{NEXT_RELEASE} release'")
     run_command(f"git push origin release-{NEXT_RELEASE}")
+
 
 def get_commit_id_of_latest_release():
     import requests
 
-    repo = 'georgia-tech-db/evadb'
-    url = f'https://api.github.com/repos/{repo}/releases'
+    repo = "georgia-tech-db/evadb"
+    url = f"https://api.github.com/repos/{repo}/releases"
     response = requests.get(url)
     data = response.json()
 
     latest_release = data[0]
-    release_date = latest_release['created_at']
+    release_date = latest_release["created_at"]
 
     return release_date
 
+
 def append_changelog(insert_changelog: str, version: str):
-    with open(EvaDB_CHANGELOG_PATH, 'r') as file:
+    with open(EvaDB_CHANGELOG_PATH, "r") as file:
         file_contents = file.read()
 
     # Location after ### [Removed] in file
@@ -195,12 +199,19 @@ def append_changelog(insert_changelog: str, version: str):
     version = version.split("+")[0]
 
     today_date = date.today()
-    header= f"##  [{version}] - {today_date}\n\n"
+    header = f"##  [{version}] - {today_date}\n\n"
 
-    modified_content = file_contents[:position] + header + insert_changelog +  "\n" + file_contents[position:]
+    modified_content = (
+        file_contents[:position]
+        + header
+        + insert_changelog
+        + "\n"
+        + file_contents[position:]
+    )
 
-    with open(EvaDB_CHANGELOG_PATH, 'w') as file:
+    with open(EvaDB_CHANGELOG_PATH, "w") as file:
         file.write(modified_content)
+
 
 def publish_wheels(tag):
     run_command("rm -rf dist build")
@@ -218,36 +229,33 @@ def publish_wheels(tag):
 def upload_assets(changelog, tag):
     # Authentication token
     access_token = os.environ["GITHUB_KEY"]
-
+    print(access_token)
     # Repository information
-    repo_owner = 'georgia-tech-db'
-    repo_name = 'evadb'
+    repo_owner = "georgia-tech-db"
+    repo_name = "evadb"
 
     # Release information
-    tag_name = 'v' + tag
-    assert 'vv' not in tag
-    asset_filepaths = [
-        f'dist/evadb-{tag}-py3-none-any.whl', 
-        f'dist/evadb-{tag}.tar.gz'
-    ]
+    tag_name = "v" + tag
+    assert "vv" not in tag
+    asset_filepaths = [f"dist/evadb-{tag}-py3-none-any.whl", f"dist/evadb-{tag}.tar.gz"]
 
     # Create a PyGithub instance
     g = Github(access_token)
 
     # Get the repository
-    repo = g.get_repo(f'{repo_owner}/{repo_name}')
+    repo = g.get_repo(f"{repo_owner}/{repo_name}")
 
     # Create the release
     release_name = tag_name
-    release_message = f'{tag_name}\n\n {changelog}' 
+    release_message = f"{tag_name}\n\n {changelog}"
 
     # Publish the release
     release = repo.create_git_release(
-        tag=tag_name, 
-        name=release_name, 
-        message=release_message, 
-        draft=False, 
-        prerelease=False
+        tag=tag_name,
+        name=release_name,
+        message=release_message,
+        draft=False,
+        prerelease=False,
     )
 
     # Retrieve the release by tag name
@@ -255,12 +263,13 @@ def upload_assets(changelog, tag):
 
     # Upload assets to the release
     for filepath in asset_filepaths:
-        asset_name = filepath.split('/')[-1]
+        asset_name = filepath.split("/")[-1]
         asset_path = Path(f"{EvaDB_DIR}/{filepath}")
         print("path: " + str(asset_path))
         release.upload_asset(str(asset_path), asset_name)
 
-    print('Release created and published successfully.')
+    print("Release created and published successfully.")
+
 
 def bump_up_version(next_version):
     version_path = os.path.join(os.path.join(EvaDB_DIR, "evadb"), "version.py")
@@ -290,6 +299,7 @@ def bump_up_version(next_version):
     run_command("git commit -m '[BUMP]: " + NEXT_RELEASE + "'")
     run_command("git push --set-upstream origin bump-" + NEXT_RELEASE)
 
+
 # ==============================================
 # Main Function
 # ==============================================
@@ -306,57 +316,57 @@ if __name__ == "__main__":
 
     parser.add_argument(
         "-n",
-        "--next-release-type", 
-        type=str, 
+        "--next-release-type",
+        type=str,
         choices=[
-            SemanticVersionTypeEnum.PRERELEASE, 
-            SemanticVersionTypeEnum.PATCH, 
+            SemanticVersionTypeEnum.PRERELEASE,
+            SemanticVersionTypeEnum.PATCH,
             SemanticVersionTypeEnum.MINOR,
-            SemanticVersionTypeEnum.MAJOR
+            SemanticVersionTypeEnum.MAJOR,
         ],
         help="Next version type.",
-        required=True
+        required=True,
     )
 
     parser.add_argument(
         "-c",
         "--get-changelog",
-        action='store_true',
+        action="store_true",
         help="Retrieve the changelog since last release",
     )
 
     parser.add_argument(
         "-u",
         "--update-changelog",
-        action='store_true',
+        action="store_true",
         help="Update changelog.",
     )
 
     parser.add_argument(
         "-r",
         "--release-version",
-        action='store_true',
+        action="store_true",
         help="Publish a new version of EvaDB.",
     )
 
     parser.add_argument(
         "-p",
         "--publish-to-pypi",
-        action='store_true',
+        action="store_true",
         help="Create wheels and upload to PyPI.",
     )
 
     parser.add_argument(
         "-a",
         "--upload-assets",
-        action='store_true',
+        action="store_true",
         help="Upload assets on Github.",
     )
 
     parser.add_argument(
         "-b",
         "--bump-up-version",
-        action='store_true',
+        action="store_true",
         help="Bump up version.",
     )
 
@@ -369,7 +379,7 @@ if __name__ == "__main__":
     print("CURRENT VERSION: " + current_version)
     current_version_str_without_dev = current_version
     if "dev" in current_version:
-        current_version_str_without_dev = current_version.split('+')[0]
+        current_version_str_without_dev = current_version.split("+")[0]
     print("CURRENT VERSION WITHOUT DEV: " + current_version_str_without_dev)
 
     selected_release_type = args.next_release_type
@@ -385,7 +395,7 @@ if __name__ == "__main__":
         next_version = version.next_minor()
         next_version_str = "v" + str(next_version)
     elif selected_release_type == SemanticVersionTypeEnum.MAJOR:
-        next_version = version.next_major()    
+        next_version = version.next_major()
         next_version_str = "v" + str(next_version)
 
     print("NEXT    VERSION : " + next_version_str)
@@ -416,24 +426,32 @@ if __name__ == "__main__":
         bump_up_version(next_version)
 
     ## DO EVERYTHING BY DEFAULT
+    is_all = not (
+        args.get_changelog
+        | args.update_changelog
+        | args.release_version
+        | args.publish_to_pypi
+        | args.upload_assets
+        | args.bump_up_version
+    )
 
-    # GET CHANGELOG 
-    release_date = get_commit_id_of_latest_release()
-    changelog = get_changelog(release_date)
-    print(changelog)
+    if is_all:
+        # GET CHANGELOG
+        release_date = get_commit_id_of_latest_release()
+        changelog = get_changelog(release_date)
+        print(changelog)
 
-    # UPDATE CHANGELOG
-    append_changelog(changelog, current_version_str_without_dev)
+        # UPDATE CHANGELOG
+        append_changelog(changelog, current_version_str_without_dev)
 
-    # RELEASE VERSION
-    release_version(current_version_str_without_dev)
+        # RELEASE VERSION
+        release_version(current_version_str_without_dev)
 
-    # PUBLISH WHEELS ON PYPI
-    publish_wheels(current_version_str_without_dev)
+        # PUBLISH WHEELS ON PYPI
+        publish_wheels(current_version_str_without_dev)
 
-    # UPLOAD ASSETS ON GITHUB
-    upload_assets(changelog, current_version_str_without_dev)
+        # UPLOAD ASSETS ON GITHUB
+        upload_assets(changelog, current_version_str_without_dev)
 
-    # BUMP UP VERSION
-    bump_up_version(next_version)
-
+        # BUMP UP VERSION
+        bump_up_version(next_version)
