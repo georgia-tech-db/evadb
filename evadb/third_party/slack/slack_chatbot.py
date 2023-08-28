6ac71f874b095da5dbf4232fb8cfede9f3106519
Kaushik Ravichandran <kravicha3@ada-01.cc.gatech.edu>
2023-08-28 13:17:55 -0400

    use keys from yml

--- evadb/third_party/slack/slack_chatbot.py
+++ evadb/third_party/slack/slack_chatbot.py
@@ -1,9 +1,12 @@
-SLACK_BOT_TOKEN = "xoxb-5796901551090-5819342908849-iJkccELdXM246Y7SejIYpzsq"
-SLACK_APP_TOKEN = "xapp-1-A05Q39W0CBB-5792165989415-e2b813f745b32fc3227e764d2ae62633ca92f2076257801c692b32767ba43f3b"
-
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 from slack import WebClient
 from slack_bolt import App
+import openai
+from evadb.configuration.configuration_manager import ConfigurationManager
+
+SLACK_BOT_TOKEN = ConfigurationManager().get_value("third_party", "SLACK_BOT_TOKEN")
+SLACK_APP_TOKEN = ConfigurationManager().get_value("third_party", "SLACK_APP_TOKEN")
+OPENAI_API_KEY = ConfigurationManager().get_value("third_party", "OPENAI_KEY")
 
 # Event API & Web API
 app = App(token=SLACK_BOT_TOKEN) 
@@ -18,22 +21,21 @@ def handle_message_events(body, logger):
     # Let the user know that we are busy with the request 
     response = client.chat_postMessage(channel=body["event"]["channel"], 
                                        thread_ts=body["event"]["event_ts"],
-                                       text=f"Hello from your bot! :robot_face: \nThanks for your request, I'm on it!")
+                                       text=f"Hello from EVADB bot! :robot_face:")
 
-    response = "I got your message: " + prompt
     # Check ChatGPT
-    # openai.api_key = OPENAI_API_KEY
-    # response = openai.Completion.create(
-    #     engine="text-davinci-003",
-    #     prompt=prompt,
-    #     max_tokens=1024,
-    #     n=1,
-    #     stop=None,
-    #     temperature=0.5).choices[0].text
+    openai.api_key = OPENAI_API_KEY
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5).choices[0].text
 
     # Reply to thread 
     response = client.chat_postMessage(channel=body["event"]["channel"], 
                                        thread_ts=body["event"]["event_ts"],
-                                       text=f"Here you go: \n{response}")
+                                       text=f"{response}")
 
-# SocketModeHandler(app, SLACK_APP_TOKEN).start()
+SocketModeHandler(app, SLACK_APP_TOKEN).start()
