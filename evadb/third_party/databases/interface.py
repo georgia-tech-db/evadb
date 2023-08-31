e168c0ce784c86af9e6fd6ad3bc810445ad8d970
Gaurav Tarlok Kakkar <gaurav21776@gmail.com>
2023-08-30 23:11:37 -0400

    fix linter

--- evadb/third_party/databases/interface.py
+++ evadb/third_party/databases/interface.py
@@ -26,9 +26,7 @@ def get_database_handler(engine: str, **kwargs):
     try:
         mod = dynamic_import(engine)
     except ImportError:
-        req_file = os.path.join(
-            os.path.dirname(__file__), engine, "requirements.txt"
-        )
+        req_file = os.path.join(os.path.dirname(__file__), engine, "requirements.txt")
         if os.path.isfile(req_file):
             with open(req_file) as f:
                 raise ImportError(f"Please install the following packages {f.read()}")
