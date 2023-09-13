8692ff1d41396132b37b08b06ecb25afb01721b1
xzdandy <xzdandy@gmail.com>
2023-09-13 04:21:27 -0400

    Add unittest when required columns are missing in binder

--- test/unit_tests/binder/test_statement_binder.py
+++ test/unit_tests/binder/test_statement_binder.py
@@ -612,3 +612,40 @@ class StatementBinderTests(unittest.TestCase):
 
             err_msg = "Unexpected column type found for forecasting function."
             self.assertEqual(str(cm.exception), err_msg)
+
+    def test_bind_create_function_should_raise_forecast_missing_required_columns(self):
+        with patch.object(StatementBinder, "bind"):
+            create_function_statement = MagicMock()
+            create_function_statement.function_type = "forecasting"
+            id_col_obj = ColumnCatalogEntry(
+                name="type",
+                type=MagicMock(),
+                array_type=MagicMock(),
+                array_dimensions=MagicMock(),
+            )
+            ds_col_obj = ColumnCatalogEntry(
+                name="saledate",
+                type=MagicMock(),
+                array_type=MagicMock(),
+                array_dimensions=MagicMock(),
+            )
+            create_function_statement.query.target_list = [
+                TupleValueExpression(
+                    name=id_col_obj.name, table_alias="a", col_object=id_col_obj
+                ),
+                TupleValueExpression(
+                    name=ds_col_obj.name, table_alias="a", col_object=ds_col_obj
+                ),
+            ]
+            create_function_statement.metadata = [
+                ("id", "type"),
+                ("time", "saledate"),
+                ("predict", "ma"),
+            ]
+            binder = StatementBinder(StatementBinderContext(MagicMock()))
+
+            with self.assertRaises(AssertionError) as cm:
+                binder._bind_create_function_statement(create_function_statement)
+
+            err_msg = "Missing required {'ma'} columns for forecasting function."
+            self.assertEqual(str(cm.exception), err_msg)
