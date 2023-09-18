8430ed82209d7ae3debdebb0057b913149e509a0
xzdandy <xzdandy@gmail.com>
2023-09-18 02:21:04 -0400

    Add testcases for if not exists

--- test/unit_tests/executor/test_create_udf_executor.py
+++ test/unit_tests/executor/test_create_udf_executor.py
@@ -57,6 +57,61 @@ class CreateFunctionExecutorTest(unittest.TestCase):
             {"key1": "value1", "key2": "value2"},
         )
 
+    def test_should_raise_or_replace_if_not_exists(self):
+        plan = type(
+            "CreateFunctionPlan",
+            (),
+            {
+                "name": "function",
+                "or_replace": True,
+                "if_not_exists": True,
+            },
+        )
+        evadb = MagicMock()
+        create_function_executor = CreateFunctionExecutor(evadb, plan)
+        with self.assertRaises(AssertionError) as cm:
+            next(create_function_executor.exec())
+        self.assertEqual(
+            str(cm.exception),
+            "OR REPLACE and IF NOT EXISTS can not be both set for CREATE FUNCTION.",
+        )
+
+    @patch("evadb.executor.create_function_executor.load_function_class_from_file")
+    def test_should_skip_if_not_exists_function(
+        self, load_function_class_from_file_mock
+    ):
+        catalog_instance = MagicMock()
+        catalog_instance().get_function_catalog_entry_by_name.return_value = True
+        catalog_instance().insert_function_catalog_entry.return_value = "function"
+        impl_path = MagicMock()
+        abs_path = impl_path.absolute.return_value = MagicMock()
+        abs_path.as_posix.return_value = "test.py"
+        load_function_class_from_file_mock.return_value.return_value = "mock_class"
+        plan = type(
+            "CreateFunctionPlan",
+            (),
+            {
+                "name": "function",
+                "or_replace": False,
+                "if_not_exists": True,
+                "inputs": ["inp"],
+                "outputs": ["out"],
+                "impl_path": impl_path,
+                "function_type": "classification",
+                "metadata": {"key1": "value1", "key2": "value2"},
+            },
+        )
+        evadb = MagicMock()
+        evadb.catalog = catalog_instance
+        evadb.config = MagicMock()
+        create_function_executor = CreateFunctionExecutor(evadb, plan)
+        actual_batch = next(create_function_executor.exec())
+        catalog_instance().insert_function_catalog_entry.assert_not_called()
+        self.assertEqual(
+            actual_batch.frames[0][0],
+            "Function function already exists, nothing added.",
+        )
+
     @patch("evadb.executor.create_function_executor.load_function_class_from_file")
     def test_should_raise_error_on_incorrect_io_definition(
         self, load_function_class_from_file_mock
