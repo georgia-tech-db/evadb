b65ad20d687d923719e8337058bd726b18568e68
imnidhi <nidhim214@gmail.com>
2023-10-11 16:28:38 -0400

    Added upload api

--- apps/rest/run_evadb.py
+++ apps/rest/run_evadb.py
@@ -1,86 +1,57 @@
-from flask import Flask
+from flask import Flask,flash, request, redirect,render_template
 from flask_restful import Api, Resource
+import os
+from werkzeug.utils import secure_filename
 import evadb
-
-app = Flask(__name__)
-api = Api(app)
+from pathlib import Path
 
 cursor = evadb.connect().cursor()
+cwd = os.getcwd()
+UPLOAD_FOLDER = cwd + "/files"
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'mp3'}
 
-class DatabaseResource(Resource):
-    def get(self,db_name):
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+api = Api(app)
+app.secret_key = "evadb-rest-apis"
 
-        # Create a database
-        query = """ CREATE DATABASE """ + db_name + """
-            WITH ENGINE = 'postgres',
-            PARAMETERS = {
-                "user": "eva",
-                "password": "password",
-                "host": "localhost",
-                "port": "5000",
-                "database":""" + db_name + """
-            }; """
-        
-        res = cursor.query(query).df()
-        return {"msg" : "Created Database" , "response" : res.to_json()}
-
-class TableResource(Resource):
-    def get(self, name):
-        query = """ 
-            CREATE TABLE IF NOT EXISTS """ + name + """ (
-                id INTEGER UNIQUE,
-                label TEXT(30)
-                    ); 
-        """
-        res = cursor.query(query).df()
-        return {"msg" : "Created table ", "response" : res.to_json() }
-    
-    def post(self,name):
-        query = """
-                INSERT INTO """+ name + """ (id, label)
-                VALUES (
-                2,
-                '7019462-Eva'
-                );
-        """
-        res = cursor.query(query).df()
-        return {"msg" : "Entry added to database" , "response" : res.to_json()}
-    
-    def delete(self,name):
-        query = """
-                DROP table """+ name +""";
-            """
-        res = cursor.query(query).df()
-        return {"response" : res.to_json()}
-
-
-
-api.add_resource(DatabaseResource, "/db/<string:name>")
-api.add_resource(TableResource, "/table/<string:name>")
-
-
-# GET UDFS
-@app.route('/udfs', methods = ['GET'])
-def udfs():
-    res = cursor.query("SHOW UDFS;").df()
-    return {"response" : res.to_json()}   
-
-#GET FUNCTIONS
-@app.route('/func', methods = ['GET'])
-def func():
-    res = cursor.query("SHOW FUNCTIONS;").df()
-    return {"response" : res.to_json()}     
-
-@app.route('/table_data/<name>',methods = ['GET'])
-def get_data(name):
-    query = "Select * from" + name + ";"
-    res = cursor.query(query).df() #Show 
-    return {"response" : res.to_json()}
-
+# Get Query response
 @app.route('/query/<query>',methods = ['GET'])
+
 def query_from_db(query):
-    res = cursor.query(query).df() #Show 
+    res = cursor.query(query).df() 
     return {"response" : res.to_json()}
+
+
+# Upload file to server
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+    
+@app.route('/upload',methods = ['GET','POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file detected')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            path = Path(UPLOAD_FOLDER +"/"+ filename)
+            print()
+            if path.is_file():
+                flash('File uploaded')
+                return {"response":"File saved"}
+            else:
+                flash('Failed to upload file')
+                return redirect(request.url)
+    return render_template('upload.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)         
