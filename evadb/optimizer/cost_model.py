ff8d881005382cbdb71b70a28272da4dc8abf8ea
Andy Xu <xzdandy@gmail.com>
2023-10-19 05:02:33 -0400

    Add CostEntry

--- evadb/optimizer/cost_model.py
+++ evadb/optimizer/cost_model.py
@@ -12,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from dataclasses import dataclass
 from functools import singledispatch
 
 from evadb.optimizer.group_expression import GroupExpression
@@ -21,6 +22,13 @@ from evadb.plan_nodes.hash_join_build_plan import HashJoinBuildPlan
 from evadb.plan_nodes.hash_join_probe_plan import HashJoinProbePlan
 from evadb.plan_nodes.nested_loop_join_plan import NestedLoopJoinPlan
 from evadb.plan_nodes.seq_scan_plan import SeqScanPlan
+from evadb.plan_nodes.storage_plan import StoragePlan
+
+
+@dataclass
+class CostEntry:
+    plan_cost: float = 1.0
+    amortize_factor: float = 1.0
 
 
 class CostModel:
@@ -31,35 +39,41 @@ class CostModel:
     def __init__(self):
         pass
 
-    def calculate_cost(self, gexpr: GroupExpression):
+    def calculate_cost(self, gexpr: GroupExpression) -> CostEntry:
         """
         Return the cost of the group expression.
         """
 
         @singledispatch
         def cost(opr: AbstractPlan):
-            return 1.0
+            return CostEntry(plan_cost=1.0)
 
         @cost.register(NestedLoopJoinPlan)
         def cost_nested_loop_join_build_plan(opr: NestedLoopJoinPlan):
-            return 1.0
+            return CostEntry(plan_cost=1.0)
 
         @cost.register(HashJoinBuildPlan)
         def cost_hash_join_build_plan(opr: HashJoinBuildPlan):
-            return 1.0
+            return CostEntry(plan_cost=1.0)
 
         @cost.register(HashJoinProbePlan)
         def cost_hash_join_probe_plan(opr: HashJoinProbePlan):
-            return 1.0
+            return CostEntry(plan_cost=1.0)
 
         @cost.register(SeqScanPlan)
         def cost_seq_scan(opr: SeqScanPlan):
-            return 1.0
+            return CostEntry(plan_cost=1.0)
+
+        @cost.register(StoragePlan)
+        def cost_storage_plan(opr: StoragePlan):
+            return CostEntry(
+                plan_cost=opr.batch_mem_size, amortize_factor=1.0 / opr.batch_mem_size
+            )
 
         @cost.register(ApplyAndMergePlan)
         def cost_apply_and_merge(opr: ApplyAndMergePlan):
             if opr.func_expr.has_cache():
-                return 0
-            return 1
+                return CostEntry(plan_cost=0)
+            return CostEntry(plan_cost=1.0)
 
         return cost(gexpr.opr)
