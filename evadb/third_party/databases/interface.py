bd344ed2c6a0a6ece31342748d4b1d76bcbf40c8
Kaushik <108662423+kaushikravichandran@users.noreply.github.com>
2023-11-05 21:11:48 -0500

    feat: third party app support in EVADB (#1033)
    This PR introduces a generic interface to support 3rd party apps in
    EVADB. As an example. the template for integrating slack has been added.
    In a subsequent PR the integration with slack will be completed.
    
    ---------
    
    Co-authored-by: Gaurav Tarlok Kakkar <gaurav21776@gmail.com>
    Co-authored-by: Joy Arulraj <jjamesaj@gmail.com>
    Co-authored-by: Joy Arulraj <arulraj@gatech.edu>
    Co-authored-by: Kaushik Ravichandran <kravicha3@ada-01.cc.gatech.edu>

--- evadb/third_party/databases/interface.py
+++ evadb/third_party/databases/interface.py
@@ -48,6 +48,8 @@ def _get_database_handler(engine: str, **kwargs):
         return mod.SnowFlakeDbHandler(engine, **kwargs)
     elif engine == "github":
         return mod.GithubHandler(engine, **kwargs)
+    elif engine == "slack":
+        return mod.SlackHandler(engine, **kwargs)
     else:
         raise NotImplementedError(f"Engine {engine} is not supported")
 
