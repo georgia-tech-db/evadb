1fbb74f6069c308582add2466ab3fb86c6701e63
Shashank Suman <shashanksuman47@gmail.com>
2023-11-14 12:25:47 -0500

    Job scheduler implementation (#1308)
    This PR adds support for creating and dropping jobs in evadb based on
    this [task](https://github.com/georgia-tech-db/evadb/issues/1248).
    
    1.  Jobs can be created using the create job query:
    
       
    > CREATE JOB {job_name} AS {
    >             {job_queries; ...}
    >     }
    >     START {start_time}
    >     END {end_time}
    >     EVERY {repeat_period} {repeat_unit}
    
    2. Created jobs can be dropped using:
    
    > DROP JOB {job_name}
    
    3. The scheduled jobs will only be triggered if the job scheduler
    process is started explicitly using:
    
    > EvaDBConnection.start_jobs()
    
    4. The job scheduler process can be stopped using:
    
    > EvaDBConnection.stop_jobs()
    
    ---------
    
    Co-authored-by: Gaurav Tarlok Kakkar <gaurav21776@gmail.com>

--- test/unit_tests/parser/test_parser.py
+++ test/unit_tests/parser/test_parser.py
@@ -1243,3 +1243,30 @@ class ParserTests(unittest.TestCase):
         self.assertNotEqual(tuple_frame, table_ref)
         self.assertNotEqual(join_node, table_ref)
         self.assertNotEqual(table_ref, table_info)
+
+    def test_create_job(self):
+        queries = [
+            """CREATE OR REPLACE FUNCTION HomeSalesForecast FROM
+                ( SELECT * FROM postgres_data.home_sales )
+                TYPE Forecasting
+                PREDICT 'price';""",
+            "Select HomeSalesForecast(10);",
+        ]
+        job_query = f"""CREATE JOB my_job AS {{
+            {''.join(queries)}
+        }}
+        START '2023-04-01'
+        END '2023-05-01'
+        EVERY 2 hour
+        """
+
+        parser = Parser()
+        job_stmt = parser.parse(job_query)[0]
+        self.assertEqual(job_stmt.job_name, "my_job")
+        self.assertEqual(len(job_stmt.queries), 2)
+        self.assertTrue(queries[0].rstrip(";") == str(job_stmt.queries[0]))
+        self.assertTrue(queries[1].rstrip(";") == str(job_stmt.queries[1]))
+        self.assertEqual(job_stmt.start_time, "2023-04-01")
+        self.assertEqual(job_stmt.end_time, "2023-05-01")
+        self.assertEqual(job_stmt.repeat_interval, 2)
+        self.assertEqual(job_stmt.repeat_period, "hour")
