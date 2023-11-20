963c145e9a071a37e19d45a9110c9b6f41c9e8c0
huaihuaiweng <38502955+huaihuaiweng@users.noreply.github.com>
2023-11-20 02:02:16 -0500

    added Alembic integration and an example of adding a new column to table_catalog

---
+++ alembic/versions/e6dc73b305fe_first_migration_adding_a_new_column_to_.py
@@ -0,0 +1,29 @@
+"""first migration: adding a new column to catalog_table
+
+Revision ID: e6dc73b305fe
+Revises: 
+Create Date: 2023-11-19 23:28:29.655424
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'e6dc73b305fe'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    with op.batch_alter_table("table_catalog") as batch_op:
+        batch_op.add_column(sa.Column("TEST", sa.String()))
+
+
+def downgrade() -> None:
+    with op.batch_alter_table("table_catalog") as batch_op:
+        batch_op.drop_column(sa.Column("TEST"))
+
