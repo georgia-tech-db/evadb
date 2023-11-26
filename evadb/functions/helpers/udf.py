ea727199e09242d2cae6bd2c852bb0b5d394c78a
Prakhar Jagwani <54989891+Prakhar314@users.noreply.github.com>
2023-11-26 03:00:02 +0000

    linting

--- evadb/functions/helpers/udf.py
+++ evadb/functions/helpers/udf.py
@@ -1,9 +1,24 @@
+# coding=utf-8
+# Copyright 2018-2023 EvaDB
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import pandas as pd
 
 from evadb.catalog.catalog_type import NdArrayType
 from evadb.functions.abstract.abstract_function import AbstractFunction
 from evadb.functions.decorators.decorators import forward, setup
 from evadb.functions.decorators.io_descriptors.data_types import PandasDataframe
+
 
 class UserDefinedFunction(AbstractFunction):
     """
@@ -21,25 +36,30 @@ class UserDefinedFunction(AbstractFunction):
         return self._func.__name__
 
     @setup(cacheable=True, batchable=True)
-    def setup(
-        self
-    ) -> None:
-        import inspect 
+    def setup(self) -> None:
+        import inspect
+
         sig = inspect.signature(self._func)
         params = sig.parameters
         # assert that all params have a type annotation
         for param in params.values():
-            assert param.annotation != inspect.Parameter.empty, f"Parameter {param.name} has no type annotation"
+            assert (
+                param.annotation != inspect.Parameter.empty
+            ), f"Parameter {param.name} has no type annotation"
         self._inputs = list(params.values())
         # get the return type annotation
         self._output = sig.return_annotation
         # assert that the return type annotation is not empty
-        assert self._output != inspect.Parameter.empty, "Return type annotation is empty"
+        assert (
+            self._output != inspect.Parameter.empty
+        ), "Return type annotation is empty"
 
         input_io_arg = PandasDataframe(
             columns=[x.name for x in self._inputs],
-            column_types=[NdArrayType.from_python_type(x.annotation) for x in self._inputs],
-            column_shapes=[(1,) for x in self._inputs]
+            column_types=[
+                NdArrayType.from_python_type(x.annotation) for x in self._inputs
+            ],
+            column_shapes=[(1,) for x in self._inputs],
         )
 
         output_io_arg = PandasDataframe(
@@ -62,7 +82,7 @@ class UserDefinedFunction(AbstractFunction):
         out_df[self.name.lower()] = in_df.apply(self._func, axis=1)
 
         return out_df
-    
+
     def __init__(self, inner_func: callable, **kwargs):
         self._func = inner_func
         super().__init__()
--- evadb/utils/load_function_class_from_file.py
+++ evadb/utils/load_function_class_from_file.py
@@ -1,3 +1,17 @@
+# coding=utf-8
+# Copyright 2018-2023 EvaDB
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 from evadb.functions.helpers.udf import UserDefinedFunction
 
 
@@ -64,5 +78,5 @@ def load_function_class_from_file(filepath, classname=None):
         raise ImportError(
             f"{filepath} contains {len(classes)} classes, please specify the correct class to load by naming the function with the same name in the CREATE query."
         )
-    
+
     return classes[0]
